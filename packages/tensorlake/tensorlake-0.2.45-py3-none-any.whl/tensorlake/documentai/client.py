c4f8e19f63c3e8c0cea5b3b62afa735f30c8823d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 16:04:00 +0000

    Add package tensorlake/0.2.45/tensorlake-0.2.45-py3-none-any.whl

---
+++ packages/tensorlake/tensorlake-0.2.45-py3-none-any.whl/tensorlake/documentai/client.py
@@ -0,0 +1,27 @@
+"""
+Tensorlake Document AI client
+"""
+
+from __future__ import annotations
+
+from typing import Optional
+
+from ._base import _BaseClient
+from ._datasets import _DatasetMixin
+from ._files import _FilesMixin
+from ._parse import _ParseMixin
+from .models import Region
+
+
+class DocumentAI(_ParseMixin, _FilesMixin, _DatasetMixin, _BaseClient):
+    """
+    Document AI client for Tensorlake.
+    """
+
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        server_url: Optional[str] = None,
+        region: Optional[Region] = Region.US,
+    ):
+        super().__init__(api_key=api_key, server_url=server_url)
