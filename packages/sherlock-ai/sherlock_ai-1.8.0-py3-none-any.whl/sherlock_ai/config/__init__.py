8166c2271287fef66c7f59043057c548f303357b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 16:04:35 +0000

    Add package sherlock-ai/1.8.0/sherlock_ai-1.8.0-py3-none-any.whl

---
+++ packages/sherlock-ai/sherlock_ai-1.8.0-py3-none-any.whl/sherlock_ai/config/__init__.py
@@ -0,0 +1,13 @@
+# src/sherlock_ai/config/__init__.py
+"""
+Configuration classes for sherlock-ai package
+"""
+
+from .logging import LogFileConfig, LoggerConfig, LoggingConfig, LoggingPresets
+
+__all__ = [
+    "LogFileConfig",
+    "LoggerConfig",
+    "LoggingConfig",
+    "LoggingPresets",
+]
