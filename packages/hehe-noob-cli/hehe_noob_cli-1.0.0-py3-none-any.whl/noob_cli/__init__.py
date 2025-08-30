915421fe088dad47ed7927a94d4541734e989995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:26 +0000

    Add package hehe-noob-cli/1.0.0/hehe_noob_cli-1.0.0-py3-none-any.whl

---
+++ packages/hehe-noob-cli/hehe_noob_cli-1.0.0-py3-none-any.whl/noob_cli/__init__.py
@@ -0,0 +1,9 @@
+"""HeHe Noob CLI - A command-line interface for noobs"""
+
+__version__ = "1.0.0"
+__author__ = "Tur Za"
+__email__ = "write2turza@gmail.com"
+
+from .cli import HeHeNoobCLI, main
+
+__all__ = ["HeHeNoobCLI", "main"]
