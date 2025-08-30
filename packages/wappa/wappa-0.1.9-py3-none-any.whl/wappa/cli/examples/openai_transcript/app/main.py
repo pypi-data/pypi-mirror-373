d981075c8ff1193b5b57ff145435c06b1e1a0ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:55 +0000

    Add package wappa/0.1.9/wappa-0.1.9-py3-none-any.whl

---
+++ packages/wappa/wappa-0.1.9-py3-none-any.whl/wappa/cli/examples/openai_transcript/app/main.py
@@ -0,0 +1,8 @@
+from wappa import Wappa
+from .master_event import TranscriptEventHandler
+
+app = Wappa()
+app.set_event_handler(TranscriptEventHandler())
+
+if __name__ == "__main__":
+    app.run()
