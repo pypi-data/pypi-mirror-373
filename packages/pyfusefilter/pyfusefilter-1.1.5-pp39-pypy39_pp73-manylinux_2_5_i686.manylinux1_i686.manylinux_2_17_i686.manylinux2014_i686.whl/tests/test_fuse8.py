b03bc6f30e4edb9eb06230e505a414db617d143d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:39 +0000

    Add package pyfusefilter/1.1.5/pyfusefilter-1.1.5-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/pyfusefilter/pyfusefilter-1.1.5-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/tests/test_fuse8.py
@@ -0,0 +1,62 @@
+from pyfusefilter import Fuse8
+from random import sample
+import tempfile, os
+
+def test_readme():
+    test_str = ["あ","अ", 51, 0.0, 12.3]
+    filter = Fuse8(test_str)
+    assert filter.contains("अ") == True
+    assert filter[51]  #You can use __getitem__ instead of contains
+    assert filter["か"] == False
+    assert filter.contains(150) == False
+
+def test_fuse8_int():
+    xor_filter = Fuse8(50)
+    xor_filter.populate([_ for _ in range(50)])
+    for i in range(50):
+        assert xor_filter.contains(i) == True
+
+def test_fuse8_int_iterable():
+    xor_filter = Fuse8(50)
+    xor_filter.populate(range(50))
+    for i in range(50):
+        assert xor_filter.contains(i) == True
+
+def test_fuse8_strings():
+    xor_filter = Fuse8(10)
+    test_str = ["あ", "/dev/null; touch /tmp/blns.fail ; echo", "अ", "Normal", "122"]
+    xor_filter.populate(test_str.copy())
+    for i in test_str:
+        assert xor_filter.contains(i) == True
+
+
+def test_fuse8_floats():
+    xor_filter = Fuse8(10)
+    test_floats = [1.23, 9999.88, 323.43, 0.0]
+    xor_filter.populate(test_floats.copy())
+    for i in test_floats:
+        assert xor_filter.contains(i) == True
+
+
+def test_fuse8_all():
+    xor_filter = Fuse8(5)
+    test_str = ["string", 51, 0.0, 12.3]
+    xor_filter.populate(test_str.copy())
+    for i in test_str:
+        assert xor_filter.contains(i) == True
+
+def test_fuse8_serialize():
+    xor_filter = Fuse8(5)
+    test_str = ["string", 51, 0.0, 12.3]
+    xor_filter.populate(test_str.copy())
+    serialized_filter = tempfile.NamedTemporaryFile(delete=False).name
+    with open(serialized_filter, 'wb') as f:
+        f.write(xor_filter.serialize())
+
+    with open(serialized_filter, 'rb') as f:
+        recover_xor_filter = Fuse8.deserialize(f.read())
+
+    for i in test_str:
+        assert recover_xor_filter.contains(i)
+
+    os.remove(serialized_filter)
