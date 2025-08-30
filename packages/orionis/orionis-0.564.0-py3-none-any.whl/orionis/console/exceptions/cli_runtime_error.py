c97305fc6999d5b3e0175ea1f58eecd70c15723a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:04:23 +0000

    Add package orionis/0.564.0/orionis-0.564.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.564.0-py3-none-any.whl/orionis/console/exceptions/cli_runtime_error.py
@@ -0,0 +1,41 @@
+class CLIOrionisRuntimeError(RuntimeError):
+    """
+    Custom exception raised when there is a runtime issue with Orionis processing.
+
+    Parameters
+    ----------
+    message : str
+        The response message associated with the exception.
+
+    Attributes
+    ----------
+    message : str
+        Stores the response message passed during initialization.
+
+    Methods
+    -------
+    __str__()
+        Returns a string representation of the exception, including the response message.
+    """
+
+    def __init__(self, message: str):
+        """
+        Initializes the CLIOrionisRuntimeError with the given response message.
+
+        Parameters
+        ----------
+        message : str
+            The response message associated with the exception.
+        """
+        super().__init__(message)
+
+    def __str__(self):
+        """
+        Returns a string representation of the exception, including the response message.
+
+        Returns
+        -------
+        str
+            A string containing the exception name and the response message.
+        """
+        return f"CLIOrionisRuntimeError: {self.args[0]}"
