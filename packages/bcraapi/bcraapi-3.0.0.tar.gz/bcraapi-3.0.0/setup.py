7774c968519d68f9cc36003741258b5f7f041ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:46 +0000

    Add package bcraapi/3.0.0/bcraapi-3.0.0.tar.gz

---
+++ packages/bcraapi/bcraapi-3.0.0.tar.gz/bcraapi-3.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from pathlib import Path
+
+VERSION = "3.0.0"
+THIS_PATH = Path(__file__).parent
+DESCRIPTION = "Python API for Banco Central de la República Argentina (BCRA)"
+LONG_DESCRIPTION = (THIS_PATH / "README.md").read_text(encoding="utf-8")
+
+
+setup(
+    name="bcraapi",
+    version=VERSION,
+    url="https://github.com/Jaldekoa/BCRA-Wrapper",
+    author="Jon Aldekoa",
+    author_email="jaldekoa@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=["bcraapi"],
+    package_data={"bcraapi": ["cert/*.crt"]},
+    include_package_data=True,
+    test_suite='bcraapi.tests.test',
+    platforms=["Any"],
+    install_requires=["pandas", "requests"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ]
+)
