1f5888c5ce21341571aab94e383bc821eec1d3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:05:05 +0000

    Add package hakoniwa-pdu/1.0.7/hakoniwa_pdu-1.0.7-py3-none-any.whl

---
+++ packages/hakoniwa-pdu/hakoniwa_pdu-1.0.7-py3-none-any.whl/hakoniwa_pdu/rpc/codes.py
@@ -0,0 +1,21 @@
+from enum import IntEnum
+
+class SystemControlOpCode(IntEnum):
+    """
+    Opcodes for the SystemControl service request.
+    """
+    ACTIVATE = 0
+    START = 1
+    STOP = 2
+    RESET = 3
+    TERMINATE = 4
+    STATUS = 5
+    # Note: 'activate' is not yet assigned a value.
+
+class SystemControlStatusCode(IntEnum):
+    """
+    Status codes for the SystemControl service response.
+    """
+    OK = 0
+    ERROR = 1
+    # Add other status codes as needed.
