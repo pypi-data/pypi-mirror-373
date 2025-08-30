dbb3f3bb496a0ba41af88bc5091428d82b0ccd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:21 +0000

    Add package geofig/1.0b6/geofig-1.0b6.tar.gz

---
+++ packages/geofig/geofig-1.0b6.tar.gz/geofig-1.0b6/src/geofig/__init__.py
@@ -0,0 +1,18 @@
+"""Library to procedurally generate geometric figures as SVG images."""
+
+from .figure import CSS, Figure, Padding, ts
+from .geometry import Arc, Scalar, Transformer, closest_entity, closest_point, ellipse_angle
+
+
+__all__ = (
+    "Arc",
+    "CSS",
+    "Figure",
+    "Padding",
+    "Scalar",
+    "Transformer",
+    "closest_entity",
+    "closest_point",
+    "ellipse_angle",
+    "ts",
+)
