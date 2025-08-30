3c1274fda57d85ca4c6241283bd93122689eccc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:36 +0000

    Add package titanic-ml-pipeline/0.1.5/titanic_ml_pipeline-0.1.5.tar.gz

---
+++ packages/titanic-ml-pipeline/titanic_ml_pipeline-0.1.5.tar.gz/titanic_ml_pipeline-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+# testingss2jsjs
+setup(
+    name="titanic-ml-pipeline",
+    version="0.1.5",
+    description="Pipeline Titanic con scikit-learn (SVD + LogisticRegression)",
+    author="Diego Hernandez",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "pandas>=1.5",
+        "numpy>=1.23",
+        "scikit-learn>=1.2",
+        "seaborn>=0.13",
+        "joblib>=1.3"
+    ],
+    entry_points={
+        "console_scripts": [
+            "titanic-train=titanic_ml_pipeline.main:main",
+        ]
+    },
+    include_package_data=True,
+)
