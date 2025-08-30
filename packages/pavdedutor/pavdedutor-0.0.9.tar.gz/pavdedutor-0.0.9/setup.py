3f0a2d5e21e45d1135bc584f40e6e486beb296ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:22 +0000

    Add package pavdedutor/0.0.9/pavdedutor-0.0.9.tar.gz

---
+++ packages/pavdedutor/pavdedutor-0.0.9.tar.gz/pavdedutor-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(
+    name='pavdedutor',
+    version='0.0.9',
+    license='MIT',
+    documentation='https://wmoural.github.io/pavdedutor/inicio/',
+    author=['Wellington Moura', 'Francisco Macedo'],
+    author_email='pro.wmoura@gmail.com',
+    description='Cálculo otimizado de valores deduzíveis para análise de patologias em pavimentos rígidos',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords='dnit pavimento norma ábaco rígido flexível',
+    packages=['pavdedutor'],
+    install_requires=['pandas', 'numpy', 'tabulate'],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
