81567b58d2d338ba85b793c6b3f6e1c5e12a667b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:53 +0000

    Add package carlosferreyra/0.0.14/carlosferreyra-0.0.14-py3-none-any.whl

---
+++ packages/carlosferreyra/carlosferreyra-0.0.14-py3-none-any.whl/carlosferreyra/__main__.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    """CLI entry point for carlosferreyra package."""
+    print("Testing entry point. it should work with uvx carlosferreyra")
+
+
+if __name__ == "__main__":
+    main()
