796e375f7daabf041c21cc88877de1fe27a078e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:16 +0000

    Add package leadtools-common-runtime/23.0.0.5/leadtools_common_runtime-23.0.0.5-py3-none-any.whl

---
+++ packages/leadtools-common-runtime/leadtools_common_runtime-23.0.0.5-py3-none-any.whl/leadtools_common_runtime/__init__.py
@@ -0,0 +1,15 @@
+# *************************************************************
+# Copyright (c) 1991-2025 Apryse Software Corp.
+# All Rights Reserved.
+# *************************************************************
+
+#     / /   / ____/   |  / __ \/_  __/ __ \/ __ \/ /  / ___/
+#    / /   / __/ / /| | / / / / / / / / / / / / / /   \__ \
+#   / /___/ /___/ ___ |/ /_/ / / / / /_/ / /_/ / /______/ /
+#  /_____/_____/_/  |_/_____/ /_/  \____/\____/_____/____/
+
+# This module contains runtime data files required to use LEADTOOLS .NET Core in Python 3.7+
+# This module requires Python.NET
+# This module requires the .NET Core runtime
+# Full License Text: https://www.leadtools.com/help/sdk/licensing/intellectual-property-notices.html
+
---
+++ packages/leadtools-common-runtime/leadtools_common_runtime-23.0.0.5-py3-none-any.whl/leadtools_common_runtime/__pkginfo__.py
@@ -0,0 +1,26 @@
+# *************************************************************
+# Copyright (c) 1991-2025 Apryse Software Corp.
+# All Rights Reserved.
+# *************************************************************
+
+#     / /   / ____/   |  / __ \/_  __/ __ \/ __ \/ /  / ___/
+#    / /   / __/ / /| | / / / / / / / / / / / / / /   \__ \
+#   / /___/ /___/ ___ |/ /_/ / / / / /_/ / /_/ / /______/ /
+#  /_____/_____/_/  |_/_____/ /_/  \____/\____/_____/____/
+
+# This module contains runtime data files required to use LEADTOOLS .NET Core in Python 3.7+
+# This module requires Python.NET
+# This module requires the .NET Core runtime
+# Full License Text: https://www.leadtools.com/help/sdk/licensing/intellectual-property-notices.html
+
+__product__ = "LEADTOOLS Common Runtime Files"
+__author__ = "Apryse Software Corp."
+__version__ = "23.0.0.1"
+vs = __version__.split(".")
+__version_info__ = (vs[0], vs[1], vs[2], vs[3])
+__copyright__ = "Copyright (c) 1991-2025 Apryse Software Corp."
+__license__ = "Proprietary License - All Rights Reserved"
+
+__uri__ = "https://www.leadtools.com"
+__email__ = "support@leadtools.com"
+__chat__ = "https://www.leadtools.com/support/chat"
