7cd9bbf710b0e09bf3b1b38ddb690287c4ffe151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:33 +0000

    Add package paar-dev/0.0.16/paar_dev-0.0.16-py3-none-any.whl

---
+++ packages/paar-dev/paar_dev-0.0.16-py3-none-any.whl/paar_dev/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    gerar_df_phoenix,
+    abrir_planilha,
+    tratar_colunas_df,
+    gsheet_to_df,
+    plotar_tabela_aggrid_colher_selecao,
+    tratar_colunas_df_para_insercao_gsheet
+)
+
+__all__ = [
+    "gerar_df_phoenix",
+    "abrir_planilha",
+    "tratar_colunas_df",
+    "gsheet_to_df",
+    "plotar_tabela_aggrid_colher_selecao",
+    "tratar_colunas_df_para_insercao_gsheet"
+]
