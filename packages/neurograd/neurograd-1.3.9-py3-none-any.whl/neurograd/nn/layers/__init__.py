8bfeacc67a21b0def73c386121dd8ff5851951ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:34 +0000

    Add package neurograd/1.3.9/neurograd-1.3.9-py3-none-any.whl

---
+++ packages/neurograd/neurograd-1.3.9-py3-none-any.whl/neurograd/nn/layers/__init__.py
@@ -0,0 +1,18 @@
+from ..module import Module, Sequential
+from .linear import Linear, MLP
+from .conv import Conv2D, MaxPool2D, AveragePool2D, MaxPooling2D, AveragePooling2D
+from .batchnorm import BatchNorm, BatchNorm2D
+from .dropout import Dropout, Dropout2D
+from neurograd.functions.tensor_ops import Flatten, Pad
+from .augment import (
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomResizedCrop,
+    ShiftScaleRotate,
+    ColorJitter,
+    RandomGamma,
+    RGBShift,
+    GaussianNoise,
+    CoarseDropout,
+    ComposeAug,
+)
