b84764f2dd2ba39552aad4d8f4be9ac0b7a187cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:25 +0000

    Add package named-arrays/0.26.0/named_arrays-0.26.0-py3-none-any.whl

---
+++ packages/named-arrays/named_arrays-0.26.0-py3-none-any.whl/named_arrays/_core_array_functions.py
@@ -0,0 +1,36 @@
+from typing import Callable
+
+import numpy as np
+import named_arrays as na
+
+__all__ = [
+    'HANDLED_FUNCTIONS',
+]
+
+HANDLED_FUNCTIONS = dict()
+
+
+def implements(numpy_function: Callable):
+    """Register an ``__array_function__`` implementation for :class:`named_array.AbstractArray` objects."""
+    def decorator(func):
+        HANDLED_FUNCTIONS[numpy_function] = func
+        return func
+    return decorator
+
+
+@implements(np.shape)
+def shape(
+        a: na.AbstractScalarArray,
+) -> dict[str, int]:
+    return a.shape
+
+
+@implements(np.broadcast_to)
+def broadcast_to(
+    array: na.AbstractArray,
+    shape: dict[str, int],
+) -> na.AbstractExplicitArray:
+    return na.broadcast_to(
+        array=array,
+        shape=shape,
+    )
