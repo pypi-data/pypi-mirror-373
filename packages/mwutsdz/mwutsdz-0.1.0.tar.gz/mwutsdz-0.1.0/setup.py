09466a124f50ad5055af801523279ac5934b2773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 16:03:57 +0000

    Add package mwutsdz/0.1.0/mwutsdz-0.1.0.tar.gz

---
+++ packages/mwutsdz/mwutsdz-0.1.0.tar.gz/mwutsdz-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mwutsdz",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "pandas",
+        "matplotlib"
+    ],
+    author="Ton Nom",
+    author_email="ton.email@example.com",
+    description="MWUT-SDZ: Détection automatique de knee point dans les données de cycle de batteries",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/toncompte/mwutsdz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
+
