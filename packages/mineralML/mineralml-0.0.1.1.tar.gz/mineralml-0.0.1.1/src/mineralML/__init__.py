3ee181c02141735b644309863d701d1f92d73063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 15:03:47 +0000

    Add package mineralML/0.0.1.1/mineralml-0.0.1.1.tar.gz

---
+++ packages/mineralML/mineralml-0.0.1.1.tar.gz/mineralml-0.0.1.1/src/mineralML/__init__.py
@@ -0,0 +1,20 @@
+# %%
+
+__author__ = "Sarah Shi"
+
+import os
+import copy
+import numpy as np
+import pandas as pd
+import warnings
+
+from matplotlib import pyplot as plt
+
+from mineralML.core import *
+from mineralML.stoichiometry import *
+from mineralML.synthetic_minerals import *
+from mineralML.unsupervised import *
+from mineralML.supervised import *
+from mineralML.confusion_matrix import *
+
+from ._version import __version__
