e694d55ebda91c07703466040cb16db02f0c4735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 14:03:58 +0000

    Add package nixt/400/nixt-400-py3-none-any.whl

---
+++ packages/nixt/nixt-400-py3-none-any.whl/nixt/modules/tbl.py
@@ -0,0 +1,57 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
+
+
+MD5 = {
+    "fnd": "491c747fa2cc0d18577c59fb0e890026",
+    "irc": "83a4ef4329b9fff61d0e9be71b11c09d",
+    "log": "5849c21700eca3bb52ae9ae5e1e4212d",
+    "lst": "a55ddd1a44f65f8c8bde773952add426",
+    "mbx": "fd1ffa705878f13fcd108279bd48e87b",
+    "mdl": "db9cec4e7e0e39b458c449e73237905a",
+    "req": "793704d64443c4f19512924e294943ea",
+    "rss": "2ae232a55bbb00fd754f972d5fdcbb1e",
+    "rst": "152f5d8400bc2cce03bf11622c716686",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "b12881ab4103278d7a5ccff787a9043f",
+    "thr": "f7c5fe5e8e7e36452c6b3a39fe0f8f82",
+    "tmr": "22e1baf198f726d45f9686918207d0e5",
+    "udp": "ff009d24d36deda6fd63dd984a206c54",
+    "upt": "fb2ac27489391a75ceefe0b8c36e503d",
+    "web": "93e88d0e5bd4540c23f08b125060ee41",
+    "wsd": "7bb44c895580b00d0f6bf083e0881267",
+}
