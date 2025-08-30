9888ee3e8309fc73bb80ecfc45eb0f087b3b8149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:14:39 +0000

    Add package numero-fun/0.1.2/numero_fun-0.1.2-py3-none-any.whl

---
+++ packages/numero-fun/numero_fun-0.1.2-py3-none-any.whl/numero_fun/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa: E501
+"""
+Numero Uno - A Fun Number Game and Numerology Calculator
+"""
+
+from .calculator import calculate_number, letter_to_number, get_sum
+
+__version__ = "0.1.0"
+__author__ = "Garima Shrivastava"
+
+__all__ = ["calculate_number", "letter_to_number", "get_sum"]
