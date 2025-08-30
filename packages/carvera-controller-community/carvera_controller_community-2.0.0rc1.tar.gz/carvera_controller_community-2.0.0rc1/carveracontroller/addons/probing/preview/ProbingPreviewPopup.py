7dd3b466573f8eeb250a6aa31ab72e1248540163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 17:04:02 +0000

    Add package carvera-controller-community/2.0.0rc1/carvera_controller_community-2.0.0rc1.tar.gz

---
+++ packages/carvera-controller-community/carvera_controller_community-2.0.0rc1.tar.gz/carvera_controller_community-2.0.0rc1/carveracontroller/addons/probing/preview/ProbingPreviewPopup.py
@@ -0,0 +1,32 @@
+from kivy.properties import StringProperty
+from kivy.uix.modalview import ModalView
+import logging
+logger = logging.getLogger(__name__)
+
+from carveracontroller.addons.probing.operations.OperationsBase import OperationsBase
+
+class ProbingPreviewPopup(ModalView):
+    title = StringProperty('Confirm')
+    probe_preview_label = StringProperty('N/A')
+    config: dict[str, float]
+    gcode = StringProperty("")
+
+
+    def __init__(self, controller, **kwargs):
+        self.controller = controller
+        super(ProbingPreviewPopup, self).__init__(**kwargs)
+
+    def get_probe_switch_type(self):
+        return 1
+        # if self.cb_probe_normally_closed.active:
+        #     return ProbingConstants.switch_type_nc
+        #
+        # if self.cb_probe_normally_open.active:
+        #     return ProbingConstants.switch_type_no
+
+    def  start_probing(self):
+        if len(self.gcode) > 0:
+            logger.debug("running gcode: " + self.gcode)
+            self.controller.executeCommand(self.gcode + "\n")
+        else:
+            logger.error("no gcode")
