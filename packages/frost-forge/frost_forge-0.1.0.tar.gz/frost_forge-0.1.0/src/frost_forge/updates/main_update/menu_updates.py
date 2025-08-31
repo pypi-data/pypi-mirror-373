a7621e0a561dabf527c71bcc00296b34fb4c59d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:47 +0000

    Add package frost-forge/0.1.0/frost_forge-0.1.0.tar.gz

---
+++ packages/frost-forge/frost_forge-0.1.0.tar.gz/frost_forge-0.1.0/src/frost_forge/updates/main_update/menu_updates.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+from ..menu_update import update_keys, update_mouse
+
+def update_menu(state, chunks):
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            state.run = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            chunks = update_mouse(state, event, chunks)
+        elif event.type == pg.KEYDOWN:
+            state.save_file_name, state.controls, state.menu_placement, state.scroll, state.control_adjusted = update_keys(state.menu_placement, state.save_file_name, state.controls, state.control_adjusted, chunks, state.scroll)
+    return chunks
---
+++ packages/frost-forge/frost_forge-0.1.0.tar.gz/frost_forge-0.1.0/src/frost_forge/updates/menu_update/mouse.py
@@ -0,0 +1,56 @@
+from json import dumps
+from os import path
+
+from ...render.menu_rendering import SAVES_FOLDER
+from .load_save import save_loading
+from .create_save import save_creating
+from .options import option
+from ...info import SCREEN_SIZE, WORLD_TYPES
+
+def update_mouse(state, event, chunks):
+    if state.menu_placement == "load_save":
+        if state.position[1] <= 50:
+            state.menu_placement = "main_menu"
+        elif state.position[1] <= 100:
+            state.menu_placement = "save_options"
+            state.world_type = 0
+        else:
+            chunks = save_loading(state, chunks)
+    elif state.menu_placement.startswith("options"):
+        option(state, chunks)
+
+    elif state.menu_placement == "save_options":
+        if state.position[1] <= 50:
+            chunks = save_creating(state, chunks)
+        elif state.position[1] <= 100:
+            state.world_type = (state.world_type + 1) % len(WORLD_TYPES)
+    elif state.menu_placement == "save_creation":
+        if 200 <= state.position[1] <= 250 and state.save_file_name != "" and state.save_file_name.split("_")[0] != "autosave":
+            state.menu_placement = "main_menu"
+            with open(path.join(SAVES_FOLDER, state.save_file_name + ".txt"), "w", encoding="utf-8") as file:
+                file.write(f"{chunks};{state.location['tile']};{state.tick};{state.noise_offset};{state.world_type}")
+            state.save_file_name = ""
+            chunks = {}
+        elif 300 <= state.position[1] <= 350:
+            state.menu_placement = "main_menu"
+            state.save_file_name = ""
+            chunks = {}
+
+    elif state.menu_placement == "main_menu":
+        if 0 <= state.position[1] <= 50:
+            state.menu_placement = "load_save"
+        elif 100 <= state.position[1] <= 150:
+            state.menu_placement = "options_main"
+        elif 200 <= state.position[1] <= 250:
+            state.run = False
+
+    elif state.menu_placement == "controls_options":
+        if event.button == 4:
+            if state.scroll > 0:
+                state.scroll -= 1
+        elif event.button == 5:
+            if state.scroll < len(state.controls) - SCREEN_SIZE[1] // 50 - 1:
+                state.scroll += 1
+        else:
+            state.control_adjusted = state.scroll + state.position[1] // 50
+    return chunks
