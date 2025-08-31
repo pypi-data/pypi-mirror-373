5a92767f922c557574d13c5c709ce677ef068084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:01 +0000

    Add package domselect/0.0.3/domselect-0.0.3.tar.gz

---
+++ packages/domselect/domselect-0.0.3.tar.gz/domselect-0.0.3/domselect/__init__.py
@@ -0,0 +1,6 @@
+from .base import BaseSelector
+from .lexbor_selector import LexborSelector
+from .lxml_selector import LxmlSelector
+
+__all__ = ["BaseSelector", "LexborSelector", "LxmlSelector"]
+__version__ = "0.0.3"
