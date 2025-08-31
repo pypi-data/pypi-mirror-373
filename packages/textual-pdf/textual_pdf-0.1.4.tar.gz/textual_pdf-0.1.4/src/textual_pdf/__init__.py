ed77d7127f5cd1a5993c9c86b19c68cf1939be98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:20:44 +0000

    Add package textual-pdf/0.1.4/textual_pdf-0.1.4.tar.gz

---
+++ packages/textual-pdf/textual_pdf-0.1.4.tar.gz/textual_pdf-0.1.4/src/textual_pdf/__init__.py
@@ -0,0 +1,4 @@
+from textual_pdf.exceptions import NotAPDFError, PDFHasAPasswordError, PDFRuntimeError
+from textual_pdf.pdf_viewer import PDFViewer
+
+__all__ = ["PDFViewer", "NotAPDFError", "PDFHasAPasswordError", "PDFRuntimeError"]
