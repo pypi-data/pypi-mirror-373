9c9da6aaf92a5b78565f3f679770680c32a006b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:20:06 +0000

    Add package ripple-down-rules/0.3.1/ripple_down_rules-0.3.1-py3-none-any.whl

---
+++ packages/ripple-down-rules/ripple_down_rules-0.3.1-py3-none-any.whl/ripple_down_rules/datastructures/tracked_object.py
@@ -0,0 +1,215 @@
+from __future__ import annotations
+
+import inspect
+import uuid
+from collections import defaultdict
+from dataclasses import dataclass, field, Field, fields
+from enum import Enum
+from functools import lru_cache
+
+import pydot
+import rustworkx as rx
+from typing_extensions import Any, Type, ClassVar, Dict, List, Optional, Tuple
+
+from .field_info import FieldInfo
+from .. import logger
+from ..rules import Rule
+from ..utils import recursive_subclasses
+
+
+class Direction(Enum):
+    OUTBOUND = False
+    INBOUND = True
+
+
+class Relation(str, Enum):
+    has = "has"
+    isA = "isA"
+    dependsOn = "dependsOn"
+
+
+@dataclass(unsafe_hash=True)
+class TrackedObjectMixin:
+    """
+    A class that is used as a base class to all classes that needs to be tracked for RDR inference, and reasoning.
+    """
+    _rdr_tracked_object_id: int = field(init=False, repr=False, hash=False,
+                                        compare=False, default_factory=lambda: uuid.uuid4().int)
+    """
+    The unique identifier of the conclusion.
+    """
+    _dependency_graph: ClassVar[rx.PyDAG[Type[TrackedObjectMixin]]] = rx.PyDAG()
+    """
+    A graph that represents the relationships between all tracked objects.
+    """
+    _class_graph_indices: ClassVar[Dict[Type[TrackedObjectMixin], int]] = {}
+    """
+    The index of the current class in the dependency graph.
+    """
+    _edges: ClassVar[Dict[Relation, List[Tuple[int, int]]]] = defaultdict(list)
+    """
+    All the edges indexed by relation type.
+    """
+    _overridden_by: Type[TrackedObjectMixin] = field(init=False, repr=False, hash=False,
+                                                     compare=False, default=None)
+    """
+    Whether the class has been overridden by a subclass.
+    This is used to only include the new class in the dependency graph, not the overridden class.
+    """
+
+    @classmethod
+    def _reset_dependency_graph(cls) -> None:
+        """
+        Reset the dependency graph and all class indices.
+        """
+        cls._dependency_graph = rx.PyDAG()
+        cls._class_graph_indices = {}
+        cls._edges = defaultdict(list)
+        cls.parse_fields.cache_clear()
+        cls.get_fields.cache_clear()
+        cls.parse_field.cache_clear()
+
+    @classmethod
+    def _my_graph_idx(cls):
+        return cls._class_graph_indices[cls]
+
+    @classmethod
+    def make_class_dependency_graph(cls, composition: bool = True):
+        """
+        Create a direct acyclic graph containing the class hierarchy.
+
+        :param composition: If True, the class dependency graph will include composition relations.
+        """
+        classes_to_track = recursive_subclasses(cls) + [Rule] + recursive_subclasses(Rule)
+        for clazz in classes_to_track:
+            cls._add_class_to_dependency_graph(clazz)
+
+        for clazz in cls._class_graph_indices:
+            bases = [base for base in clazz.__bases__ if
+                     base.__module__ not in ["builtins"] and base in cls._class_graph_indices]
+
+            for base in bases:
+                cls._add_class_to_dependency_graph(base)
+                clazz_idx = cls._class_graph_indices[clazz]
+                base_idx = cls._class_graph_indices[base]
+                if (clazz_idx, base_idx) in cls._edges[Relation.isA] or base._overridden_by == clazz:
+                    continue
+                cls.add_edge(clazz_idx, base_idx, Relation.isA)
+
+        if not composition:
+            return
+        for clazz, idx in cls._class_graph_indices.items():
+            if clazz.__module__ == "builtins":
+                continue
+            TrackedObjectMixin.parse_fields(clazz)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def parse_fields(clazz) -> None:
+        for f in TrackedObjectMixin.get_fields(clazz):
+
+            logger.debug("=" * 80)
+            logger.debug(f"Processing Field {clazz.__name__}.{f.name}: {f.type}.")
+
+            # skip private fields
+            if f.name.startswith("_"):
+                logger.debug(f"Skipping since the field starts with _.")
+                continue
+
+            field_info = FieldInfo(clazz, f)
+            TrackedObjectMixin.parse_field(field_info)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def get_fields(clazz) -> List[Field]:
+        skip_fields = []
+        bases = [base for base in clazz.__bases__ if issubclass(base, TrackedObjectMixin)]
+        for base in bases:
+            skip_fields.extend(TrackedObjectMixin.get_fields(base))
+
+        result = [cls_field for cls_field in fields(clazz) if cls_field not in skip_fields]
+
+        return result
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def parse_field(field_info: FieldInfo):
+        parent_idx = TrackedObjectMixin._class_graph_indices[field_info.clazz]
+        field_cls: Optional[Type[TrackedObjectMixin]] = None
+        field_relation = Relation.has
+        if len(field_info.type) == 1 and issubclass(field_info.type[0], TrackedObjectMixin):
+            field_cls = field_info.type[0]
+        else:
+            # TODO: Create a new TrackedObjectMixin class for new type
+            logger.debug(f"Skipping unhandled field type: {field_info.type}")
+            # logger.debug(f"Creating new TrackedObject type for builtin type {field_info.type}.")
+            # field_cls = cls._create_tracked_object_class_for_field(field_info)
+
+        if field_cls is not None:
+            field_cls_idx = TrackedObjectMixin._class_graph_indices.get(field_cls, None)
+            if field_cls_idx is not None and (parent_idx, field_cls_idx) in TrackedObjectMixin._edges[Relation.has]:
+                return
+            elif field_cls_idx is None:
+                TrackedObjectMixin._add_class_to_dependency_graph(field_cls)
+                field_cls_idx = TrackedObjectMixin._class_graph_indices[field_cls]
+            TrackedObjectMixin.add_edge(parent_idx, field_cls_idx, field_relation)
+
+    @classmethod
+    def add_edge(cls, parent_idx: int, child_idx: int, relation: Relation):
+        cls._dependency_graph.add_edge(parent_idx, child_idx, relation)
+        cls._edges[relation].append((parent_idx, child_idx))
+
+    @classmethod
+    def _create_tracked_object_class_for_field(cls, field_info: FieldInfo):
+        raise NotImplementedError
+
+    @classmethod
+    def to_dot(cls, filepath: str, format='png') -> None:
+        if not filepath.endswith(f".{format}"):
+            filepath += f".{format}"
+        dot_str = cls._dependency_graph.to_dot(
+            lambda node: dict(
+                color='black', fillcolor='lightblue', style='filled', label=node.__name__),
+            lambda edge: dict(color='black', style='solid', label=edge))
+        dot = pydot.graph_from_dot_data(dot_str)[0]
+        dot.write(filepath, format=format)
+
+    @classmethod
+    def _add_class_to_dependency_graph(cls, class_to_add: Type[TrackedObjectMixin]) -> None:
+        """
+        Add a class to the dependency graph.
+        """
+        if class_to_add not in cls._class_graph_indices:
+            if not issubclass(class_to_add, TrackedObjectMixin):
+                class_to_add._overridden_by = None
+            cls_idx = cls._dependency_graph.add_node(class_to_add._overridden_by or class_to_add)
+            cls._class_graph_indices[class_to_add] = cls_idx
+            if class_to_add._overridden_by:
+                cls._class_graph_indices[class_to_add._overridden_by] = cls_idx
+
+    def __getattribute__(self, name: str) -> Any:
+        # if name not in [f.name for f in fields(TrackedObjectMixin)] + ['has', 'is_a', 'depends_on']\
+        #         and not name.startswith("_"):
+        #     self._record_dependency(name)
+        return object.__getattribute__(self, name)
+
+    def _record_dependency(self, attr_name):
+        # Inspect stack to find instance of CallableExpression
+        for frame_info in inspect.stack():
+            func_name = frame_info.function
+            local_self = frame_info.frame.f_locals.get("self", None)
+            if (
+                    func_name == "__call__" and
+                    local_self is not None and
+                    type(local_self).__module__ == "callable_expression" and
+                    type(local_self).__name__ == "CallableExpression"
+            ):
+                logger.debug("TrackedObject used inside CallableExpression")
+                break
+
+
+annotations = TrackedObjectMixin.__annotations__
+for val in [f.name for f in fields(TrackedObjectMixin)]:
+    annotations.pop(val, None)
+
+X = TrackedObjectMixin
---
+++ packages/ripple-down-rules/ripple_down_rules-0.3.1-py3-none-any.whl/ripple_down_rules/predicates.py
@@ -0,0 +1,186 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Generator, ClassVar
+
+from typing_extensions import Type, TYPE_CHECKING, Tuple, Dict, List
+
+from .datastructures.enums import InferMode
+from .datastructures.tracked_object import TrackedObjectMixin, Direction, Relation
+
+if TYPE_CHECKING:
+    pass
+
+
+@dataclass(eq=False)
+class Predicate(TrackedObjectMixin, ABC):
+
+    cached_results: ClassVar[Dict[Tuple, List]] = defaultdict(list)
+    """
+    Cached results between retrievals, makes future retrievals faster,
+    """
+    inferred_once: ClassVar[bool] = None
+    """
+    If inference has been performed at least once.
+    """
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.inferred_once = False
+
+    @classmethod
+    @abstractmethod
+    def relation(cls) -> Relation:
+        """
+        The relation type of the predicate.
+        """
+
+    def __call__(self, *args, infer: InferMode = InferMode.Auto, **kwargs):
+        if self.should_infer(mode=infer):
+            self.infer(*args, **kwargs)
+            self.cached_results.clear()
+            self.__class__.inferred_once = True
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in self.cached_results:
+            yield from self.cached_results[key]
+        else:
+            for value in self.retrieve(*args, **kwargs):
+                self.cached_results[key].append(value)
+                yield value
+
+    @classmethod
+    def should_infer(cls, mode: InferMode) -> bool:
+        """
+        Determine if the predicate relations should be inferred or just retrieve current relations.
+
+        :param mode: The infer mode of the predicate (could be Auto, Always, Never)
+        """
+        if mode == InferMode.Always:
+            return True
+        elif mode == InferMode.Never:
+            return False
+        else:
+            return cls._should_infer()
+
+    @classmethod
+    @abstractmethod
+    def _should_infer(cls) -> bool:
+        """
+        Predicate specific reasoning on when to infer relations.
+        """
+
+    @classmethod
+    @abstractmethod
+    def infer(cls, *args, **kwargs):
+        """
+        Evaluate the predicate with the given arguments.
+        This method should be implemented by subclasses.
+        """
+
+    @classmethod
+    def retrieve(cls, node1_type: Type[TrackedObjectMixin],
+                 node2_type: Type[TrackedObjectMixin], recursive: bool = False, is_reversed: bool = False) \
+            -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
+        if node1_type is not TrackedObjectMixin:
+            direction = Direction.INBOUND.value if is_reversed else Direction.OUTBOUND.value
+            owner_idx = node1_type._my_graph_idx()
+            neighbors = cls._dependency_graph.adj_direction(owner_idx, direction)
+            neighbor_generator = ((owner_idx, n) for n, e in neighbors.items()
+                                  if (e == cls.relation() and issubclass(cls._dependency_graph.get_node_data(n),
+                                                                         node2_type))
+                                  or (e == Relation.isA and any(cls.retrieve(cls._dependency_graph.get_node_data(n),
+                                                                             node2_type)))
+                                  )
+            latest_results = []
+            for v in neighbor_generator:
+                res = (cls._dependency_graph.get_node_data(v[0]), cls._dependency_graph.get_node_data(v[1]))
+                latest_results.append(v)
+                yield res
+            if recursive:
+                for n in [n for n, e in neighbors.items() if e == cls.relation()]:
+                    for v in cls.retrieve(cls._dependency_graph.get_node_data(n), node2_type, recursive=True,
+                                          is_reversed=is_reversed):
+                        yield node1_type, v[1]
+        elif node2_type is not TrackedObjectMixin:
+            # owner type is TrackedObjectMixin
+            yield from map(lambda t: (t[1], t[0]),
+                           cls.retrieve(node2_type, node1_type, recursive=recursive, is_reversed=True))
+        else:
+            # both are TrackedObjectMixin
+            yield from map(
+                lambda t: (cls._dependency_graph.get_node_data(t[0]), cls._dependency_graph.get_node_data(t[1])),
+                cls._edges[cls.relation()])
+
+    def __hash__(self):
+        return hash(self.__class__.__name__)
+
+    def __eq__(self, other):
+        if not isinstance(other, Predicate):
+            return False
+        return self.__class__ == other.__class__
+
+
+@dataclass(eq=False)
+class IsA(Predicate):
+    """
+    A predicate that checks if an object type is a subclass of another object type.
+    """
+
+    @classmethod
+    def relation(cls):
+        return Relation.isA
+
+    @classmethod
+    def infer(cls, child_type: Type[TrackedObjectMixin], parent_type: Type[TrackedObjectMixin]):
+        TrackedObjectMixin.make_class_dependency_graph(composition=False)
+
+    @classmethod
+    def _should_infer(cls) -> bool:
+        return not cls.inferred_once
+
+
+isA = IsA()
+
+
+@dataclass(eq=False)
+class Has(Predicate):
+    """
+    A predicate that checks if an object type has a certain member object type.
+    """
+    @classmethod
+    def relation(cls):
+        return Relation.has
+
+    @classmethod
+    def infer(cls, owner_type: Type[TrackedObjectMixin], member_type: Type[TrackedObjectMixin]):
+        TrackedObjectMixin.make_class_dependency_graph()
+
+    @classmethod
+    def _should_infer(cls) -> bool:
+        return not cls.inferred_once
+
+
+has = Has()
+
+
+@dataclass(eq=False)
+class DependsOn(Predicate):
+    """
+    A predicate that checks if an object type depends on another object type.
+    """
+
+    @classmethod
+    def relation(cls):
+        return Relation.dependsOn
+
+    @classmethod
+    def infer(cls, dependent: Type[TrackedObjectMixin],
+                 dependency: Type[TrackedObjectMixin], recursive: bool = False) -> bool:
+        raise NotImplementedError("Should be overridden in rdr meta")
+
+    @classmethod
+    def _should_infer(cls) -> bool:
+        return not cls.inferred_once
+
+
+dependsOn = DependsOn()
