9e8d4a337a30430121ee509fd6444705fa0226b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:45 +0000

    Add package venai-foobartest/1.28.3/venai_foobartest-1.28.3.tar.gz

---
+++ packages/venai-foobartest/venai_foobartest-1.28.3.tar.gz/venai_foobartest-1.28.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+from pathlib import Path
+
+parent = Path(__file__).parent
+
+description = (parent / "README.md").read_text()
+requirements = (parent / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="venai-foobartest",
+    version="1.28.3",
+    author="Mert Sirakaya",
+    author_email="contact@tomris.dev",
+    maintainer="Mert Sirakaya",
+    maintainer_email="contact@tomris.dev",
+    description="VenusAI is a secure and extensible Agent framework built for modern AI applications.",
+    packages=["venus"],
+    fullname="VenusAI",
+    install_requires=requirements,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VenusAgent/VenusAI",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    include_package_data=True,
+    license="MIT",
+    keywords="safety, ai, agent, llm, agentic ai, pydantic, pydantic-ai, self-healing, sandbox, ai agent",
+    project_urls={
+        "Documentation": "https://venai.tech/docs",
+        "Source": "https://github.com/VenusAgent/VenusAI",
+        "Tracker": "https://github.com/VenusAgent/VenusAI/issues",
+    },
+    entry_points={
+        "console_scripts": [
+            "venus=venus.__main__:main",
+            "venai=venus.__main__:main",
+        ],
+    },
+    
+)
