acd4bbbf4fad4ac762d3fd4d02a653dbbce13b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:20:56 +0000

    Add package flask-Humanify/0.2.7.1/flask_humanify-0.2.7.1.tar.gz

---
+++ packages/flask-Humanify/flask_humanify-0.2.7.1.tar.gz/flask_humanify-0.2.7.1/flask_humanify/__init__.py
@@ -0,0 +1,15 @@
+"""
+Flask-Humanify
+-----------
+A Flask extension that protects against bots and DDoS attacks.
+"""
+
+__version__ = "0.2.7.1"
+
+from . import utils
+from .humanify import Humanify
+from .features.rate_limiter import RateLimiter
+from .features.error_handler import ErrorHandler
+
+
+__all__ = ["Humanify", "RateLimiter", "ErrorHandler", "utils"]
