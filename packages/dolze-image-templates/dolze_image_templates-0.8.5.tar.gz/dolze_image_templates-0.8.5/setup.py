8d5f14ce30ca5697e361456f0088b543dd3e8c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 05:04:02 +0000

    Add package dolze-image-templates/0.8.5/dolze_image_templates-0.8.5.tar.gz

---
+++ packages/dolze-image-templates/dolze_image_templates-0.8.5.tar.gz/dolze_image_templates-0.8.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dolze-image-templates",
+    version="0.1.2",
+    author="Dolze Team",
+    author_email="support@dolze.com",
+    description="A package for generating Dolze templates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/dolze-templates",
+    packages=find_packages(),
+    package_data={
+        "dolze_image_templates": ["templates/*", "fonts/*"],
+    },
+    install_requires=[
+        "Pillow>=9.0.0",
+        "requests>=2.25.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
