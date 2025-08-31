da96190dbe68d6826a97ae021838fc7d0ebf0eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:15 +0000

    Add package orionis/0.572.0/orionis-0.572.0.tar.gz

---
+++ packages/orionis/orionis-0.572.0.tar.gz/orionis-0.572.0/orionis/console/exceptions/__init__.py
@@ -0,0 +1,15 @@
+from .cli_exception import (
+    CLIOrionisException,
+    CLIOrionisRuntimeError,
+    CLIOrionisScheduleException,
+    CLIOrionisValueError,
+    CLIOrionisTypeError
+)
+
+__all__ = [
+    'CLIOrionisException',
+    'CLIOrionisRuntimeError',
+    'CLIOrionisScheduleException',
+    'CLIOrionisValueError',
+    'CLIOrionisTypeError',
+]
