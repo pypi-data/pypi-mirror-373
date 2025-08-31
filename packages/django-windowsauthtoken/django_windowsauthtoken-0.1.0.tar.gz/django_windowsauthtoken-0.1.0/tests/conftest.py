23dafbd91b2284003691866c4c89ccec1edd10b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:42 +0000

    Add package django-windowsauthtoken/0.1.0/django_windowsauthtoken-0.1.0.tar.gz

---
+++ packages/django-windowsauthtoken/django_windowsauthtoken-0.1.0.tar.gz/django_windowsauthtoken-0.1.0/tests/conftest.py
@@ -0,0 +1,21 @@
+import os
+
+from django.conf import settings
+
+
+def pytest_configure():
+    # Make sure that we can run all tests even on non-Windows platforms
+    os.environ.setdefault("WINDOWSAUTHTOKEN_IGNORE_PYWIN32_ERRORS", "true")
+
+    # Set up Django settings for the tests
+    settings.configure(
+        DATABASES={"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}},
+        INSTALLED_APPS=[
+            "django.contrib.contenttypes",
+            "django.contrib.auth",
+            "django.contrib.sessions",
+            "django.contrib.admin",
+        ],
+        ROOT_URLCONF="urlconf",
+        SECRET_KEY="django-insecure-test-key",
+    )
