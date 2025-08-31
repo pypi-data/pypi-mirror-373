d09bc832c3e0522fd42c87499a07bd1adcfa7c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:02 +0000

    Add package memory-agent/2.1.0/memory_agent-2.1.0.tar.gz

---
+++ packages/memory-agent/memory_agent-2.1.0.tar.gz/memory_agent-2.1.0/memory_agent/state.py
@@ -0,0 +1,8 @@
+from langgraph.prebuilt.chat_agent_executor import AgentState
+from langmem.short_term import RunningSummary
+
+
+class State(AgentState):
+    # NOTE: we're adding this key to keep track of previous summary information
+    # to make sure we're not summarizing on every LLM call
+    context: dict[str, RunningSummary]
