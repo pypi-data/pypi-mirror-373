89a77932a7ecbe0c9c67c10150a7bea2b124814a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 05:04:00 +0000

    Add package gyptis/1.0.3/gyptis-1.0.3.tar.gz

---
+++ packages/gyptis/gyptis-1.0.3.tar.gz/gyptis-1.0.3/src/gyptis/__about__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of gyptis
+# Version: 1.0.2
+# License: MIT
+# See the documentation at gyptis.gitlab.io
+"""
+Information about the package.
+"""
+
+try:
+    # Python 3.8
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+try:
+    data = metadata.metadata("gyptis")
+    __version__ = metadata.version("gyptis")
+    __author__ = data.get("author")
+    __description__ = data.get("summary")
+except Exception:
+    __version__ = "unknown"
+    __author__ = "unknown"
+    __description__ = "unknown"
