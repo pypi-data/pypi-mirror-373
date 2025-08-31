ff93b8e139777e34df34e444d4fe5e27275e9469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:01 +0000

    Add package bluer-ugv/6.738.1/bluer_ugv-6.738.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.738.1.tar.gz/bluer_ugv-6.738.1/bluer_ugv/sparrow/parts.py
@@ -0,0 +1,7 @@
+dict_of_parts = {
+    "DC-gearboxed-motor-12V-120RPM": "proposed, not used.",
+    "dc-motor-12-VDC-45W": "type 2, 2 x right + 2 x left",
+    "ultrasonic-sensor": "4 x",
+    "resistor": "4 x 1 kΩ + 4 x 2 kΩ + 4 x 330-470 Ω",
+    "LED": "4 x blue",
+}
