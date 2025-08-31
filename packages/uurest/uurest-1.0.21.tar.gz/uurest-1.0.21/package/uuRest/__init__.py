e345afde279eaafd3531d45bfaaa5a3e31318082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 14:03:51 +0000

    Add package uurest/1.0.21/uurest-1.0.21.tar.gz

---
+++ packages/uurest/uurest-1.0.21.tar.gz/uurest-1.0.21/package/uuRest/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from .uufetch import call, fetch, fetch_setup, translate_fetch, smart_translate_fetch, null
+
+__all__ = (
+    "call",
+    "fetch",
+    "fetch_setup",
+    "translate_fetch",
+    "smart_translate_fetch",
+    "null"
+)
+
+
