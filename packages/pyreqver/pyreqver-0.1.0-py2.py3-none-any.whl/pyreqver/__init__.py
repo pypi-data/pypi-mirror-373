097b8ac01ce02e478e253dbbd542cd961e135b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:40 +0000

    Add package pyreqver/0.1.0/pyreqver-0.1.0-py2.py3-none-any.whl

---
+++ packages/pyreqver/pyreqver-0.1.0-py2.py3-none-any.whl/pyreqver/__init__.py
@@ -0,0 +1,6 @@
+"""PyReqVer - Python Requirements Version Checker
+
+PyReqVer is a command-line tool that helps you find Python versions that support all libraries in your requirements.txt file.
+"""
+
+__version__ = "0.1.0"
