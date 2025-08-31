87350cbd03e0f383f23ef340161c05b7bfd8f25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:41 +0000

    Add package kairoAI/1.0.0/kairoai-1.0.0-py3-none-any.whl

---
+++ packages/kairoAI/kairoai-1.0.0-py3-none-any.whl/kairo/utils.py
@@ -0,0 +1,8 @@
+def print_logo(console):
+    logo = """
+  _  __      _        
+ | |/ /__ _ (_)__ ___ 
+ |   / _ `// / -_|_-<
+ |_/_\_,_//_/\__/___/
+    """
+    console.print(f"[bold magenta]{logo}[/bold magenta]")
