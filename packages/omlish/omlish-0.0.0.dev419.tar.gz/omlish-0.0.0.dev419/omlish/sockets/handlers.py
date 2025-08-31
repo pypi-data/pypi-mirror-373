8e05c3062c3458a1335744129135bee524d10589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 14:03:57 +0000

    Add package omlish/0.0.0.dev419/omlish-0.0.0.dev419.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev419.tar.gz/omlish-0.0.0.dev419/omlish/sockets/handlers.py
@@ -0,0 +1,23 @@
+# ruff: noqa: UP006 UP007 UP045
+# @omlish-lite
+import abc
+import typing as ta
+
+from .addresses import SocketAddress
+from .io import SocketIoPair  # noqa
+
+
+SocketHandler = ta.Callable[[SocketAddress, 'SocketIoPair'], None]  # ta.TypeAlias
+
+
+##
+
+
+class SocketHandler_(abc.ABC):  # noqa
+    @abc.abstractmethod
+    def __call__(self, addr: SocketAddress, f: SocketIoPair) -> None:
+        raise NotImplementedError
+
+
+class SocketHandlerClose(Exception):  # noqa
+    pass
