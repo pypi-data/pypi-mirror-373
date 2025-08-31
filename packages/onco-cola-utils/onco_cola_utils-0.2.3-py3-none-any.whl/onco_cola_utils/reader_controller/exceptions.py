9f34a7cc8f3fc8195eb3ec70156569df209404b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:09 +0000

    Add package onco-cola-utils/0.2.3/onco_cola_utils-0.2.3-py3-none-any.whl

---
+++ packages/onco-cola-utils/onco_cola_utils-0.2.3-py3-none-any.whl/onco_cola_utils/reader_controller/exceptions.py
@@ -0,0 +1,16 @@
+"""КАСТОМНЫЕ ОШИБКИ"""
+
+
+class ContentLengthError(Exception):
+    """Нет содержимого"""
+    pass
+
+
+class WrongSheetListError(Exception):
+    """Неверный лист документа"""
+    pass
+
+
+class LocalIDError(Exception):
+    """Отсутствует local_id"""
+    pass
