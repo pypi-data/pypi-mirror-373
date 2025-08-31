d322e72aca1a960d499a2da09d857925873a2c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:09 +0000

    Add package yyinput/0.1.2/yyinput-0.1.2-py3-none-any.whl

---
+++ packages/yyinput/yyinput-0.1.2-py3-none-any.whl/yyinput/__init__.py
@@ -0,0 +1,26 @@
+
+
+import fire 
+from .dbasr import DBASR
+
+
+class ENTRY(object):
+    """主入口类"""
+    
+    def __init__(self):
+        # 使用装饰器创建VPC访问控制代理
+        self.dbasr  = DBASR()
+
+    
+
+
+
+
+
+
+
+
+def main() -> None:
+    """Main function to run the CLI."""
+    fire.Fire(ENTRY)
+
