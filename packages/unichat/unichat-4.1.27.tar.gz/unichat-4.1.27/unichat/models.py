7d6c7d73b230aaf020ec315f732d348853ec880f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:13 +0000

    Add package unichat/4.1.27/unichat-4.1.27.tar.gz

---
+++ packages/unichat/unichat-4.1.27.tar.gz/unichat-4.1.27/unichat/models.py
@@ -0,0 +1,128 @@
+MODELS_LIST =  {
+    "anthropic_models": [
+        "claude-3-5-haiku-latest",
+        "claude-3-5-sonnet-latest",
+        "claude-3-7-sonnet-latest",
+        "claude-sonnet-4-0",
+        "claude-opus-4-0",
+        "claude-opus-4-1"
+    ],
+    "mistral_models": [
+        "mistral-small-latest",
+        "mistral-medium-latest",
+        "mistral-large-latest",
+        "codestral-latest",
+        "pixtral-large-latest",
+        "magistral-medium-latest"
+    ],
+    "openai_models": [
+        "gpt-4.1-nano",
+        "gpt-4.1-mini",
+        "gpt-4o-mini",
+        "gpt-4o",
+        "gpt-4o-2024-11-20",
+        "chatgpt-4o-latest",
+        "gpt-4.1",
+        "o3-mini",
+        "o3",
+        "o3-pro",
+        "o4-mini",
+        "gpt-5-nano",
+        "gpt-5-mini",
+        "gpt-5",
+        "gpt-5-chat-latest"
+    ],
+    "grok_models": [
+        "grok-code-fast",
+        "grok-4-latest",
+        "grok-3-latest",
+        "grok-3-mini-latest"
+    ],
+    "gemini_models": [
+        "gemini-2.5-flash",
+        "gemini-2.5-pro"
+    ],
+    "deepseek_models": [
+        "deepseek-chat",
+        "deepseek-reasoner"
+    ],
+    "alibaba_models": [
+        "qwen-turbo-latest",
+        "qwen-flash",
+        "qwen3-coder-plus",
+        "qwen3-coder-flash",
+        "qwen-plus-latest",
+        "qwen-max-latest",
+        "qwq-plus",
+        "qvq-max-latest",
+        "qwen-vl-max-latest",
+        "qwen-vl-plus-latest"
+    ],
+    "inception_models": [
+        "mercury",
+        "mercury-coder"
+    ],
+    "moonshot_models": [
+        "kimi-latest",
+        "kimi-k2-0711-preview",
+        "kimi-k2-turbo-preview",
+        "kimi-thinking-preview"
+    ],
+    "openrouter_models": [
+        "openrouter/horizon-beta"
+    ]
+}
+MODELS_MAX_TOKEN =  {
+    "gpt-4.1-nano": 1047576,
+    "gpt-4.1-mini": 1047576,
+    "gpt-4o-mini": 200000,
+    "gpt-4o": 128000,
+    "gpt-4o-2024-11-20": 128000,
+    "chatgpt-4o-latest": 128000,
+    "gpt-4.1": 1047576,
+    "o3-mini": 200000,
+    "o3": 200000,
+    "o3-pro": 200000,
+    "o4-mini": 200000,
+    "gpt-5-nano": 400000,
+    "gpt-5-mini": 400000,
+    "gpt-5": 400000,
+    "gpt-5-chat-latest": 400000,
+    "mistral-small-latest": 128000,
+    "mistral-medium-latest": 128000,
+    "mistral-large-latest": 128000,
+    "codestral-latest": 256000,
+    "pixtral-large-latest": 128000,
+    "magistral-medium-latest": 40000,
+    "claude-3-5-haiku-latest": 8192,
+    "claude-3-5-sonnet-latest": 8192,
+    "claude-3-7-sonnet-latest": 64000,
+    "claude-sonnet-4-0": 64000,
+    "claude-opus-4-0": 32000,
+    "claude-opus-4-1": 32000,
+    "grok-code-fast": 256000,
+    "grok-4-latest": 256000,
+    "grok-3-latest": 131072,
+    "grok-3-mini-latest": 131072,
+    "gemini-2.5-flash": 1048576,
+    "gemini-2.5-pro": 1048576,
+    "deepseek-chat": 64000,
+    "deepseek-reasoner": 64000,
+    "qwen-turbo-latest": 1000000,
+    "qwen-flash": 1000000,
+    "qwen3-coder-plus": 1000000,
+    "qwen3-coder-flash": 1000000,
+    "qwen-plus-latest": 1000000,
+    "qwen-max-latest": 32768,
+    "qwq-plus": 131072,
+    "qvq-max-latest": 131072,
+    "qwen-vl-max-latest": 131072,
+    "qwen-vl-plus-latest": 131072,
+    "mercury": 128000,
+    "mercury-coder": 128000,
+    "kimi-latest": 131072,
+    "kimi-k2-0711-preview": 131072,
+    "kimi-k2-turbo-preview": 131072,
+    "kimi-thinking-preview": 131072,
+    "openrouter/horizon-beta": 256000
+}
