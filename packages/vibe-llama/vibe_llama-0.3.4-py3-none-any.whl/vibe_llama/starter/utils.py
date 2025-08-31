c9d9d31358e75f3290f3c802a841e5ab3c47509b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:03:53 +0000

    Add package vibe-llama/0.3.4/vibe_llama-0.3.4-py3-none-any.whl

---
+++ packages/vibe-llama/vibe_llama-0.3.4-py3-none-any.whl/vibe_llama/starter/utils.py
@@ -0,0 +1,47 @@
+import os
+import httpx
+import asyncio
+
+from pathlib import Path
+from typing import Optional
+
+
+def write_file(
+    file_path: str, content: str, overwrite_file: bool, service_url: str
+) -> None:
+    directory = os.path.dirname(file_path)
+    if not Path(directory).is_dir():
+        os.makedirs(directory, exist_ok=True)
+    if not overwrite_file:
+        if Path(file_path).is_file():
+            with open(file_path) as f:
+                file_content = f.read()
+            content = file_content + "\n" + content
+    if file_path.startswith(".cursor"):
+        frontmatter = f"""---
+description: Instructions from {service_url} for Cursor coding agent
+alwaysApply: false
+---
+
+"""
+        content = frontmatter + "\n" + content
+    with open(file_path, "w") as w:
+        w.write(content)
+    return None
+
+
+async def get_instructions(
+    instructions_url: str, max_retries: int = 10, retry_interval: float = 0.5
+) -> Optional[str]:
+    async with httpx.AsyncClient() as client:
+        retries = 0
+        while True:
+            if retries < max_retries:
+                response = await client.get(instructions_url)
+                if response.status_code == 200:
+                    return response.text
+                else:
+                    retries += 1
+                    await asyncio.sleep(retry_interval)
+            else:
+                return None
