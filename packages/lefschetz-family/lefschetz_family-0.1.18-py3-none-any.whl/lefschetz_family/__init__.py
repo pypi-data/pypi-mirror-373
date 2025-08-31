4b648b657b7ffacca32eebeaf529a34008c2bd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:20:20 +0000

    Add package lefschetz-family/0.1.18/lefschetz_family-0.1.18-py3-none-any.whl

---
+++ packages/lefschetz-family/lefschetz_family-0.1.18-py3-none-any.whl/lefschetz_family/__init__.py
@@ -0,0 +1,7 @@
+
+from .hypersurface import Hypersurface
+from .ellipticSurface import EllipticSurface
+from .doubleCover import DoubleCover
+from .fibreProduct import FibreProduct
+
+# from . import config
