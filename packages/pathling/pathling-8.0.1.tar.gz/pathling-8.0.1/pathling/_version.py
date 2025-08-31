76aa2f17ac51b0d0a7553fda9d10739f22ad0405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:41 +0000

    Add package pathling/8.0.1/pathling-8.0.1.tar.gz

---
+++ packages/pathling/pathling-8.0.1.tar.gz/pathling-8.0.1/pathling/_version.py
@@ -0,0 +1,9 @@
+#
+# Auto generated from POM project version.
+# Please do not modify.
+#
+__version__="8.0.1"
+__java_version__="8.0.1"
+__scala_version__="2.12"
+__delta_version__="3.3.2"
+__hadoop_version__="3.3.4"
