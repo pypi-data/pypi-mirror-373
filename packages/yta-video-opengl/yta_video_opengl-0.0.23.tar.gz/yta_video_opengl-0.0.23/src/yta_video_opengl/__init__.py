9d2989c7dab32804b95303b9ecab10988a94adb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:00 +0000

    Add package yta-video-opengl/0.0.23/yta_video_opengl-0.0.23.tar.gz

---
+++ packages/yta-video-opengl/yta_video_opengl-0.0.23.tar.gz/yta_video_opengl-0.0.23/src/yta_video_opengl/__init__.py
@@ -0,0 +1,12 @@
+"""
+Module to include video handling with OpenGL.
+"""
+def main():
+    # from yta_video_opengl.tests import video_modified_stored
+
+    # video_modified_stored()
+    print('main')
+
+
+if __name__ == '__main__':
+    main()
