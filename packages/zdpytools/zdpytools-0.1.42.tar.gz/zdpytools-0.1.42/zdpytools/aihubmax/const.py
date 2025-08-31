2af2d6c1c97a9f224e4d0c4da5f124cdce9e9b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:04:14 +0000

    Add package zdpytools/0.1.42/zdpytools-0.1.42.tar.gz

---
+++ packages/zdpytools/zdpytools-0.1.42.tar.gz/zdpytools-0.1.42/zdpytools/aihubmax/const.py
@@ -0,0 +1,15 @@
+"""
+AIHubMax API常量定义
+"""
+
+# API基础URL
+AIHUBMAX_API_HOST = "https://aihubmax.com"
+
+# API路径
+# 临时文件相关
+TMPFILE_UPLOAD_URI = "/ability/tmpfile/upload"
+
+# 文件上传专用配置
+TMPFILE_UPLOAD_HOST = "https://tfs.zooai.cc"
+TMPFILE_UPLOAD_PATH = "/api/upload"
+UPLOAD_API_KEY = "58tyo1ido8uwofny"
