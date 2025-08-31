3a15755f9e0f5363da8225100b8615f368c5d03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:49 +0000

    Add package trading-core/0.1.6/trading_core-0.1.6-py3-none-any.whl

---
+++ packages/trading-core/trading_core-0.1.6-py3-none-any.whl/trading_core/models/binance/data/BinanceFutureCOINMExchangeInfoSymbol.py
@@ -0,0 +1,110 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from sqlalchemy import Column, String, Integer, Float, JSON
+from .base import ExchangeInfoSymbol
+
+class BinanceFutureCOINMExchangeInfoSymbol(BaseModel):
+    symbol: str
+    pair: str
+    contractType: str
+    deliveryDate: int
+    onboardDate: int
+    contractStatus: str
+    contractSize: int
+    quoteAsset: str
+    baseAsset: str
+    marginAsset: str
+    pricePrecision: int
+    quantityPrecision: int
+    baseAssetPrecision: int
+    quotePrecision: int
+    triggerProtect: str
+    underlyingType: str
+    underlyingSubType: List[str]
+    filters: List[dict]
+    OrderType: List[str]
+    timeInForce: List[str]
+    liquidationFee: str
+    marketTakeBound: str
+
+class BinanceFutureCOINMExchangeInfoSymbolTable(ExchangeInfoSymbol):
+    __tablename__ = "binance_future_coinm_exchange_info_symbols"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    symbol = Column(String, index=True, unique=True)
+    pair = Column(String, index=True)
+    contractType = Column(String)
+    deliveryDate = Column(Integer)
+    onboardDate = Column(Integer)
+    contractStatus = Column(String)
+    contractSize = Column(Integer)
+    quoteAsset = Column(String)
+    baseAsset = Column(String)
+    marginAsset = Column(String)
+    pricePrecision = Column(Integer)
+    quantityPrecision = Column(Integer)
+    baseAssetPrecision = Column(Integer)
+    quotePrecision = Column(Integer)
+    triggerProtect = Column(String)
+    underlyingType = Column(String)
+    underlyingSubType = Column(JSON)
+    filters = Column(JSON)
+    OrderType = Column(JSON)
+    timeInForce = Column(JSON)
+    liquidationFee = Column(String)
+    marketTakeBound = Column(String)
+
+class BinanceFutureCOINMExchangeInfoSymbolMapper:
+    @staticmethod
+    def from_raw(raw: dict) -> BinanceFutureCOINMExchangeInfoSymbol:
+        return BinanceFutureCOINMExchangeInfoSymbol(
+            symbol=raw["symbol"],
+            pair=raw["pair"],
+            contractType=raw["contractType"],
+            deliveryDate=raw["deliveryDate"],
+            onboardDate=raw["onboardDate"],
+            contractStatus=raw["contractStatus"],
+            contractSize=raw["contractSize"],
+            quoteAsset=raw["quoteAsset"],
+            baseAsset=raw["baseAsset"],
+            marginAsset=raw["marginAsset"],
+            pricePrecision=raw["pricePrecision"],
+            quantityPrecision=raw["quantityPrecision"],
+            baseAssetPrecision=raw["baseAssetPrecision"],
+            quotePrecision=raw["quotePrecision"],
+            triggerProtect=raw.get("triggerProtect", "0"),
+            underlyingType=raw["underlyingType"],
+            underlyingSubType=raw.get("underlyingSubType", []),
+            filters=raw.get("filters", []),
+            OrderType=raw.get("OrderType", []),
+            timeInForce=raw.get("timeInForce", []),
+            liquidationFee=raw.get("liquidationFee", "0"),
+            marketTakeBound=raw.get("marketTakeBound", "0"),
+        )
+
+    @staticmethod
+    def to_table(event: BinanceFutureCOINMExchangeInfoSymbol) -> BinanceFutureCOINMExchangeInfoSymbolTable:
+        return BinanceFutureCOINMExchangeInfoSymbolTable(
+            symbol=event.symbol,
+            pair=event.pair,
+            contractType=event.contractType,
+            deliveryDate=event.deliveryDate,
+            onboardDate=event.onboardDate,
+            contractStatus=event.contractStatus,
+            contractSize=event.contractSize,
+            quoteAsset=event.quoteAsset,
+            baseAsset=event.baseAsset,
+            marginAsset=event.marginAsset,
+            pricePrecision=event.pricePrecision,
+            quantityPrecision=event.quantityPrecision,
+            baseAssetPrecision=event.baseAssetPrecision,
+            quotePrecision=event.quotePrecision,
+            triggerProtect=event.triggerProtect,
+            underlyingType=event.underlyingType,
+            underlyingSubType=event.underlyingSubType,
+            filters=event.filters,
+            OrderType=event.OrderType,
+            timeInForce=event.timeInForce,
+            liquidationFee=event.liquidationFee,
+            marketTakeBound=event.marketTakeBound,
+        )
