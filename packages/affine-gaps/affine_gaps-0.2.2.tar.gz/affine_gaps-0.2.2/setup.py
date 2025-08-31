0566a27f3b146da0e53249cbf2c616130a91fa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 14:03:48 +0000

    Add package affine-gaps/0.2.2/affine_gaps-0.2.2.tar.gz

---
+++ packages/affine-gaps/affine_gaps-0.2.2.tar.gz/affine_gaps-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="affine-gaps",
+    version="0.2.2",
+    author="Ash Vardanian",
+    author_email="1983160+ashvardanian@users.noreply.github.com",
+    description="Numba-accelerated Python implementation of affine gap penalty extensions for Needleman-Wunsch and Smith-Waterman algorithms",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashvardanian/affine-gaps",
+    py_modules=["affine_gaps"],
+    install_requires=["numba", "numpy", "colorama"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    extras_require={"dev": ["biopython", "stringzilla", "pytest", "pytest-repeat"]},
+    entry_points={
+        "console_scripts": [
+            "affine-gaps=affine_gaps:main",
+        ],
+    },
+)
