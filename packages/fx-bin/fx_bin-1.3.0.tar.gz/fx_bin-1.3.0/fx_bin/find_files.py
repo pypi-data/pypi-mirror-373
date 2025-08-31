3b7856cfecee46bb309a39e09b78518cf736228d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:41 +0000

    Add package fx-bin/1.3.0/fx_bin-1.3.0.tar.gz

---
+++ packages/fx-bin/fx_bin-1.3.0.tar.gz/fx_bin-1.3.0/fx_bin/find_files.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import click
+
+
+def find_files(keyword: str) -> None:
+    cwd = os.getcwd()
+    for root, dirs, files in os.walk(cwd):
+        for item in dirs + files:
+            if keyword in item:
+                click.echo(os.path.join(root, item))
+
+
+@click.command()
+@click.argument('keyword')
+def main(keyword: str) -> int:
+    if not keyword or keyword.strip() == "":
+        click.echo(
+            "Please type text to search. For example: fx ff bar", err=True
+        )
+        click.echo("Usage: fx ff KEYWORD", err=True)
+        return 1
+    find_files(keyword)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
