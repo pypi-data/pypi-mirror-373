64380abf4b28a5aa80de4f4c7f3d5ec593b233ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:39 +0000

    Add package tocount/0.1/tocount-0.1.tar.gz

---
+++ packages/tocount/tocount-0.1.tar.gz/tocount-0.1/tocount/params.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""tocount parameters and constants."""
+
+TOCOUNT_VERSION = "0.1"
+
+INVALID_TEXT_ESTIMATOR_MESSAGE = "Invalid value. `estimator` must be an instance of TextEstimator enum."
+INVALID_TEXT_MESSAGE = "Invalid value. `text` must be a string."
