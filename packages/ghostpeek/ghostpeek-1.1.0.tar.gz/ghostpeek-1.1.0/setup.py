c6f87e5cee955c21b3c8266ba22e6f32ceb48f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 05:04:09 +0000

    Add package ghostpeek/1.1.0/ghostpeek-1.1.0.tar.gz

---
+++ packages/ghostpeek/ghostpeek-1.1.0.tar.gz/ghostpeek-1.1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Fallback setup.py for older pip versions
+Most configuration is in pyproject.toml
+"""
+
+from setuptools import setup
+
+# Fallback for older pip versions that don't support pyproject.toml
+setup()
