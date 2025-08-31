f39e11dd989572f4238697e9e13d3f3510d96940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:21:02 +0000

    Add package tptt/0.11.7/tptt-0.11.7.tar.gz

---
+++ packages/tptt/tptt-0.11.7.tar.gz/tptt-0.11.7/src/tptt/__init__.py
@@ -0,0 +1,26 @@
+"""
+This module implements the TPTT model with linear attention (LiZA) and LoRA support.
+"""
+
+from .configuration_tptt import (TpttConfig, generate_model_card,
+                                 parse_mode_name)
+from .modeling_tptt import (LCache, LinearAttention, LinearAttentionOp,
+                            LiZAttention, TpttModel, get_tptt_model,
+                            load_tptt_safetensors, save_tptt_safetensors)
+from .train_tptt import LiZACallback, SaveBestModelCallback
+
+__all__ = [
+    "TpttConfig",
+    "TpttModel",
+    "get_tptt_model",
+    "LiZACallback",
+    "SaveBestModelCallback",
+    "LCache",
+    "LinearAttentionOp",
+    "LiZAttention",
+    "generate_model_card",
+    "LinearAttention",
+    "parse_mode_name",
+    "load_tptt_safetensors",
+    "save_tptt_safetensors",
+]
