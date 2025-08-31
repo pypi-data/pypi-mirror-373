a7c4101eba96fa929a89235353b4d53cec5aa58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 04:04:33 +0000

    Add package x-make-markdown-x/1.0.4/x_make_markdown_x-1.0.4.tar.gz

---
+++ packages/x-make-markdown-x/x_make_markdown_x-1.0.4.tar.gz/x_make_markdown_x-1.0.4/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+setup(
+name="x_make_markdown_x",
+version="1.0.4",
+author="Roy GM",
+author_email="eye4357@outlook.com",
+description="Makes markdown.",
+long_description=open(os.path.join(r'c:/x_main_x/x_py_source_x/x_make_markdown_x\docs', "README.md"), encoding="utf-8").read(),
+long_description_content_type="text/markdown",
+url="https://pypi.org/project/x_make_markdown_x/",
+packages=["x_make_markdown_x"],
+install_requires=['markdown', 'pdfkit'],
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
