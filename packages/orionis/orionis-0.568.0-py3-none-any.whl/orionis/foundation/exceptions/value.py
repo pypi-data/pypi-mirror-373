39436827096639843c922f846c7f5fd2fb27702a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:20:32 +0000

    Add package orionis/0.568.0/orionis-0.568.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.568.0-py3-none-any.whl/orionis/foundation/exceptions/value.py
@@ -0,0 +1,19 @@
+class OrionisValueError(ValueError):
+
+    def __init__(self, msg: str):
+        """
+        Parameters
+        ----------
+        msg : str
+            Descriptive error message explaining the cause of the exception.
+        """
+        super().__init__(msg)
+
+    def __str__(self) -> str:
+        """
+        Returns
+        -------
+        str
+            Formatted string describing the exception.
+        """
+        return str(self.args[0])
