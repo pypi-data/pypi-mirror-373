52aa9a837856f3ff0a6eccd3f29faaa20674cea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 03:19:35 +0000

    Add package spreadlinks/0.5.0/spreadlinks-0.5.0-py3-none-any.whl

---
+++ packages/spreadlinks/spreadlinks-0.5.0-py3-none-any.whl/spreadlinks/models.py
@@ -0,0 +1,22 @@
+from django.core.cache import cache
+from django.http import Http404
+
+from spreadlinks.linklibrarylib import LibrarySet
+
+
+def get_library_set(root_dir):
+    """Find the libraries in the specified file-system directoiry."""
+    key = "library_set"
+    result = cache.get(key)
+    if not result:
+        result = LibrarySet(root_dir)
+        cache.set(key, result)
+    return result
+
+
+def get_library_or_404(root_dir, library_name):
+    """Given a parent direcgtory and library name, find the library or throw an HTTP exception."""
+    try:
+        return get_library_set(root_dir)[library_name]
+    except KeyError:
+        raise Http404()
---
+++ packages/spreadlinks/spreadlinks-0.5.0-py3-none-any.whl/spreadlinks/urls.py
@@ -0,0 +1,33 @@
+# Map URLs to functions for the spreadlinks module.
+
+from django.conf import settings
+from django.urls import include, path, re_path
+
+import spreadlinks.views
+
+kwargs = {"root_dir": settings.SPREADLINKS_DIR}
+library_detail_paths = [
+    path("", spreadlinks.views.library_detail, kwargs, name="library_detail"),
+    path(
+        "page<int:page>",
+        spreadlinks.views.library_detail,
+        kwargs,
+        name="library_detail",
+    ),
+]
+
+urlpatterns = [
+    path("", spreadlinks.views.library_list, kwargs, name="library_list"),
+    path(
+        "<library_name>/",
+        include(
+            [
+                path("", include(library_detail_paths)),
+                re_path(
+                    r"^tags/(?P<urlencoded_keywords>[a-z_0-9+:-]+)",
+                    include(library_detail_paths),
+                ),
+            ]
+        ),
+    ),
+]
