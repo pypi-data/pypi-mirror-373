cd6749a549098a28f529fb5a6d64d6868b5b3023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:57 +0000

    Add package writer-pdf/0.2.0/writer_pdf-0.2.0.tar.gz

---
+++ packages/writer-pdf/writer_pdf-0.2.0.tar.gz/writer_pdf-0.2.0/writer_pdf/__init__.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# @File  : __init__.py.py
+# @Author: ronin.G
+# @Date  : 2025/08/21/16:28
+from .writer_pdf import SimplePDFDocument
+from .writer_pdf import AllTool
+__version__ = "0.2.0"
