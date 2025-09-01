ed7797a4dbd5ae4f54ca68c7a878192428ca2e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:47 +0000

    Add package aiwaf/0.1.9.3.1/aiwaf-0.1.9.3.1.tar.gz

---
+++ packages/aiwaf/aiwaf-0.1.9.3.1.tar.gz/aiwaf-0.1.9.3.1/aiwaf/__init__.py
@@ -0,0 +1,6 @@
+default_app_config = "aiwaf.apps.AiwafConfig"
+
+__version__ = "0.1.9.3.1"
+
+# Note: Middleware classes are available from aiwaf.middleware
+# Import them only when needed to avoid circular imports during Django app loading
