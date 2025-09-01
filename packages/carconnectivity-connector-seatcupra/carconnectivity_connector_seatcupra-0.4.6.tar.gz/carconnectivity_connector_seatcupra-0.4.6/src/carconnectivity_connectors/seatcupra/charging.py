4e16540264221ba9012adf789b87014ab9ea0c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:44 +0000

    Add package carconnectivity-connector-seatcupra/0.4.6/carconnectivity_connector_seatcupra-0.4.6.tar.gz

---
+++ packages/carconnectivity-connector-seatcupra/carconnectivity_connector_seatcupra-0.4.6.tar.gz/carconnectivity_connector_seatcupra-0.4.6/src/carconnectivity_connectors/seatcupra/charging.py
@@ -0,0 +1,89 @@
+"""
+Module for charging for Seat/Cupra vehicles.
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from enum import Enum
+
+from carconnectivity.charging import Charging
+from carconnectivity.vehicle import ElectricVehicle
+
+if TYPE_CHECKING:
+    from typing import Optional, Dict
+
+    from carconnectivity.objects import GenericObject
+
+
+class SeatCupraCharging(Charging):  # pylint: disable=too-many-instance-attributes
+    """
+    SeatCupraCharging class for handling SeatCupra vehicle charging information.
+
+    This class extends the Charging class and includes an enumeration of various
+    charging states specific to SeatCupra vehicles.
+    """
+    def __init__(self, vehicle: ElectricVehicle | None = None, origin: Optional[Charging] = None) -> None:
+        if origin is not None:
+            super().__init__(vehicle=vehicle, origin=origin)
+            self.settings = SeatCupraCharging.Settings(parent=self, origin=origin.settings)
+        else:
+            super().__init__(vehicle=vehicle)
+            self.settings = SeatCupraCharging.Settings(parent=self, origin=self.settings)
+
+    class Settings(Charging.Settings):
+        """
+        This class represents the settings for car volkswagen car charging.
+        """
+        def __init__(self, parent: Optional[GenericObject] = None, origin: Optional[Charging.Settings] = None) -> None:
+            if origin is not None:
+                super().__init__(parent=parent, origin=origin)
+            else:
+                super().__init__(parent=parent)
+            self.max_current_in_ampere: Optional[bool] = None
+
+    class SeatCupraChargingState(Enum,):
+        """
+        Enum representing the various charging states for a SeatCupra vehicle.
+        """
+        OFF = 'off'
+        READY_FOR_CHARGING = 'readyForCharging'
+        NOT_READY_FOR_CHARGING = 'notReadyForCharging'
+        CONSERVATION = 'conservation'
+        CHARGE_PURPOSE_REACHED_NOT_CONSERVATION_CHARGING = 'chargePurposeReachedAndNotConservationCharging'
+        CHARGE_PURPOSE_REACHED_CONSERVATION = 'chargePurposeReachedAndConservation'
+        CHARGING = 'charging'
+        ERROR = 'error'
+        UNSUPPORTED = 'unsupported'
+        DISCHARGING = 'discharging'
+        UNKNOWN = 'unknown charging state'
+
+    class SeatCupraChargeMode(Enum,):
+        """
+        Enum class representing different SeatCupra charge modes.
+        """
+        MANUAL = 'manual'
+        INVALID = 'invalid'
+        OFF = 'off'
+        TIMER = 'timer'
+        ONLY_OWN_CURRENT = 'onlyOwnCurrent'
+        PREFERRED_CHARGING_TIMES = 'preferredChargingTimes'
+        TIMER_CHARGING_WITH_CLIMATISATION = 'timerChargingWithClimatisation'
+        HOME_STORAGE_CHARGING = 'homeStorageCharging'
+        IMMEDIATE_DISCHARGING = 'immediateDischarging'
+        UNKNOWN = 'unknown charge mode'
+
+
+# Mapping of Cupra charging states to generic charging states
+mapping_seatcupra_charging_state: Dict[SeatCupraCharging.SeatCupraChargingState, Charging.ChargingState] = {
+    SeatCupraCharging.SeatCupraChargingState.OFF: Charging.ChargingState.OFF,
+    SeatCupraCharging.SeatCupraChargingState.NOT_READY_FOR_CHARGING: Charging.ChargingState.OFF,
+    SeatCupraCharging.SeatCupraChargingState.READY_FOR_CHARGING: Charging.ChargingState.READY_FOR_CHARGING,
+    SeatCupraCharging.SeatCupraChargingState.CONSERVATION: Charging.ChargingState.CONSERVATION,
+    SeatCupraCharging.SeatCupraChargingState.CHARGE_PURPOSE_REACHED_NOT_CONSERVATION_CHARGING: Charging.ChargingState.READY_FOR_CHARGING,
+    SeatCupraCharging.SeatCupraChargingState.CHARGE_PURPOSE_REACHED_CONSERVATION: Charging.ChargingState.CONSERVATION,
+    SeatCupraCharging.SeatCupraChargingState.CHARGING: Charging.ChargingState.CHARGING,
+    SeatCupraCharging.SeatCupraChargingState.ERROR: Charging.ChargingState.ERROR,
+    SeatCupraCharging.SeatCupraChargingState.UNSUPPORTED: Charging.ChargingState.UNSUPPORTED,
+    SeatCupraCharging.SeatCupraChargingState.DISCHARGING: Charging.ChargingState.DISCHARGING,
+    SeatCupraCharging.SeatCupraChargingState.UNKNOWN: Charging.ChargingState.UNKNOWN
+}
---
+++ packages/carconnectivity-connector-seatcupra/carconnectivity_connector_seatcupra-0.4.6.tar.gz/carconnectivity_connector_seatcupra-0.4.6/src/carconnectivity_connectors/seatcupra/vehicle.py
@@ -0,0 +1,91 @@
+"""Module for vehicle classes."""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from carconnectivity.vehicle import GenericVehicle, ElectricVehicle, CombustionVehicle, HybridVehicle
+
+from carconnectivity_connectors.seatcupra.capability import Capabilities
+from carconnectivity_connectors.seatcupra.climatization import SeatCupraClimatization
+from carconnectivity_connectors.seatcupra.charging import SeatCupraCharging
+
+SUPPORT_IMAGES = False
+try:
+    from PIL import Image
+    SUPPORT_IMAGES = True
+except ImportError:
+    pass
+
+if TYPE_CHECKING:
+    from typing import Optional, Dict
+    from carconnectivity.garage import Garage
+    from carconnectivity.charging import Charging
+
+    from carconnectivity_connectors.base.connector import BaseConnector
+
+
+class SeatCupraVehicle(GenericVehicle):  # pylint: disable=too-many-instance-attributes
+    """
+    A class to represent a generic Seat/Cupra vehicle.
+
+    Attributes:
+    -----------
+    vin : StringAttribute
+        The vehicle identification number (VIN) of the vehicle.
+    license_plate : StringAttribute
+        The license plate of the vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[SeatCupraVehicle] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin)
+            self.capabilities: Capabilities = origin.capabilities
+            self.capabilities.parent = self
+            if SUPPORT_IMAGES:
+                self._car_images = origin._car_images
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector)
+            self.climatization = SeatCupraClimatization(vehicle=self, origin=self.climatization)
+            self.capabilities: Capabilities = Capabilities(vehicle=self)
+            if SUPPORT_IMAGES:
+                self._car_images: Dict[str, Image.Image] = {}
+
+
+class SeatCupraElectricVehicle(ElectricVehicle, SeatCupraVehicle):
+    """
+    Represents a Seat/Cupra electric vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[SeatCupraVehicle] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin)
+            if isinstance(origin, ElectricVehicle):
+                self.charging: Charging = SeatCupraCharging(vehicle=self, origin=origin.charging)
+            else:
+                self.charging: Charging = SeatCupraCharging(vehicle=self, origin=self.charging)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector)
+            self.charging: Charging = SeatCupraCharging(vehicle=self, origin=self.charging)
+
+
+class SeatCupraCombustionVehicle(CombustionVehicle, SeatCupraVehicle):
+    """
+    Represents a Seat/Cupra combustion vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[SeatCupraVehicle] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector)
+
+
+class SeatCupraHybridVehicle(HybridVehicle, SeatCupraVehicle):
+    """
+    Represents a Seat/Cupra hybrid vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[SeatCupraVehicle] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector)
