5221997acabba178c05c94c8446a77fb4a9851da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 04:10:44 +0000

    Add package radiant-wrapper/0.1a10/radiant_wrapper-0.1a10.tar.gz

---
+++ packages/radiant-wrapper/radiant_wrapper-0.1a10.tar.gz/radiant_wrapper-0.1a10/radiant/wrapper/scripts/radiant_p4a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+IMAGE = 'dunderlab/radiant_p4a:latest'
+SOURCE = os.path.abspath(os.curdir)
+APP = os.path.split(SOURCE)[-1].replace('_', '')
+
+DOCKER_CREATE = f'docker create -i --name {APP} --mount type=bind,source={SOURCE},target=/app/env {IMAGE}'
+DOCKER_START = f'docker start {APP}'
+DOCKER_EXEC = f'docker exec {APP}'
+DOCKER_STOP = f'docker stop {APP}'
+# DOCKER_RUN = f'docker run --rm --mount type=bind,source={SOURCE},target=/app/env {IMAGE}'
+
+def main():
+    os.system(DOCKER_CREATE)
+    os.system(DOCKER_START)
+    os.system(f'{DOCKER_EXEC} p4a {" ".join(sys.argv[1:])}')
+    os.system(DOCKER_STOP)
+
+if __name__ == '__main__':
+    main()
