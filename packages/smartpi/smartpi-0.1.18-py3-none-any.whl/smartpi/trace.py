cafe8e96d3e33ca778d7704d75499f8a9b62acfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:05:21 +0000

    Add package smartpi/0.1.18/smartpi-0.1.18-py3-none-any.whl

---
+++ packages/smartpi/smartpi-0.1.18-py3-none-any.whl/smartpi/trace.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import time
+from typing import List, Optional
+from smartpi import base_driver
+
+#循迹卡单通道光值读取 port:连接P端口；正常返回：通道光值数据; 读取错误：None  
+def get_analog(port:bytes, chn:bytes) -> Optional[bytes]:
+    trace_str=[0xA0, 0x21, 0x01, 0x71, 0x00, 0xBE]
+    trace_str[0]=0XA0+port
+    trace_str[4]=20+chn
+    response = base_driver.single_operate_sensor(trace_str)       
+    if response == None:
+        return None
+    else:
+        return response[4]
+        
+#循迹卡设置全部颜色 port:连接P端口；正常返回：通道光值数据; 读取错误：None  
+def set_color(port:bytes, color:bytes) -> Optional[bytes]:
+    trace_str=[0xA0, 0x20, 0x01, 0x71, 0x00, 0xBE]
+    trace_str[0]=0XA0+port
+    trace_str[4]=color
+    response = base_driver.single_operate_sensor(trace_str)       
+    if response == None:
+        return None
+    else:
+        return response[4]
+
+        
+        
