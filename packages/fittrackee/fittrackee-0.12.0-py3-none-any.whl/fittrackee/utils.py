2eecb4f656dd3fa744269abf689973d5fc40fbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 04:10:35 +0000

    Add package fittrackee/0.12.0/fittrackee-0.12.0-py3-none-any.whl

---
+++ packages/fittrackee/fittrackee-0.12.0-py3-none-any.whl/fittrackee/utils.py
@@ -0,0 +1,38 @@
+import time
+from uuid import UUID
+
+import nh3
+import shortuuid
+from sqlalchemy.sql import text
+
+from fittrackee import db
+
+
+def clean(sql: str, days: int) -> int:
+    limit = int(time.time()) - (days * 86400)
+    result = db.session.execute(text(sql), {"limit": limit})
+    db.session.commit()
+    return result.rowcount  # type: ignore
+
+
+def encode_uuid(uuid_value: UUID) -> str:
+    """
+    Return short id string from a UUID
+    """
+    return shortuuid.encode(uuid_value)
+
+
+def decode_short_id(short_id: str) -> UUID:
+    """
+    Return UUID from a short id string
+    """
+    return shortuuid.decode(short_id)
+
+
+def clean_input(input_text: str) -> str:
+    # HTML sanitization
+    return nh3.clean(
+        input_text,
+        tags={"a", "br", "p", "span"},
+        attributes={"a": {"href", "target"}},
+    )
