aee756069f4af881b6225266ed36725c27832579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:06:08 +0000

    Add package rapidstats/0.3.0/rapidstats-0.3.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rapidstats/rapidstats-0.3.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rapidstats/__init__.py
@@ -0,0 +1,6 @@
+from rapidstats import bin, drift, metrics, polars, preprocessing, selection
+
+from ._bootstrap import Bootstrap
+from ._corr import correlation_matrix
+from ._distributions import Random, norm
+from ._general import auc
