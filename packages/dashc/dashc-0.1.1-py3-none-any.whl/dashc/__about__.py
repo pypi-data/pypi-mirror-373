1f2d31d015f079a907a06a14b4b5b49d9a78f326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:05:04 +0000

    Add package dashc/0.1.1/dashc-0.1.1-py3-none-any.whl

---
+++ packages/dashc/dashc-0.1.1-py3-none-any.whl/dashc/__about__.py
@@ -0,0 +1,19 @@
+"""Metadata for dashc."""
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__description__",
+    "__requires_python__",
+    "__readme__",
+    "__keywords__",
+    "__status__",
+]
+
+__title__ = "dashc"
+__version__ = "0.1.1"
+__description__ = "Tool to generate python -c bash as if it were a package format."
+__requires_python__ = ">=3.8"
+__readme__ = "README.md"
+__keywords__ = ["bash", "packaging"]
+__status__ = "4 - Beta"
