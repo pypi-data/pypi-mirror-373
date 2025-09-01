7c18afea9af8b1cb31c84e3243dd1353cbf1a928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:57 +0000

    Add package context-cleaner/0.2.0/context_cleaner-0.2.0-py3-none-any.whl

---
+++ packages/context-cleaner/context_cleaner-0.2.0-py3-none-any.whl/context_cleaner/dashboard/__init__.py
@@ -0,0 +1,11 @@
+"""Dashboard components for Context Cleaner."""
+
+from .web_server import ProductivityDashboard
+from .analytics_dashboard import AnalyticsDashboard
+from .comprehensive_health_dashboard import ComprehensiveHealthDashboard
+
+__all__ = [
+    "ProductivityDashboard",
+    "AnalyticsDashboard",
+    "ComprehensiveHealthDashboard",
+]
