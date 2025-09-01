a6856d7599acb15ed095639d252511f4ec9648be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:05:00 +0000

    Add package tonutils/1.0.0a2/tonutils-1.0.0a2.tar.gz

---
+++ packages/tonutils/tonutils-1.0.0a2.tar.gz/tonutils-1.0.0a2/tonutils/contracts/jetton/__init__.py
@@ -0,0 +1,25 @@
+from .get_methods import (
+    JettonMasterGetMethods,
+    JettonWalletGetMethods,
+)
+from .master import (
+    JettonMasterStandard,
+    JettonMasterStablecoin,
+    JettonMasterStablecoinV2,
+)
+from .wallet import (
+    JettonWalletStandard,
+    JettonWalletStablecoin,
+    JettonWalletStablecoinV2,
+)
+
+__all__ = [
+    "JettonMasterGetMethods",
+    "JettonWalletGetMethods",
+    "JettonMasterStandard",
+    "JettonMasterStablecoin",
+    "JettonMasterStablecoinV2",
+    "JettonWalletStandard",
+    "JettonWalletStablecoin",
+    "JettonWalletStablecoinV2",
+]
