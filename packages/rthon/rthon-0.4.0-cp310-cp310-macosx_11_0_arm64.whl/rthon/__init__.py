f8cb275009ca261f7b08ebaf0514691e276ee8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:18 +0000

    Add package rthon/0.4.0/rthon-0.4.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/rthon/rthon-0.4.0-cp310-cp310-macosx_11_0_arm64.whl/rthon/__init__.py
@@ -0,0 +1,35 @@
+# Import all probability distribution functions
+from .distributions import (
+    # Normal distribution
+    dnorm, pnorm, qnorm, rnorm,
+    # Uniform distribution  
+    dunif, punif, qunif, runif,
+    # Binomial distribution
+    dbinom, pbinom, qbinom, rbinom,
+    # Poisson distribution
+    dpois, ppois, qpois, rpois,
+    # Exponential distribution
+    dexp, pexp, qexp, rexp,
+    # Gamma distribution
+    dgamma, pgamma, qgamma, rgamma
+)
+
+# Import regression functions
+from .regression import lm, lm_info, LinearModel, Formula
+
+__all__ = [
+    # Normal distribution
+    "dnorm", "pnorm", "qnorm", "rnorm",
+    # Uniform distribution
+    "dunif", "punif", "qunif", "runif",
+    # Binomial distribution
+    "dbinom", "pbinom", "qbinom", "rbinom",
+    # Poisson distribution
+    "dpois", "ppois", "qpois", "rpois",
+    # Exponential distribution
+    "dexp", "pexp", "qexp", "rexp",
+    # Gamma distribution
+    "dgamma", "pgamma", "qgamma", "rgamma",
+    # Linear regression
+    "lm", "lm_info", "LinearModel", "Formula"
+]
