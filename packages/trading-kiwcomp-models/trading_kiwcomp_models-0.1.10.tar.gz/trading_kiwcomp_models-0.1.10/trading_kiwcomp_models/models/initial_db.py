1abfa96bce454331f30164abd102c33705b30c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:47 +0000

    Add package trading-kiwcomp-models/0.1.10/trading_kiwcomp_models-0.1.10.tar.gz

---
+++ packages/trading-kiwcomp-models/trading_kiwcomp_models-0.1.10.tar.gz/trading_kiwcomp_models-0.1.10/trading_kiwcomp_models/models/initial_db.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from binance.data.base import ExchangeInfoSymbol
+from binance.data.base import KlineDaily
+from binance.stream.base import StreamKline
+from binance.stream.base import MarkPriceUpdate
+from urllib.parse import quote_plus
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+DB_USER = "kiwadmin"
+DB_PASSWORD = "kiwpass@1689"
+DB_HOST = "pgdev.internal.kiwcomp.com"
+DB_PORT = "5432"
+DB_NAME = "testdb"
+
+# ✅ encode password
+password_encoded = quote_plus(DB_PASSWORD)
+
+# ✅ SQLAlchemy engine
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{password_encoded}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL, echo=True)
+
+# ✅ create tables via SQLAlchemy
+for base in [ExchangeInfoSymbol, StreamKline, MarkPriceUpdate, KlineDaily]:
+    base.metadata.create_all(engine)
