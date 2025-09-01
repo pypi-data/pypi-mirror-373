f3ed38fef948901a1f204ff2eda49664cd12e0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:26 +0000

    Add package anumoy-verma/0.1.0/anumoy_verma-0.1.0.tar.gz

---
+++ packages/anumoy-verma/anumoy_verma-0.1.0.tar.gz/anumoy_verma-0.1.0/test/test_utils.py
@@ -0,0 +1,13 @@
+[build-system]
+requires = ["setuptools", "wheel"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "anumoy-utils"      # or "anu-firstpkg", "anumoy-tools", etc.
+version = "0.10.1"
+description = "My first Python package"
+readme = "README.md"
+license = {text = "MIT"}
+authors = [{name = "Anumoy Verma", email = "your-email@example.com"}]
+requires-python = ">=3.8"
+dependencies = []
