cbbea0d87c7f73f42d136631be40d0b5df5670f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 04:10:14 +0000

    Add package earlysign/0.1.7/earlysign-0.1.7-py3-none-any.whl

---
+++ packages/earlysign/earlysign-0.1.7-py3-none-any.whl/earlysign/api/__init__.py
@@ -0,0 +1,4 @@
+"""
+This layer of submodules provides the application interfaces for various use cases.
+Some of them are intended to provide a compatibility layer for the existing libraries for other languages and programming systems.
+"""
