8aa827f9eef59be39c830fb1e562885e31808d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:05:09 +0000

    Add package bc-detect-secrets/1.5.45/bc_detect_secrets-1.5.45.tar.gz

---
+++ packages/bc-detect-secrets/bc_detect_secrets-1.5.45.tar.gz/bc_detect_secrets-1.5.45/setup.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_version():
+    """Parse package __version__.py to get version."""
+    versionpy = (Path('detect_secrets') / '__version__.py').read_text()
+    return versionpy.split("'")[1]
+
+
+VERSION = get_version()
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='bc-detect-secrets',
+    python_requires='>=3.8',
+    packages=find_packages(exclude=(['test*', 'tmp*'])),
+    version=VERSION,
+    description='Tool for detecting secrets in the codebase',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    author='bridgecrew',
+    author_email='meet@bridgecrew.io',
+    url='https://github.com/bridgecrewio/detect-secrets',
+    keywords=['secret-management', 'pre-commit', 'security', 'entropy-checks'],
+    install_requires=[
+        'pyyaml',
+        'requests',
+        'unidiff',
+    ],
+    include_package_data=True,
+    package_data={
+        'detect_secrets': [
+            'py.typed',
+        ],
+    },
+    extras_require={
+        'word_list': [
+            'pyahocorasick',
+        ],
+        'gibberish': [
+            'gibberish-detector',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'detect-secrets = detect_secrets.main:main',
+            'detect-secrets-hook = detect_secrets.pre_commit_hook:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Typing :: Typed',
+    ],
+)
