3c06b63bfb16cb046425627a68f4026a2396b8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:05:01 +0000

    Add package SOUL-RAJVEER-STT/0.1/soul_rajveer_stt-0.1.tar.gz

---
+++ packages/SOUL-RAJVEER-STT/soul_rajveer_stt-0.1.tar.gz/soul_rajveer_stt-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='SOUL_RAJVEER-STT',
+    version='0.1',
+    author='Rajveer Singh Chouhan',
+    author_email='chouhanrajveer009@gmail.com',
+    description='this is speech to text  package created by  a rajveer '
+)
+packages = find_packages(),
+install_requirments = [
+    'selenium'
+    'webdriver_manger'
+]
