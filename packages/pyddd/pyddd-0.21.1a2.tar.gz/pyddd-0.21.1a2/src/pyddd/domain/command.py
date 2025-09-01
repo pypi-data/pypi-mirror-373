38df52ae7ad4545aa1805f1849cf82f57cca760e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:04:53 +0000

    Add package pyddd/0.21.1a2/pyddd-0.21.1a2.tar.gz

---
+++ packages/pyddd/pyddd-0.21.1a2.tar.gz/pyddd-0.21.1a2/src/pyddd/domain/command.py
@@ -0,0 +1,20 @@
+from pyddd.domain.message import (
+    BaseDomainMessage,
+    BaseDomainMessageMeta,
+)
+from pyddd.domain.abstractions import (
+    ICommand,
+)
+
+
+class _DomainCommandMeta(BaseDomainMessageMeta):
+    def __init__(cls, name, bases, namespace, *, domain: str = None, version: int = 1):
+        super().__init__(name, bases, namespace, domain=domain, version=version)
+        if domain is None and cls.__module__ != __name__:
+            try:
+                _ = cls._domain_name
+            except AttributeError:
+                raise ValueError(f"required set domain name for command '{cls.__module__}.{cls.__name__}'")
+
+
+class DomainCommand(BaseDomainMessage, ICommand, metaclass=_DomainCommandMeta): ...
