5214ac79ee0d27ebb0143acda3f5c75232420d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:51 +0000

    Add package maleo-database/0.0.3/maleo_database-0.0.3.tar.gz

---
+++ packages/maleo-database/maleo_database-0.0.3.tar.gz/maleo_database-0.0.3/src/managers/engines/postgresql.py
@@ -0,0 +1,58 @@
+from sqlalchemy.engine import create_engine, Engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from typing import Literal, Tuple, Union, overload
+from ...config import PostgreSQLDatabaseConfig
+from ...enums import Connection
+
+
+class PostgreSQLEngineManager:
+    def __init__(self, config: PostgreSQLDatabaseConfig) -> None:
+        self.config = config
+        self._async_engine: AsyncEngine = self._init(Connection.ASYNC)
+        self._sync_engine: Engine = self._init(Connection.SYNC)
+
+    @overload
+    def _init(self, connection: Literal[Connection.ASYNC]) -> AsyncEngine: ...
+    @overload
+    def _init(self, connection: Literal[Connection.SYNC]) -> Engine: ...
+    def _init(
+        self, connection: Connection = Connection.ASYNC
+    ) -> Union[AsyncEngine, Engine]:
+        url = self.config.connection.make_url(connection)
+
+        pooling_kwargs = self.config.pooling.model_dump(
+            exclude={
+                "strategy",
+                "prepared_statement_cache_size",
+                "pool_reset_on_return",
+            },
+            exclude_none=True,
+        )
+
+        engine_kwargs = {"echo": self.config.connection.echo, **pooling_kwargs}
+
+        if connection is Connection.ASYNC:
+            self._async_engine = create_async_engine(url, **engine_kwargs)
+            return self._async_engine
+        elif connection is Connection.SYNC:
+            self._sync_engine = create_engine(url, **engine_kwargs)
+            return self._sync_engine
+
+    @overload
+    def get(self, connection: Literal[Connection.ASYNC]) -> AsyncEngine: ...
+    @overload
+    def get(self, connection: Literal[Connection.SYNC]) -> Engine: ...
+    def get(
+        self, connection: Connection = Connection.ASYNC
+    ) -> Union[AsyncEngine, Engine]:
+        if connection is Connection.ASYNC:
+            return self._async_engine
+        elif connection is Connection.SYNC:
+            return self._sync_engine
+
+    def get_all(self) -> Tuple[AsyncEngine, Engine]:
+        return (self._async_engine, self._sync_engine)
+
+    async def dispose(self):
+        await self._async_engine.dispose()
+        self._sync_engine.dispose()
