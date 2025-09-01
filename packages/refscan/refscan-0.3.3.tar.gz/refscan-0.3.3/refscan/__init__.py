a8c47e7229be0f390f757c071324c4e1ae88580a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:50 +0000

    Add package refscan/0.3.3/refscan-0.3.3.tar.gz

---
+++ packages/refscan/refscan-0.3.3.tar.gz/refscan-0.3.3/refscan/__init__.py
@@ -0,0 +1,18 @@
+from importlib.metadata import metadata, PackageNotFoundError
+
+
+def get_package_metadata(key: str) -> str:
+    r"""
+    Returns metadata about the installed package.
+
+    References:
+    - https://docs.python.org/3/library/importlib.metadata.html#distribution-metadata
+    - https://github.com/python-poetry/poetry/issues/273#issuecomment-570999678
+    """
+    metadata_value = ""
+    try:
+        package_metadata = metadata("refscan")
+        metadata_value = package_metadata.get(key, "")
+    except PackageNotFoundError:
+        pass
+    return metadata_value
