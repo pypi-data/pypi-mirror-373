30a44c0b84afabad494f376966f6e74ab03c85a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:05:02 +0000

    Add package minecraft-datapack-language/14.0.1/minecraft_datapack_language-14.0.1.tar.gz

---
+++ packages/minecraft-datapack-language/minecraft_datapack_language-14.0.1.tar.gz/minecraft_datapack_language-14.0.1/minecraft_datapack_language/UpdateMDL.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# MDL Development Cycle Script
+
+set -e
+
+echo "🔧 UPDATE MDL - Git Upload, Release, Wait, Upgrade"
+
+# Git Upload
+echo "🔧 Git Upload..."
+git add .
+git commit -m "MDL Development Cycle"
+git push
+
+# Release
+echo "🔧 Release..."
+./scripts/release.sh patch "MDL Development Cycle"
+
+# Wait
+echo "🔧 Wait..."
+sleep 5
+
+# Upgrade
+echo "🔧 Upgrade..."
+pipx upgrade minecraft-datapack-language
+pipx upgrade minecraft-datapack-language
+pipx upgrade minecraft-datapack-language
+pipx upgrade minecraft-datapack-language
+pipx upgrade minecraft-datapack-language
