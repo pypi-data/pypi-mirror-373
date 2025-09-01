88f81bebbe89831b051d0479a9ab427c3b9baee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:24 +0000

    Add package domselect/0.0.4/domselect-0.0.4.tar.gz

---
+++ packages/domselect/domselect-0.0.4.tar.gz/domselect-0.0.4/domselect/base.py
@@ -0,0 +1,170 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import overload
+
+from .errors import AttributeNotFoundError, NodeNotFoundError
+from .unset import UNSET, UnsetType
+
+DEFAULT_STRIP_TEXT = True
+
+
+class BaseSelector[RawNodeT]:
+    __slots__ = ["raw_node"]
+
+    def __init__(self, raw_node: RawNodeT) -> None:
+        self.raw_node = raw_node
+
+    @classmethod
+    @abstractmethod
+    def from_content(cls, content: bytes | str) -> BaseSelector[RawNodeT]: ...
+
+    @overload
+    def first(
+        self, query: str, default: UnsetType = UNSET
+    ) -> BaseSelector[RawNodeT]: ...
+
+    @overload
+    def first(self, query: str, default: None) -> None | BaseSelector[RawNodeT]: ...
+
+    def first(
+        self, query: str, default: None | UnsetType = UNSET
+    ) -> None | BaseSelector[RawNodeT]:
+        try:
+            return self.__class__(self.find_raw(query)[0])
+        except IndexError as ex:
+            if default is None:
+                return None
+            raise NodeNotFoundError(
+                "Node not found for query: {}".format(query)
+            ) from ex
+
+    @overload
+    def first_raw(self, query: str, default: UnsetType = UNSET) -> RawNodeT: ...
+
+    @overload
+    def first_raw(self, query: str, default: None) -> None | RawNodeT: ...
+
+    def first_raw(
+        self, query: str, default: None | UnsetType = UNSET
+    ) -> None | RawNodeT:
+        try:
+            return self.find_raw(query)[0]
+        except IndexError as ex:
+            if default is None:
+                return None
+            raise NodeNotFoundError(
+                "Node not found for query: {}".format(query)
+            ) from ex
+
+    def find(self, query: str) -> list[BaseSelector[RawNodeT]]:
+        return [self.__class__(x) for x in self.find_raw(query)]
+
+    @abstractmethod
+    def find_raw(self, query: str) -> list[RawNodeT]:
+        raise NotImplementedError
+
+    def exists(self, query: str) -> bool:
+        return self.first(query, default=None) is not None
+
+    @overload
+    def attr(self, name: str, default: UnsetType = UNSET) -> str: ...
+
+    @overload
+    def attr(self, name: str, default: None) -> None | str: ...
+
+    @overload
+    def attr(self, name: str, default: str) -> str: ...
+
+    def attr(self, name: str, default: None | UnsetType | str = UNSET) -> None | str:
+        try:
+            return self.get_raw_attr(name)
+        except AttributeNotFoundError:
+            if default is UNSET:
+                raise
+            return default
+
+    @abstractmethod
+    def get_raw_attr(self, name: str) -> str:
+        raise NotImplementedError
+
+    @overload
+    def first_text(
+        self, query: str, default: UnsetType = UNSET, strip: bool = DEFAULT_STRIP_TEXT
+    ) -> str: ...
+
+    @overload
+    def first_text(
+        self, query: str, default: None, strip: bool = DEFAULT_STRIP_TEXT
+    ) -> None | str: ...
+
+    @overload
+    def first_text(
+        self, query: str, default: str, strip: bool = DEFAULT_STRIP_TEXT
+    ) -> str: ...
+
+    def first_text(
+        self,
+        query: str,
+        default: None | UnsetType | str = UNSET,
+        strip: bool = DEFAULT_STRIP_TEXT,
+    ) -> None | str:
+        try:
+            return self.first(query).text(strip=strip)
+        except NodeNotFoundError:
+            if default is UNSET:
+                raise
+            return default
+
+    @abstractmethod
+    def text(self, strip: bool = DEFAULT_STRIP_TEXT) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def tag(self) -> str:
+        raise NotImplementedError
+
+    @overload
+    def first_attr(self, query: str, name: str, default: UnsetType = UNSET) -> str: ...
+
+    @overload
+    def first_attr(self, query: str, name: str, default: str) -> str: ...
+
+    @overload
+    def first_attr(self, query: str, name: str, default: None) -> None | str: ...
+
+    def first_attr(
+        self, query: str, name: str, default: None | UnsetType | str = UNSET
+    ) -> None | str:
+        try:
+            return self.first(query).attr(name, default=default)
+        except NodeNotFoundError:
+            if default is UNSET:
+                raise
+            return default
+
+    @abstractmethod
+    def parent(self) -> BaseSelector[RawNodeT]:
+        raise NotImplementedError
+
+    @overload
+    def first_contains(
+        self, query: str, pattern: str, default: UnsetType = UNSET
+    ) -> BaseSelector[RawNodeT]: ...
+
+    @overload
+    def first_contains(
+        self, query: str, pattern: str, default: None
+    ) -> None | BaseSelector[RawNodeT]: ...
+
+    def first_contains(
+        self, query: str, pattern: str, default: None | UnsetType = UNSET
+    ) -> None | BaseSelector[RawNodeT]:
+        for sel in self.find(query):
+            if pattern in sel.text():
+                return sel
+        if default is None:
+            return None
+        raise NodeNotFoundError(
+            "Node not found for query [{}] and text [{}]".format(query, pattern)
+        )
---
+++ packages/domselect/domselect-0.0.4.tar.gz/domselect-0.0.4/domselect/lxml_selector.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import threading
+from abc import abstractmethod
+from typing import Self, TypedDict, cast
+
+import lxml.html
+from lxml.cssselect import CSSSelector
+from lxml.html import HtmlElement, fromstring
+
+from .base import DEFAULT_STRIP_TEXT, BaseSelector
+from .errors import AttributeNotFoundError, InvalidQueryError, NodeNotFoundError
+
+__all__ = ["LxmlCssSelector", "LxmlXpathSelector"]
+
+LXML_CSS_SELECTOR_CACHE: dict[str, CSSSelector] = {}
+THREAD_STORE = threading.local()
+
+
+class ThreadStoreCache(TypedDict, total=False):
+    paser: lxml.html.HTMLParser
+
+
+class BaseLxmlSelector(BaseSelector[HtmlElement]):
+    @abstractmethod
+    def find_raw(self, query: str) -> list[HtmlElement]:
+        raise NotImplementedError
+
+    @classmethod
+    def get_html_parser(cls) -> lxml.html.HTMLParser:
+        if not hasattr(THREAD_STORE, "cache"):
+            THREAD_STORE.cache = ThreadStoreCache()
+        # Each thread should operate with its own parser instance (for speed)
+        return cast(
+            lxml.html.HTMLParser,
+            THREAD_STORE.cache.setdefault("parser", lxml.html.HTMLParser()),
+        )
+
+    @classmethod
+    def from_content(cls, content: bytes | str) -> Self:
+        return cls(fromstring(content, parser=cls.get_html_parser()))
+
+    def get_raw_attr(self, name: str) -> str:
+        try:
+            return self.raw_node.attrib[name]
+        except KeyError as ex:
+            raise AttributeNotFoundError(
+                "Element does not have attribute with name [{}]".format(name)
+            ) from ex
+
+    def text(self, strip: bool = DEFAULT_STRIP_TEXT) -> str:
+        res = self.raw_node.text_content()
+        if strip:
+            return res.strip()
+        return res
+
+    def tag(self) -> str:
+        return str(self.raw_node.tag)
+
+    def parent(self) -> Self:
+        node = self.raw_node.getparent()
+        if node is None:
+            raise NodeNotFoundError("Parent node does not exists")
+        return self.__class__(node)
+
+
+class LxmlCssSelector(BaseLxmlSelector):
+    __slots__ = []
+
+    def find_raw(self, query: str) -> list[HtmlElement]:
+        res = LXML_CSS_SELECTOR_CACHE.setdefault(query, CSSSelector(query))(
+            self.raw_node
+        )
+        if not isinstance(res, list):
+            raise InvalidQueryError(
+                "Selector support only queries which results in list of DOM nodes"
+            )
+        return res
+
+
+class LxmlXpathSelector(BaseLxmlSelector):
+    __slots__ = []
+
+    def find_raw(self, query: str) -> list[HtmlElement]:
+        res = self.raw_node.xpath(query)
+        if not isinstance(res, list):
+            raise InvalidQueryError(
+                "Selector support only queries which results in list of DOM nodes"
+            )
+        return res
