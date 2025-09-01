dcd5f8428543c19a32d01e32046ea9c68baf2656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:17 +0000

    Add package noLZSS/0.1.0/nolzss-0.1.0-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/noLZSS/nolzss-0.1.0-cp311-cp311-macosx_10_9_x86_64.whl/noLZSS/__init__.py
@@ -0,0 +1,17 @@
+from ._noLZSS import (
+    factorize,
+    factorize_file,
+    count_factors,
+    count_factors_file,
+    write_factors_binary_file,
+    __version__
+)  # type: ignore
+
+__all__ = [
+    "factorize",
+    "factorize_file", 
+    "count_factors",
+    "count_factors_file",
+    "write_factors_binary_file",
+    "__version__"
+]
