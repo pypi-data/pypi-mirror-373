a8a24a8511e1ac45d19a8014d6dc7486032a1dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:37 +0000

    Add package unitofwork/0.1.1/unitofwork-0.1.1.tar.gz

---
+++ packages/unitofwork/unitofwork-0.1.1.tar.gz/unitofwork-0.1.1/src/unitofwork/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025 Maxim Ivanov
+# SPDX-License-Identifier: MIT
+
+import logging
+
+from .interfaces import SupportsRollback
+from .uow import RollbackError, UnitOfWork, UnitOfWorkError
+
+
+# Set up null handler for the library's logger
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+__all__ = [
+    'RollbackError',
+    'SupportsRollback',
+    'UnitOfWork',
+    'UnitOfWorkError',
+]
