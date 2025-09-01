d79b54e8df1b6a69b8cd34c9adb4b65001d47361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:52 +0000

    Add package segment-geospatial-uni/0.0.2/segment_geospatial_uni-0.0.2-py2.py3-none-any.whl

---
+++ packages/segment-geospatial-uni/segment_geospatial_uni-0.0.2-py2.py3-none-any.whl/samgeo/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for segment-geospatial."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.12.6"
+
+
+from .samgeo import *
+from .samgeo2 import *
+from .common import show_image_gui
