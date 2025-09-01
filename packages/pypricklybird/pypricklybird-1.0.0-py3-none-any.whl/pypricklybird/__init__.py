627f33a78061544a388aeb45ecb395a91898bc86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 05:11:14 +0000

    Add package pypricklybird/1.0.0/pypricklybird-1.0.0-py3-none-any.whl

---
+++ packages/pypricklybird/pypricklybird-1.0.0-py3-none-any.whl/pypricklybird/__init__.py
@@ -0,0 +1,54 @@
+"""Convert binary data into a human friendly format.
+
+This is a python implementation of the [pricklybird][1] format.
+
+## Usage
+```python
+>>> from pypricklybird import convert_to_pricklybird, convert_from_pricklybird
+>>> data = bytearray.fromhex("4243")
+>>> code = convert_to_pricklybird(data);
+>>> code
+'flea-flux-full'
+>>> convert_from_pricklybird(code).hex()
+'4243'
+
+```
+
+## Documentation
+
+[1]: https://github.com/ndornseif/pricklybird
+"""
+
+from pypricklybird.converter import (
+    CRC8_POLY,
+    CRC8_TABLE,
+    HASH_TABLE,
+    HASH_TABLE_SIZE,
+    WORDLIST,
+    CRCError,
+    DecodeError,
+    bytes_to_words,
+    calculate_crc8,
+    convert_from_pricklybird,
+    convert_to_pricklybird,
+    word_hash,
+    words_to_bytes,
+)
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "CRC8_POLY",
+    "CRC8_TABLE",
+    "HASH_TABLE",
+    "HASH_TABLE_SIZE",
+    "WORDLIST",
+    "CRCError",
+    "DecodeError",
+    "bytes_to_words",
+    "calculate_crc8",
+    "convert_from_pricklybird",
+    "convert_to_pricklybird",
+    "word_hash",
+    "words_to_bytes",
+]
