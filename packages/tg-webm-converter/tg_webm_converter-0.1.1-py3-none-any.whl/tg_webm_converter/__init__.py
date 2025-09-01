3023bd464cb0c4c136e1ce1701026057a96afddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:05:03 +0000

    Add package tg-webm-converter/0.1.1/tg_webm_converter-0.1.1-py3-none-any.whl

---
+++ packages/tg-webm-converter/tg_webm_converter-0.1.1-py3-none-any.whl/tg_webm_converter/__init__.py
@@ -0,0 +1,6 @@
+"""tg-webm-converter - Convert images to Telegram WebM stickers."""
+from .converter import TgWebMConverter
+
+__version__ = "0.1.0"
+__author__ = "Yeongjin Kim"
+__email__ = "77034308+7591yj@users.noreply.github.com"
