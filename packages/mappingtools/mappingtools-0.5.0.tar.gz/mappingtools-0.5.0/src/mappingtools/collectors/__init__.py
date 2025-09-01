d02d285c98072e137b425ace36e05b3bddf2c3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:50 +0000

    Add package mappingtools/0.5.0/mappingtools-0.5.0.tar.gz

---
+++ packages/mappingtools/mappingtools-0.5.0.tar.gz/mappingtools-0.5.0/src/mappingtools/collectors/__init__.py
@@ -0,0 +1,11 @@
+from ._collectors import CategoryCounter, MinifyingMapper, nested_defaultdict
+from .mapping_collector import MappingCollector, MappingCollectorMode
+
+
+__all__ = (
+    'CategoryCounter',
+    'MappingCollector',
+    'MappingCollectorMode',
+    'MinifyingMapper',
+    'nested_defaultdict'
+)
