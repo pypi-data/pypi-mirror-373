f6f39963e42bf795549e9bfc43d2e189397b9567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:30:08 +0000

    Add package pybiwenger/1.0.0/pybiwenger-1.0.0-py3-none-any.whl

---
+++ packages/pybiwenger/pybiwenger-1.0.0-py3-none-any.whl/pybiwenger/src/client/urls.py
@@ -0,0 +1,23 @@
+"""
+The following module contains the utilized URLs for the API classes
+"""
+
+url_login = "https://biwenger.as.com/api/v2/auth/login"
+url_account = "https://biwenger.as.com/api/v2/account"
+url_players_market = "https://biwenger.as.com/api/v2/user?fields=players(id,owner),market(*,-userID),-trophies"
+url_players_league = "https://biwenger.as.com/api/v2/players/la-liga"
+url_retire_market = "https://biwenger.as.com/api/v2/market?player="
+url_user_players = "https://biwenger.as.com/api/v2/user?fields=players(id,owner)"
+url_add_player_market = "https://biwenger.as.com/api/v2/market"
+url_catalog = "https://biwenger.as.com/api/v2/competitions/la-liga/data?lang=es&score=5"
+url_all_players = (
+    "https://biwenger.as.com/api/v2/competitions/la-liga/data?lang=es&score=5"
+)
+url_ranking = "https://biwenger.as.com/api/v2/rounds/league"
+url_transfers = (
+    "https://biwenger.as.com/api/v2/league/742220/board?type=transfer,market"
+)
+url_league = ("https://biwenger.as.com/api/v2/league/",)
+url_rounds = "https://biwenger.as.com/api/v2/rounds/league"
+url_competitions = "https://biwenger.as.com/api/v2/competitions"
+url_user = "https://biwenger.as.com/api/v2/user"
