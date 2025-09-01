928f4f8299a663839a37e68a3ac71aaf9b62372c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:45 +0000

    Add package pdm-polylith-workspace/1.3.3/pdm_polylith_workspace-1.3.3-py3-none-any.whl

---
+++ packages/pdm-polylith-workspace/pdm_polylith_workspace-1.3.3-py3-none-any.whl/pdm_polylith_workspace/polylith/parsing/__init__.py
@@ -0,0 +1,3 @@
+from pdm_polylith_workspace.polylith.parsing.core import copy_brick, parse_brick_namespace_from_path
+from pdm_polylith_workspace.polylith.parsing.rewrite import rewrite_modules
+__all__ = ['copy_brick', 'parse_brick_namespace_from_path', 'rewrite_modules']
