60ec12ea81ba350b7319da77d9f6ed79c6775708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:45 +0000

    Add package breezy/3.3.13.dev0/breezy-3.3.13.dev0-pp311-pypy311_pp73-win_amd64.whl

---
+++ packages/breezy/breezy-3.3.13.dev0-pp311-pypy311_pp73-win_amd64.whl/breezy/plugins/netrc_credential_store/tests/test_netrc.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2008, 2009, 2013, 2016 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from io import BytesIO
+
+from .... import config, osutils, tests
+from .... import transport as _mod_transport
+
+
+class TestNetrcCSNoNetrc(tests.TestCaseInTempDir):
+    def test_home_netrc_does_not_exist(self):
+        self.assertRaises(
+            _mod_transport.NoSuchFile,
+            config.credential_store_registry.get_credential_store,
+            "netrc",
+        )
+
+
+class TestNetrcCS(tests.TestCaseInTempDir):
+    def setUp(self):
+        super().setUp()
+        # Create a .netrc file
+        netrc_content = b"""
+machine host login joe password secret
+default login anonymous password joe@home
+"""
+        netrc_path = osutils.pathjoin(self.test_home_dir, ".netrc")
+        with open(netrc_path, "wb") as f:
+            f.write(netrc_content)
+        # python's netrc will complain about access permissions starting with
+        # 2.7.5-8 so we restrict the access unconditionally
+        osutils.chmod_if_possible(netrc_path, 0o600)
+
+    def _get_netrc_cs(self):
+        return config.credential_store_registry.get_credential_store("netrc")
+
+    def test_not_matching_user(self):
+        cs = self._get_netrc_cs()
+        password = cs.decode_password({"host": "host", "user": "jim"})
+        self.assertIs(None, password)
+
+    def test_matching_user(self):
+        cs = self._get_netrc_cs()
+        password = cs.decode_password({"host": "host", "user": "joe"})
+        self.assertEqual("secret", password)
+
+    def test_default_password(self):
+        cs = self._get_netrc_cs()
+        password = cs.decode_password({"host": "other", "user": "anonymous"})
+        self.assertEqual("joe@home", password)
+
+    def test_default_password_without_user(self):
+        cs = self._get_netrc_cs()
+        password = cs.decode_password({"host": "other"})
+        self.assertIs(None, password)
+
+    def test_get_netrc_credentials_via_auth_config(self):
+        # Create a test AuthenticationConfig object
+        ac_content = b"""
+[host1]
+host = host
+user = joe
+password_encoding = netrc
+"""
+        conf = config.AuthenticationConfig(_file=BytesIO(ac_content))
+        credentials = conf.get_credentials("scheme", "host", user="joe")
+        self.assertIsNot(None, credentials)
+        self.assertEqual("secret", credentials.get("password", None))
---
+++ packages/breezy/breezy-3.3.13.dev0-pp311-pypy311_pp73-win_amd64.whl/breezy/tests/test_textfile.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2006 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from io import BytesIO
+
+from ..errors import BinaryFile
+from ..textfile import check_text_lines, check_text_path, text_file
+from . import TestCase, TestCaseInTempDir
+
+
+class TextFile(TestCase):
+    def test_text_file(self):
+        s = BytesIO(b"ab" * 2048)
+        self.assertEqual(text_file(s).read(), s.getvalue())
+        s = BytesIO(b"a" * 1023 + b"\x00")
+        self.assertRaises(BinaryFile, text_file, s)
+        s = BytesIO(b"a" * 1024 + b"\x00")
+        self.assertEqual(text_file(s).read(), s.getvalue())
+
+    def test_check_text_lines(self):
+        lines = [b"ab" * 2048]
+        check_text_lines(lines)
+        lines = [b"a" * 1023 + b"\x00"]
+        self.assertRaises(BinaryFile, check_text_lines, lines)
+
+
+class TextPath(TestCaseInTempDir):
+    def test_text_file(self):
+        with open("boo", "wb") as f:
+            f.write(b"ab" * 2048)
+        check_text_path("boo")
+        with open("boo", "wb") as f:
+            f.write(b"a" * 1023 + b"\x00")
+        self.assertRaises(BinaryFile, check_text_path, "boo")
