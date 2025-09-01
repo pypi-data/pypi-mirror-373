181344ee26abf234b1e4a41d71feeed1f6d87cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:05:25 +0000

    Add package djimporter/0.10/djimporter-0.10.tar.gz

---
+++ packages/djimporter/djimporter-0.10.tar.gz/djimporter-0.10/djimporter/static/djimporter/js/upload_status.js
@@ -0,0 +1,22 @@
+let current_status = $('#js-data').data('importlog-status');
+if (['running', 'created'].includes(current_status)){
+    getData();
+}
+
+function getData() {
+    let url = $('#js-data').data('importlog-status-url');
+        $.ajax({
+        url : url,
+        dataType: "json",
+        success : function(data) {
+            let status = data['status'];
+            if (current_status != status)
+                location.reload();
+            if (current_status == 'created'){
+                setTimeout(getData, 1000);
+            } else if (current_status == 'running'){
+                setTimeout(getData, 10000);
+            }
+        }
+    });
+}
