22b93e5507b7179c640e6ccaba4dbbbd0af36780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:04:58 +0000

    Add package pitchoune/0.52.8/pitchoune-0.52.8.tar.gz

---
+++ packages/pitchoune/pitchoune-0.52.8.tar.gz/pitchoune-0.52.8/pitchoune/chats/ollama_chat.py
@@ -0,0 +1,27 @@
+from pitchoune.chat import Chat
+
+
+class OllamaChat(Chat):
+    """Chat class for Ollama models."""
+    def __init__(self, model: str, prompt: str, **params):
+        super().__init__(model, prompt, **params)
+
+    def send_msg(self, text: str) -> str:
+        """Send a message to the chat and return the response."""
+        import ollama
+        return ollama.chat(
+            model=self._model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": self._prompt,
+                }, {
+                    "role": "user",
+                    "content": text,
+                },
+            ],
+            options={
+                "temperature": self._params["temperature"],
+                "max_tokens": self._params["max_tokens"]
+            }
+        ).message.content
