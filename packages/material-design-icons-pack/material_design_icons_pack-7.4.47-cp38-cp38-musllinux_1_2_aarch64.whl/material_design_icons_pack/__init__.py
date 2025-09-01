4df7ba0c7361c34d1f6757063ebc705972a424e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:05:08 +0000

    Add package material-design-icons-pack/7.4.47/material_design_icons_pack-7.4.47-cp38-cp38-musllinux_1_2_aarch64.whl

---
+++ packages/material-design-icons-pack/material_design_icons_pack-7.4.47-cp38-cp38-musllinux_1_2_aarch64.whl/material_design_icons_pack/__init__.py
@@ -0,0 +1,5 @@
+from .material_design_icons_pack import *
+
+__doc__ = material_design_icons_pack.__doc__
+if hasattr(material_design_icons_pack, "__all__"):
+    __all__ = material_design_icons_pack.__all__
