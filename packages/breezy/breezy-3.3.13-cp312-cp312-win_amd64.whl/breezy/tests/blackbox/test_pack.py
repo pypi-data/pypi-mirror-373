5711c03276e6d1503e75b2a68f917455b10d4f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:05:10 +0000

    Add package breezy/3.3.13/breezy-3.3.13-cp312-cp312-win_amd64.whl

---
+++ packages/breezy/breezy-3.3.13-cp312-cp312-win_amd64.whl/breezy/tests/blackbox/test_pack.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2007, 2009-2012 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+"""Tests of the 'brz pack' command."""
+
+from breezy import tests
+
+
+class TestPack(tests.TestCaseWithTransport):
+    def _make_versioned_file(self, path, line_prefix="line", total_lines=10):
+        self._make_file(path, line_prefix, total_lines, versioned=True)
+
+    def _make_file(self, path, line_prefix, total_lines, versioned):
+        text = ""
+        for i in range(total_lines):
+            text += line_prefix + str(i + 1) + "\n"
+
+        with open(path, "w") as f:
+            f.write(text)
+        if versioned:
+            self.run_bzr(["add", path])
+            self.run_bzr(["ci", "-m", '"' + path + '"'])
+
+    def _update_file(self, path, text, checkin=True):
+        """Append text to file 'path' and check it in."""
+        with open(path, "a") as f:
+            f.write(text)
+
+        if checkin:
+            self.run_bzr(["ci", path, "-m", '"' + path + '"'])
+
+    def test_pack_silent(self):
+        """Pack command has no intrinsic output."""
+        self.make_branch(".")
+        out, err = self.run_bzr("pack")
+        self.assertEqual("", out)
+        self.assertEqual("", err)
+
+    def test_pack_accepts_branch_url(self):
+        """Pack command accepts the url to a branch."""
+        self.make_branch("branch")
+        out, err = self.run_bzr("pack branch")
+        self.assertEqual("", out)
+        self.assertEqual("", err)
+
+    def test_pack_accepts_repo_url(self):
+        """Pack command accepts the url to a branch."""
+        self.make_repository("repository")
+        out, err = self.run_bzr("pack repository")
+        self.assertEqual("", out)
+        self.assertEqual("", err)
+
+    def test_pack_clean_obsolete_packs(self):
+        """Ensure --clean-obsolete-packs removes obsolete pack files."""
+        wt = self.make_branch_and_tree(".")
+        t = wt.branch.repository.controldir.transport
+
+        # do multiple commits to ensure that obsolete packs are created
+        # by 'brz pack'
+        self._make_versioned_file("file0.txt")
+        for i in range(5):
+            self._update_file("file0.txt", "HELLO %d\n" % i)
+
+        out, err = self.run_bzr(["pack", "--clean-obsolete-packs"])
+
+        pack_names = t.list_dir("repository/obsolete_packs")
+        self.assertTrue(len(pack_names) == 0)
---
+++ packages/breezy/breezy-3.3.13-cp312-cp312-win_amd64.whl/breezy/tests/per_workingtree/test_get_file_mtime.py
@@ -0,0 +1,109 @@
+# Copyright (C) 2007 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Test that all WorkingTree's implement get_file_mtime."""
+
+import os
+
+from breezy import errors, transport
+from breezy.tests import TestNotApplicable
+from breezy.tests.per_workingtree import TestCaseWithWorkingTree
+
+
+class TestGetFileMTime(TestCaseWithWorkingTree):
+    """Test WorkingTree.get_file_mtime.
+
+    These are more involved because we need to handle files which have been
+    renamed, etc.
+    """
+
+    def make_basic_tree(self):
+        tree = self.make_branch_and_tree("tree")
+        self.build_tree(["tree/one"])
+        tree.add(["one"])
+        return tree
+
+    def test_get_file_mtime_nested(self):
+        tree = self.make_basic_tree()
+        subtree = self.make_branch_and_tree("tree/sub")
+        self.build_tree(["tree/sub/one"])
+        subtree.add(["one"])
+        subtree.commit("one")
+        try:
+            tree.add_reference(subtree)
+        except errors.UnsupportedOperation:
+            raise TestNotApplicable("subtrees not supported")
+        tree.commit("sub")
+
+        with tree.lock_read(), subtree.lock_read():
+            self.assertEqual(
+                tree.get_file_mtime("sub/one"), subtree.get_file_mtime("one")
+            )
+            self.assertEqual(
+                tree.basis_tree().get_file_mtime("sub/one"),
+                subtree.basis_tree().get_file_mtime("one"),
+            )
+
+    def test_get_file_mtime(self):
+        tree = self.make_basic_tree()
+
+        st = os.lstat("tree/one")
+        with tree.lock_read():
+            mtime_file_id = tree.get_file_mtime("one")
+            self.assertIsInstance(mtime_file_id, (float, int))
+            self.assertAlmostEqual(st.st_mtime, mtime_file_id)
+
+    def test_after_commit(self):
+        """Committing shouldn't change the mtime."""
+        tree = self.make_basic_tree()
+
+        st = os.lstat("tree/one")
+        tree.commit("one")
+
+        with tree.lock_read():
+            mtime = tree.get_file_mtime("one")
+            self.assertAlmostEqual(st.st_mtime, mtime)
+
+    def test_get_renamed_time(self):
+        """We should handle renamed files."""
+        tree = self.make_basic_tree()
+
+        tree.rename_one("one", "two")
+        st = os.lstat("tree/two")
+
+        with tree.lock_read():
+            mtime = tree.get_file_mtime("two")
+            self.assertAlmostEqual(st.st_mtime, mtime)
+
+    def test_get_renamed_in_subdir_time(self):
+        tree = self.make_branch_and_tree("tree")
+        self.build_tree(["tree/d/", "tree/d/a"])
+        tree.add(["d", "d/a"])
+        tree.commit("1")
+
+        tree.rename_one("d", "e")
+
+        st = os.lstat("tree/e/a")
+        with tree.lock_read():
+            mtime = tree.get_file_mtime("e/a")
+            self.assertAlmostEqual(st.st_mtime, mtime)
+
+    def test_missing(self):
+        tree = self.make_basic_tree()
+
+        os.remove("tree/one")
+        with tree.lock_read():
+            self.assertRaises(transport.NoSuchFile, tree.get_file_mtime, "one")
