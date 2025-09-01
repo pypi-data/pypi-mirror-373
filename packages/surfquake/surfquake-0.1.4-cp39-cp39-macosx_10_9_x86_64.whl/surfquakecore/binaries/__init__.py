dd5da4c4f87eab694b27eaac2056b9049da92e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 05:11:36 +0000

    Add package surfquake/0.1.4/surfquake-0.1.4-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/surfquake/surfquake-0.1.4-cp39-cp39-macosx_10_9_x86_64.whl/surfquakecore/binaries/__init__.py
@@ -0,0 +1,34 @@
+import os
+import platform
+
+
+def __get_binary_dir():
+    _root_dir = os.path.dirname(__file__)
+    _os = platform.system()
+
+    _os_bin_folder = ''
+    if _os.lower() == 'linux':
+        _os_bin_folder = "linux_bin"
+    elif _os.lower() == 'mac' or _os.lower() == 'darwin':
+        _os_bin_folder = "mac_bin"
+    elif _os.lower() == 'windows':
+        # warnings.warn(f"The OS {_os} do not support some functions.")
+        _os_bin_folder = "win_bin"
+    else:
+        raise ValueError(f"The OS {_os} is not supported.")
+
+    return os.path.join(_root_dir,  _os_bin_folder)
+
+
+_bin_dir = __get_binary_dir()
+BINARY_REAL_FILE = os.path.join(_bin_dir, "REAL", "REAL")
+BINARY_NLL_DIR = os.path.join(_bin_dir, "NLL")
+BINARY_GREEN_DIR = os.path.join(_bin_dir, "mti_green")
+BINARY_FOCMEC_DIR = os.path.join(_bin_dir, "FOCMEC")
+
+__all__ = [
+    'BINARY_NLL_DIR',
+    'BINARY_REAL_FILE',
+    'BINARY_GREEN_DIR',
+    'BINARY_FOCMEC_DIR'
+]
