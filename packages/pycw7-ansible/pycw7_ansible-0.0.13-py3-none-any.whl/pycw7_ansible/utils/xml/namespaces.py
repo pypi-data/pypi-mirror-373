3917f2b8e43b68860a4d7f259b1bc54845915110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 04:10:28 +0000

    Add package pycw7-ansible/0.0.13/pycw7_ansible-0.0.13-py3-none-any.whl

---
+++ packages/pycw7-ansible/pycw7_ansible-0.0.13-py3-none-any.whl/pycw7_ansible/utils/xml/namespaces.py
@@ -0,0 +1,11 @@
+NCDATA = 'http://www.intelbras.com/netconf/data:1.0'
+NCDATA_C = '{' + NCDATA + '}'
+
+NETCONFBASE = "urn:ietf:params:xml:ns:netconf:base:1.0"
+NETCONFBASE_C = '{' + NETCONFBASE + '}'
+
+NCCONFIG = "http://www.intelbras.com/netconf/config:1.0"
+NCCONFIG_C = '{' + NCCONFIG + '}'
+
+NCACTION = "http://www.intelbras.com/netconf/action:1.0"
+NCACTION_C = '{' + NCACTION + '}'
