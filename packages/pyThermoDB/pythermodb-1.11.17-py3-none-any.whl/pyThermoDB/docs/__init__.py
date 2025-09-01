02288cc6af412c617c866c3d37af93339312175e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:06:09 +0000

    Add package pyThermoDB/1.11.17/pythermodb-1.11.17-py3-none-any.whl

---
+++ packages/pyThermoDB/pythermodb-1.11.17-py3-none-any.whl/pyThermoDB/docs/__init__.py
@@ -0,0 +1,33 @@
+from .thermo import (
+    ThermoDB,
+    ThermoProperty,
+    ComponentSearch,
+    ListComponents,
+    ListComponentsInfo,
+    ListDatabookDescriptions
+)
+from .tableref import TableReference
+from .managedata import ManageData
+from .customref import CustomRef
+from .compbuilder import CompBuilder
+from .tabledata import TableData
+from .tableequation import TableEquation
+from .tablematrixdata import TableMatrixData
+from .tablematrixequation import TableMatrixEquation
+
+__all__ = [
+    'ThermoDB',
+    'TableReference',
+    'ManageData',
+    'CompBuilder',
+    'CustomRef',
+    'TableData',
+    'TableEquation',
+    'TableMatrixData',
+    'TableMatrixEquation',
+    'ThermoProperty',
+    'ComponentSearch',
+    'ListComponents',
+    'ListComponentsInfo',
+    'ListDatabookDescriptions'
+]
