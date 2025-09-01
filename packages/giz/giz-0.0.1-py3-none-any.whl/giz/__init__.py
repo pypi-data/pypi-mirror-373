189f13bc4890bc3b6effaaa47303ca068abef1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:40 +0000

    Add package giz/0.0.1/giz-0.0.1-py3-none-any.whl

---
+++ packages/giz/giz-0.0.1-py3-none-any.whl/giz/__init__.py
@@ -0,0 +1,33 @@
+import json
+from pathlib import Path
+
+from fire import Fire
+from appdirs import user_config_dir
+
+__version__ = "0.0.1"
+_appdata_dir = Path(user_config_dir(appname="giz", appauthor="giz"))
+CONFIG_PATH = _appdata_dir / Path("giz_config.json")
+
+
+def version():
+    print(__version__)
+
+
+def config():
+    print(f"config located at {CONFIG_PATH}")
+
+
+def commit():
+    print(f"Commit message: ")
+    raise NotImplementedError("Not implemented")
+
+
+def init_cli():
+    Fire(
+        {
+            "commit": commit,
+            "config": config,
+            "--version": version,
+            "-v": version,
+        }
+    )
