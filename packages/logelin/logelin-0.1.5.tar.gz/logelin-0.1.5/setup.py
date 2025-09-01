16d9dedf8d00015c0c67d04acb59e0a389a54df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:43 +0000

    Add package logelin/0.1.5/logelin-0.1.5.tar.gz

---
+++ packages/logelin/logelin-0.1.5.tar.gz/logelin-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='logelin',
+    version='0.1.5',
+    description='A tool which, by modelling the sampled galaxies as ' \
+    'ellipsoids with intrinsic semi-axes drawn from a multivariate ' \
+    'log-normal distribution, uses Bayesian inference to generate ' \
+    'posteriors for parameters describing the underlying distribution ' \
+    'of shapes and sizes of galaxies given projected angular extents '
+    'and redshifts. Built using NumPyro and astropy.',
+    url='https://github.com/dmjwentworth/lognormal-ellipsoid-inferrer',
+    author='Dougal Wentworth',
+    author_email='dw619@cantab.ac.uk',
+    license='MIT license',
+    packages=['logelin'],
+    install_requires=['jax',                        
+                      'astropy',                     
+                      'numpyro',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
