001b1c734e7ed291afedc328b6c751cc10d5b589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 04:10:28 +0000

    Add package trading-kiwcomp-models/0.1.15/trading_kiwcomp_models-0.1.15-py3-none-any.whl

---
+++ packages/trading-kiwcomp-models/trading_kiwcomp_models-0.1.15-py3-none-any.whl/trading_kiwcomp_models/models/binance/data/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import DeclarativeBase
+# class Base(DeclarativeBase):
+#     pass
+
+# Abstract base for ExchangeInfo
+class ExchangeInfoSymbol(DeclarativeBase):
+    pass
+
+class BinanceKline(DeclarativeBase):
+    pass
