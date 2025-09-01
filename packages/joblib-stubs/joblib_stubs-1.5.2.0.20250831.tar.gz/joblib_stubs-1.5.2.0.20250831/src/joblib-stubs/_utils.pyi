754b133d34412330d923ba4e504329c6f7c74f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:18 +0000

    Add package joblib-stubs/1.5.2.0.20250831/joblib_stubs-1.5.2.0.20250831.tar.gz

---
+++ packages/joblib-stubs/joblib_stubs-1.5.2.0.20250831.tar.gz/joblib_stubs-1.5.2.0.20250831/src/joblib-stubs/_utils.pyi
@@ -0,0 +1,25 @@
+import ast
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Any, Generic
+
+from joblib._multiprocessing_helpers import mp as mp
+from typing_extensions import ParamSpec, TypeVar
+
+_T = TypeVar("_T")
+_P = ParamSpec("_P")
+
+operators: dict[ast.AST, Callable[[Any, Any], Any]]
+
+def eval_expr(expr: str) -> Any: ...
+def eval_(node: ast.AST) -> Any: ...
+
+@dataclass(frozen=True)
+class _Sentinel(Generic[_T]):
+    default_value: _T
+    def __init__(self, default_value: _T) -> None: ...
+
+class _TracebackCapturingWrapper(Generic[_P, _T]):
+    func: Callable[_P, _T]
+    def __init__(self, func: Callable[_P, _T]) -> None: ...
+    def __call__(self, **kwargs: Any) -> _T: ...
