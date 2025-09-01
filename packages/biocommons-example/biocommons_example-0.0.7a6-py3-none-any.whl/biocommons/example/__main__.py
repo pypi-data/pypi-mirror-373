df7ceaa289aa13256f6901fd233d862b85cee9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:05:00 +0000

    Add package biocommons-example/0.0.7a6/biocommons_example-0.0.7a6-py3-none-any.whl

---
+++ packages/biocommons-example/biocommons_example-0.0.7a6-py3-none-any.whl/biocommons/example/__main__.py
@@ -0,0 +1,30 @@
+"""biocommons.example.__main__"""
+
+import logging
+import time
+
+from . import marvin
+
+_logger = logging.getLogger(__name__)
+
+
+def main():  # pragma: no cover
+    """marvin.example main"""
+    import coloredlogs  # noqa: PLC0415
+
+    logging.Formatter.converter = time.gmtime
+    coloredlogs.install(
+        level="INFO",
+        fmt="%(asctime)s [%(process)d] %(name)s:%(lineno)d %(levelname)s %(message)s",
+        datefmt="%FT%T.%f%z",
+    )
+
+    quote = marvin.get_quote()
+    _logger.warning("Got quote from Marvin (len=%s)", len(quote))
+
+    print("Marvin says:")
+    print(quote)
+
+
+if __name__ == "__main__":
+    main()
