b5ed7cfd38f62af40eac2290a1b6bb85909ee69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:09 +0000

    Add package clear-skies-doc-builder/2.0.0/clear_skies_doc_builder-2.0.0-py3-none-any.whl

---
+++ packages/clear-skies-doc-builder/clear_skies_doc_builder-2.0.0-py3-none-any.whl/clearskies_doc_builder/models/attribute_reference.py
@@ -0,0 +1,7 @@
+from clearskies.model import ModelClassReference
+from . import attribute
+
+
+class AttributeReference(ModelClassReference):
+    def get_model_class(self):
+        return attribute.Attribute
---
+++ packages/clear-skies-doc-builder/clear_skies_doc_builder-2.0.0-py3-none-any.whl/clearskies_doc_builder/models/module_reference.py
@@ -0,0 +1,7 @@
+from clearskies.model import ModelClassReference
+from . import module
+
+
+class ModuleReference(ModelClassReference):
+    def get_model_class(self):
+        return module.Module
