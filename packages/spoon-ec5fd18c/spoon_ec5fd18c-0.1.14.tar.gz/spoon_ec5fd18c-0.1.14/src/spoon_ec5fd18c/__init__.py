e1683671915743cd46e6ded075e71c3568aaa2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 14:05:51 +0000

    Add package spoon-ec5fd18c/0.1.14/spoon_ec5fd18c-0.1.14.tar.gz

---
+++ packages/spoon-ec5fd18c/spoon_ec5fd18c-0.1.14.tar.gz/spoon_ec5fd18c-0.1.14/src/spoon_ec5fd18c/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Yunqi Inc
+# SPDX-License-Identifier: Apache-2.0
+
+
+from ._lib import hello
+from ._version import __version__
+
+__all__ = [
+    "hello",
+    "__version__",
+]
