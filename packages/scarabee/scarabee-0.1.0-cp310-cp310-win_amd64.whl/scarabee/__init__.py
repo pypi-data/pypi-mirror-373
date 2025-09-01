f73cd983d955967a6abbba8c634529168c718bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:05:04 +0000

    Add package scarabee/0.1.0/scarabee-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/scarabee/scarabee-0.1.0-cp310-cp310-win_amd64.whl/scarabee/__init__.py
@@ -0,0 +1,9 @@
+from ._scarabee import *
+from . import _scarabee
+
+__author__ = _scarabee.__author__
+__copyright__ = _scarabee.__copyright__
+__license__ = _scarabee.__license__
+__maintainer__ = _scarabee.__maintainer__
+__email__ = _scarabee.__email__
+__version__ = _scarabee.__version__
