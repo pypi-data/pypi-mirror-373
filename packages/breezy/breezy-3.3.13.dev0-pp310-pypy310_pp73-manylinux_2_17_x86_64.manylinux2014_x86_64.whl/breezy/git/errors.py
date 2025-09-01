42b775540e73c2ae7f4eda39f0c2d16004d6a855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:04:42 +0000

    Add package breezy/3.3.13.dev0/breezy-3.3.13.dev0-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/breezy/breezy-3.3.13.dev0-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/breezy/git/errors.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2007 Canonical Ltd
+# Copyright (C) 2018 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""A grouping of Exceptions for bzr-git."""
+
+from .. import errors as brz_errors
+
+
+class BzrGitError(brz_errors.BzrError):
+    """The base-level exception for bzr-git errors."""
+
+
+class NoPushSupport(brz_errors.BzrError):
+    _fmt = (
+        "Push is not yet supported from %(source)r to %(target)r "
+        "using %(mapping)r for %(revision_id)r. Try dpush instead."
+    )
+
+    def __init__(self, source, target, mapping, revision_id=None):
+        self.source = source
+        self.target = target
+        self.mapping = mapping
+        self.revision_id = revision_id
+
+
+class GitSmartRemoteNotSupported(brz_errors.UnsupportedOperation):
+    _fmt = "This operation is not supported by the Git smart server protocol."
