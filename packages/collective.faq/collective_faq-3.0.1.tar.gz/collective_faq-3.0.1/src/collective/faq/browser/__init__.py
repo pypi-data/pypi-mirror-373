c9e0d3683b72784af440fefa44a9efd865a87700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 16:04:56 +0000

    Add package collective.faq/3.0.1/collective_faq-3.0.1.tar.gz

---
+++ packages/collective.faq/collective_faq-3.0.1.tar.gz/collective_faq-3.0.1/src/collective/faq/browser/__init__.py
@@ -0,0 +1,27 @@
+from collective.faq.interfaces import IFAQ
+from collective.faq.interfaces import IFAQItem
+from plone import api
+from plone.dexterity.browser.view import DefaultView
+
+
+class FAQView(DefaultView):
+    """Default view for FAQ content type."""
+
+    def questions(self, faq=None):
+        provides = [IFAQItem]
+        if not faq:
+            faq = self.context
+            provides += [IFAQ]
+        return api.content.find(
+            context=faq,
+            depth=1,
+            object_provides=provides,
+            sort_on="getObjPositionInParent",
+        )
+
+    def nested_item(self, obj):
+        return IFAQ.providedBy(obj)
+
+
+class FAQItemView(DefaultView):
+    """Default view for FAQ Item content type."""
