810b0d8e2e7754f486d90a6873155157a0e6594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 06:05:10 +0000

    Add package breezy/3.3.13/breezy-3.3.13-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/breezy/breezy-3.3.13-cp313-cp313-macosx_10_13_x86_64.whl/breezy/tests/blackbox/test_rmbranch.py
@@ -0,0 +1,103 @@
+# Copyright (C) 2010 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+"""Black-box tests for brz rmbranch."""
+
+from breezy import controldir
+from breezy.tests import TestCaseWithTransport
+
+
+class TestRemoveBranch(TestCaseWithTransport):
+    def example_tree(self, path=".", format=None):
+        tree = self.make_branch_and_tree(path, format=format)
+        self.build_tree_contents([(path + "/hello", b"foo")])
+        tree.add("hello")
+        tree.commit(message="setup")
+        self.build_tree_contents([(path + "/goodbye", b"baz")])
+        tree.add("goodbye")
+        tree.commit(message="setup")
+        return tree
+
+    def test_remove_local(self):
+        # Remove a local branch.
+        self.example_tree("a")
+        self.run_bzr_error(
+            ["Branch is active. Use --force to remove it.\n"], "rmbranch a"
+        )
+        self.run_bzr("rmbranch --force a")
+        dir = controldir.ControlDir.open("a")
+        self.assertFalse(dir.has_branch())
+        self.assertPathExists("a/hello")
+        self.assertPathExists("a/goodbye")
+
+    def test_no_branch(self):
+        # No branch in the current directory.
+        self.make_repository("a")
+        self.run_bzr_error(["Not a branch"], "rmbranch a")
+
+    def test_no_tree(self):
+        # removing the active branch is possible if there is no tree
+        tree = self.example_tree("a")
+        tree.controldir.destroy_workingtree()
+        self.run_bzr("rmbranch", working_dir="a")
+        dir = controldir.ControlDir.open("a")
+        self.assertFalse(dir.has_branch())
+
+    def test_no_arg(self):
+        # location argument defaults to current directory
+        self.example_tree("a")
+        self.run_bzr_error(
+            ["Branch is active. Use --force to remove it.\n"], "rmbranch a"
+        )
+        self.run_bzr("rmbranch --force", working_dir="a")
+        dir = controldir.ControlDir.open("a")
+        self.assertFalse(dir.has_branch())
+
+    def test_remove_colo(self):
+        # Remove a colocated branch.
+        tree = self.example_tree("a")
+        tree.controldir.create_branch(name="otherbranch")
+        self.assertTrue(tree.controldir.has_branch("otherbranch"))
+        self.run_bzr("rmbranch {},branch=otherbranch".format(tree.controldir.user_url))
+        dir = controldir.ControlDir.open("a")
+        self.assertFalse(dir.has_branch("otherbranch"))
+        self.assertTrue(dir.has_branch())
+
+    def test_remove_colo_directory(self):
+        # Remove a colocated branch.
+        tree = self.example_tree("a")
+        tree.controldir.create_branch(name="otherbranch")
+        self.assertTrue(tree.controldir.has_branch("otherbranch"))
+        self.run_bzr("rmbranch otherbranch -d {}".format(tree.controldir.user_url))
+        dir = controldir.ControlDir.open("a")
+        self.assertFalse(dir.has_branch("otherbranch"))
+        self.assertTrue(dir.has_branch())
+
+    def test_remove_active_colo_branch(self):
+        # Remove a colocated branch.
+        dir = self.make_repository("a").controldir
+        branch = dir.create_branch("otherbranch")
+        branch.create_checkout("a")
+        self.run_bzr_error(
+            ["Branch is active. Use --force to remove it.\n"],
+            "rmbranch otherbranch -d {}".format(branch.controldir.user_url),
+        )
+        self.assertTrue(dir.has_branch("otherbranch"))
+        self.run_bzr(
+            "rmbranch --force otherbranch -d {}".format(branch.controldir.user_url)
+        )
+        self.assertFalse(dir.has_branch("otherbranch"))
---
+++ packages/breezy/breezy-3.3.13-cp313-cp313-macosx_10_13_x86_64.whl/breezy/tests/per_tree/test_walkdirs.py
@@ -0,0 +1,133 @@
+# Copyright (C) 2006, 2007 Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Tests for the generic Tree.walkdirs interface."""
+
+import os
+
+from breezy import tests
+from breezy.mutabletree import MutableTree
+from breezy.osutils import supports_symlinks
+from breezy.tests.per_tree import TestCaseWithTree
+
+
+class TestWalkdirs(TestCaseWithTree):
+    def get_all_subdirs_expected(self, tree, symlinks):
+        empty_dirs_present = tree.has_versioned_directories() or isinstance(
+            tree, MutableTree
+        )
+        empty_dirs_are_versioned = tree.has_versioned_directories()
+        dirblocks = {}
+
+        dirblocks[""] = [
+            ("0file", "0file", "file", None, "file"),
+            ("1top-dir", "1top-dir", "directory", None, "directory"),
+            ("2utf\u1234file", "2utf\u1234file", "file", None, "file"),
+        ]
+
+        dirblocks["1top-dir"] = [
+            ("1top-dir/0file-in-1topdir", "0file-in-1topdir", "file", None, "file")
+        ]
+        if empty_dirs_present:
+            dirblocks["1top-dir"].append(
+                (
+                    "1top-dir/1dir-in-1topdir",
+                    "1dir-in-1topdir",
+                    "directory",
+                    None
+                    if empty_dirs_are_versioned
+                    else os.stat(tree.abspath("1top-dir/1dir-in-1topdir")),
+                    "directory" if empty_dirs_are_versioned else None,
+                )
+            )
+            dirblocks["1top-dir/1dir-in-1topdir"] = []
+        if symlinks:
+            dirblocks[""].append(("symlink", "symlink", "symlink", None, "symlink"))
+        return [
+            (path, sorted(entries)) for (path, entries) in sorted(dirblocks.items())
+        ]
+
+    def test_walkdir_root(self):
+        tree = self.get_tree_with_subdirs_and_all_supported_content_types(
+            supports_symlinks(self.test_dir)
+        )
+        with tree.lock_read():
+            expected_dirblocks = self.get_all_subdirs_expected(
+                tree, supports_symlinks(self.test_dir)
+            )
+            # test that its iterable by iterating
+            result = []
+            for dirinfo, block in tree.walkdirs():
+                newblock = []
+                for row in block:
+                    if row[4] is not None:
+                        newblock.append(row[0:3] + (None,) + row[4:])
+                    else:
+                        newblock.append(row)
+                result.append((dirinfo, newblock))
+        # check each return value for debugging ease.
+        for pos, item in enumerate(expected_dirblocks):
+            self.assertEqual(item, result[pos])
+        self.assertEqual(len(expected_dirblocks), len(result))
+
+    def test_walkdir_subtree(self):
+        tree = self.get_tree_with_subdirs_and_all_supported_content_types(
+            supports_symlinks(self.test_dir)
+        )
+        # test that its iterable by iterating
+        result = []
+        with tree.lock_read():
+            expected_dirblocks = self.get_all_subdirs_expected(
+                tree, supports_symlinks(self.test_dir)
+            )[1:]
+            for dirinfo, block in tree.walkdirs("1top-dir"):
+                newblock = []
+                for row in block:
+                    if row[4] is not None:
+                        newblock.append(row[0:3] + (None,) + row[4:])
+                    else:
+                        newblock.append(row)
+                result.append((dirinfo, newblock))
+        # check each return value for debugging ease.
+        for pos, item in enumerate(expected_dirblocks):
+            self.assertEqual(item, result[pos])
+        self.assertEqual(len(expected_dirblocks), len(result))
+
+    def test_walkdir_versioned_kind(self):
+        work_tree = self.make_branch_and_tree("tree")
+        self.build_tree(["tree/file", "tree/dir/"])
+        work_tree.add(["file", "dir"])
+        os.unlink("tree/file")
+        os.rmdir("tree/dir")
+        tree = self._convert_tree(work_tree)
+        tree.lock_read()
+        self.addCleanup(tree.unlock)
+        if not tree.supports_file_ids or tree.path2id("file") is None:
+            raise tests.TestNotApplicable("Tree type cannot represent dangling ids.")
+        expected = [
+            (
+                "",
+                (
+                    [("dir", "dir", "unknown", None, "directory")]
+                    if tree.has_versioned_directories()
+                    else []
+                )
+                + [("file", "file", "unknown", None, "file")],
+            )
+        ]
+        if tree.has_versioned_directories():
+            expected.append(("dir", []))
+        self.assertEqual(expected, list(tree.walkdirs()))
