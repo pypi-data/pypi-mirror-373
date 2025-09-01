477fb2df9bdda27147f67d53a72dfa27148e2912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 15:04:44 +0000

    Add package rite/0.0.6/rite-0.0.6.tar.gz

---
+++ packages/rite/rite-0.0.6.tar.gz/rite-0.0.6/src/rite/string/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+from .case import Case
+from .cipher import Cipher
+from .morse import Morse
+from .slug import Slug
+from .url import URL
+from .uuid.uuid import UUID
---
+++ packages/rite/rite-0.0.6.tar.gz/rite-0.0.6/src/rite/string/url.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides File Utils
+
+...
+
+Examples:
+    ...
+
+Attributes:
+    ...
+
+Todo:
+
+"""
+
+
+# Import | Standard Library
+import re
+import unicodedata
+
+# Import | Libraries
+
+# Import | Local Modules
+
+
+class URL(object):
+    """"""
+
+    # Static Methods
+
+    @staticmethod
+    def is_string_an_url(url_string: str) -> bool:
+        result = validators.url(url_string)
+
+        if isinstance(result, ValidationFailure):
+            return False
+
+        return result
