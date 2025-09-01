d70b43bb2d117a86f5da8b4cd8c8f2cc8856cc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-01 03:29:45 +0000

    Add package pdm-polylith-workspace/1.3.3/pdm_polylith_workspace-1.3.3.tar.gz

---
+++ packages/pdm-polylith-workspace/pdm_polylith_workspace-1.3.3.tar.gz/pdm_polylith_workspace-1.3.3/pdm_polylith_workspace/polylith/pdm_workspace_hooks/core.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from pdm_polylith_workspace.polylith.pdm.hooks.workspace import build_initialize
+
+def pdm_build_initialize(context):
+    """Adding an additional pth file to the virtual environment
+
+    Making the virtual environment aware of the Polylith Workspace.
+    """
+    context.ensure_build_dir()
+    data = context.config.data
+    build_dir = Path(context.build_dir)
+    root = Path(context.config.root)
+    build_initialize(data, build_dir, root)
