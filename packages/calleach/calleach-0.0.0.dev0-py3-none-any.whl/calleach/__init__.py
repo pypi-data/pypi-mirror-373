8555dc67054453b836f0f241607cde5a59c14fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:05:36 +0000

    Add package calleach/0.0.0.dev0/calleach-0.0.0.dev0-py3-none-any.whl

---
+++ packages/calleach/calleach-0.0.0.dev0-py3-none-any.whl/calleach/__init__.py
@@ -0,0 +1,5 @@
+from calleach.core import *
+from calleach.tests import *
+
+if __name__ == "__main__":
+    main()
---
+++ packages/calleach/calleach-0.0.0.dev0-py3-none-any.whl/calleach/__main__.py
@@ -0,0 +1,4 @@
+from calleach import main
+
+if __name__ == "__main__":
+    main()
