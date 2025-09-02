21596b3e6585680a39eb506f6b90afabe9fae19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:13 +0000

    Add package Chillax/0.0.4/chillax-0.0.4-py3-none-any.whl

---
+++ packages/Chillax/chillax-0.0.4-py3-none-any.whl/chillax/core.py
@@ -0,0 +1,25 @@
+import os
+import google.generativeai as genai
+
+
+def _ask(prompt: str) -> str:
+    """Send a query to Gemini and return the response."""
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+    return response.text
+
+def setAPIKey(api):
+    if api:
+        genai.configure(api_key=api)
+    else:
+        raise RuntimeError(
+            "API key not found. please enter the correct api "
+        )
+
+class Chillax:
+    def __getattr__(self, name):
+        def wrapper(*args, **kwargs):
+            return _ask(f"Perform `{name}` with args={args}, kwargs={kwargs} , just give the answer do not explain")
+        return wrapper
+
+chillax = Chillax()
