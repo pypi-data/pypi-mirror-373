834a34e9e83e49e4578dec88f1cc9cf2d56293ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:06:04 +0000

    Add package simple-sdk-axp-v1/0.0.1/simple_sdk_axp_v1-0.0.1-py3-none-any.whl

---
+++ packages/simple-sdk-axp-v1/simple_sdk_axp_v1-0.0.1-py3-none-any.whl/simple_sdk/__init__.py
@@ -0,0 +1,5 @@
+# src/simple_sdk/__init__.py
+
+from .calculator import add, subtract
+
+__version__ = "0.0.1"
---
+++ packages/simple-sdk-axp-v1/simple_sdk_axp_v1-0.0.1-py3-none-any.whl/simple_sdk/calculator.py
@@ -0,0 +1,9 @@
+# src/simple_sdk/calculator.py
+
+def add(a, b):
+    """두 숫자를 더한 값을 반환합니다."""
+    return a + b
+
+def subtract(a, b):
+    """두 숫자를 뺀 값을 반환합니다."""
+    return a - b
