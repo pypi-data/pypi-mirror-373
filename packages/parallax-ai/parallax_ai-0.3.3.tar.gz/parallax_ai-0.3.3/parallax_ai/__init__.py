58dd19ceb238529c4adaf3a82258b2b4a3668d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 16:05:50 +0000

    Add package parallax-ai/0.3.3/parallax_ai-0.3.3.tar.gz

---
+++ packages/parallax-ai/parallax_ai-0.3.3.tar.gz/parallax_ai-0.3.3/parallax_ai/__init__.py
@@ -0,0 +1,6 @@
+"""Parallax - A package for parallel inference."""
+
+__version__ = "0.3.2"
+
+# from parallax_ai.clients import ParallaxOpenAIClient
+# from parallax_ai.agents import JSONOutputAgent, KeywordOutputAgent, ClassificationAgent
