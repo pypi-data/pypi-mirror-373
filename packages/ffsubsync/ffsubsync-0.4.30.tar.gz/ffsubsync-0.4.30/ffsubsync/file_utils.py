ef708702eb7360742b3e6fba21e6aad9fdb7e543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 06:04:52 +0000

    Add package ffsubsync/0.4.30/ffsubsync-0.4.30.tar.gz

---
+++ packages/ffsubsync/ffsubsync-0.4.30.tar.gz/ffsubsync-0.4.30/ffsubsync/file_utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sys
+
+
+class open_file:
+    """
+    Context manager that opens a filename and closes it on exit, but does
+    nothing for file-like objects.
+    """
+
+    def __init__(self, filename, *args, **kwargs) -> None:
+        self.closing = kwargs.pop("closing", False)
+        if filename is None:
+            stream = sys.stdout if "w" in args else sys.stdin
+            self.fh = open(stream.fileno(), *args, **kwargs)
+        elif isinstance(filename, str):
+            self.fh = open(filename, *args, **kwargs)
+            self.closing = True
+        else:
+            self.fh = filename
+
+    def __enter__(self):
+        return self.fh
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.closing:
+            self.fh.close()
+
+        return False
