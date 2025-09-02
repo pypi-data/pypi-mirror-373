767f66799776d770d33bc513df043388db91622b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:04:57 +0000

    Add package ut-xls/1.3.1.20250901/ut_xls-1.3.1.20250901-py3-none-any.whl

---
+++ packages/ut-xls/ut_xls-1.3.1.20250901-py3-none-any.whl/ut_xls/op/ioopathnmwb.py
@@ -0,0 +1,38 @@
+from typing import Any, TypeAlias
+
+import openpyxl as op
+
+from ut_path.pathnm import PathNm
+
+from ut_xls.op.ioopathwb import IooPathWb
+
+TyWb: TypeAlias = op.workbook.workbook.Workbook
+
+TyArr = list[Any]
+TyDic = dict[Any, Any]
+TyAoA = list[TyArr]
+TyAoD = list[TyDic]
+TyDoAoA = dict[Any, TyAoA]
+TyDoAoD = dict[Any, TyAoD]
+TyPath = str
+TyPathnm = str
+TySheet = int | str
+
+TnWb = None | TyWb
+
+
+class IooPathnmWb:
+
+    @staticmethod
+    def write(
+            wb: TnWb, pathnm: TyPathnm, **kwargs) -> None:
+        _path: TyPath = PathNm.sh_path(pathnm, kwargs)
+        IooPathWb.write(wb, _path)
+
+    @staticmethod
+    def write_wb_from_doaod(
+            doaod: TyDoAoD, pathnm: str, **kwargs) -> None:
+        if not doaod:
+            return
+        _path: TyPath = PathNm.sh_path(pathnm, kwargs)
+        IooPathWb.write_wb_from_doaod(doaod, _path)
