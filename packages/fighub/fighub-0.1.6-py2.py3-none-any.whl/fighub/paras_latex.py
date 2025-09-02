96dc138b23b2c034caa5c9da6b43b2ff0bd6d67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:05:59 +0000

    Add package fighub/0.1.6/fighub-0.1.6-py2.py3-none-any.whl

---
+++ packages/fighub/fighub-0.1.6-py2.py3-none-any.whl/fighub/paras_latex.py
@@ -0,0 +1,17 @@
+
+
+def param_latex_map():
+    param_latex_map = {
+            "_alpha_": r"$\alpha$",
+            "_epsilon_": r"$\epsilon$",
+            "_beta1_": r"$\beta_1$",
+            "_beta2_": r"$\beta_2$",
+            "_M_": "$M$",
+            "_c_": "$c$",
+            "_eta_max_": r"$\eta$",
+            "_gamma_": r"$\gamma$",
+            "_delta_": r"$\delta$",
+            "_cutting_number_": "$m$",
+        }
+
+    return param_latex_map
