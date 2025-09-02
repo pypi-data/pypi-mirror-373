eadd52dfc58cf20a57864a21c122f092b48aec4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:06:28 +0000

    Add package pypika-tortoise/0.6.2/pypika_tortoise-0.6.2.tar.gz

---
+++ packages/pypika-tortoise/pypika_tortoise-0.6.2.tar.gz/pypika_tortoise-0.6.2/tests/test_parameter.py
@@ -0,0 +1,235 @@
+import unittest
+from datetime import date
+
+from pypika_tortoise import Parameter, Query, Tables, ValueWrapper
+from pypika_tortoise.context import DEFAULT_SQL_CONTEXT
+from pypika_tortoise.dialects.mssql import MSSQLQuery
+from pypika_tortoise.dialects.mysql import MySQLQuery
+from pypika_tortoise.dialects.oracle import OracleQuery
+from pypika_tortoise.dialects.postgresql import PostgreSQLQuery
+from pypika_tortoise.dialects.sqlite import SQLLiteQuery
+from pypika_tortoise.functions import Upper
+from pypika_tortoise.terms import Case, Parameterizer
+
+
+class ParametrizedTests(unittest.TestCase):
+    table_abc, table_efg = Tables("abc", "efg")
+
+    def test_param_insert(self):
+        q = (
+            Query.into(self.table_abc)
+            .columns("a", "b", "c")
+            .insert(Parameter("?"), Parameter("?"), Parameter("?"))
+        )
+
+        self.assertEqual('INSERT INTO "abc" ("a","b","c") VALUES (?,?,?)', q.get_sql())
+
+    def test_param_select_join(self):
+        q = (
+            Query.from_(self.table_abc)
+            .select("*")
+            .where(self.table_abc.category == Parameter("%s"))
+            .join(self.table_efg)
+            .on(self.table_abc.id == self.table_efg.abc_id)
+            .where(self.table_efg.date >= Parameter("%s"))
+            .limit(10)
+        )
+
+        self.assertEqual(
+            'SELECT * FROM "abc" JOIN "efg" ON "abc"."id"="efg"."abc_id" '
+            'WHERE "abc"."category"=%s AND "efg"."date">=%s LIMIT 10',
+            q.get_sql(),
+        )
+
+    def test_param_select_subquery(self):
+        q = (
+            Query.from_(self.table_abc)
+            .select("*")
+            .where(self.table_abc.category == Parameter("&1"))
+            .where(
+                self.table_abc.id.isin(
+                    Query.from_(self.table_efg)
+                    .select(self.table_efg.abc_id)
+                    .where(self.table_efg.date >= Parameter("&2"))
+                )
+            )
+            .limit(10)
+        )
+
+        self.assertEqual(
+            'SELECT * FROM "abc" WHERE "category"=&1 AND "id" IN '
+            '(SELECT "abc_id" FROM "efg" WHERE "date">=&2) LIMIT 10',
+            q.get_sql(),
+        )
+
+    def test_join(self):
+        subquery = (
+            Query.from_(self.table_efg)
+            .select(self.table_efg.fiz, self.table_efg.buz)
+            .where(self.table_efg.buz == Parameter(":buz"))
+        )
+
+        q = (
+            Query.from_(self.table_abc)
+            .join(subquery)
+            .on(self.table_abc.bar == subquery.buz)
+            .select(self.table_abc.foo, subquery.fiz)
+            .where(self.table_abc.bar == Parameter(":bar"))
+        )
+
+        self.assertEqual(
+            'SELECT "abc"."foo","sq0"."fiz" FROM "abc" JOIN (SELECT "fiz","buz" FROM "efg" WHERE "buz"=:buz)'
+            ' "sq0" ON "abc"."bar"="sq0"."buz" WHERE "abc"."bar"=:bar',
+            q.get_sql(),
+        )
+
+    def test_qmark_parameter(self):
+        self.assertEqual("?", Parameter("?").get_sql(DEFAULT_SQL_CONTEXT))
+
+    def test_oracle(self):
+        self.assertEqual("?", Parameter(idx=1).get_sql(OracleQuery.SQL_CONTEXT))
+        self.assertEqual("?", Parameter(idx=2).get_sql(OracleQuery.SQL_CONTEXT))
+
+    def test_mssql(self):
+        self.assertEqual("?", Parameter(idx=1).get_sql(MSSQLQuery.SQL_CONTEXT))
+        self.assertEqual("?", Parameter(idx=2).get_sql(MSSQLQuery.SQL_CONTEXT))
+
+    def test_mysql(self):
+        self.assertEqual("%s", Parameter(idx=1).get_sql(MySQLQuery.SQL_CONTEXT))
+        self.assertEqual("%s", Parameter(idx=2).get_sql(MySQLQuery.SQL_CONTEXT))
+
+    def test_postgres(self):
+        self.assertEqual("$1", Parameter(idx=1).get_sql(PostgreSQLQuery.SQL_CONTEXT))
+        self.assertEqual("$2", Parameter(idx=2).get_sql(PostgreSQLQuery.SQL_CONTEXT))
+
+    def test_sqlite(self):
+        self.assertEqual("?", Parameter(idx=1).get_sql(SQLLiteQuery.SQL_CONTEXT))
+        self.assertEqual("?", Parameter(idx=2).get_sql(SQLLiteQuery.SQL_CONTEXT))
+
+
+class ParameterizerTests(unittest.TestCase):
+    table_abc, table_efg = Tables("abc", "efg")
+
+    def test_param_insert(self):
+        q = Query.into(self.table_abc).columns("a", "b", "c").insert(1, 2.2, "foo")
+
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual('INSERT INTO "abc" ("a","b","c") VALUES (?,?,?)', sql)
+        self.assertEqual([1, 2.2, "foo"], values)
+
+    def test_select_join_in_mysql(self):
+        q = (
+            MySQLQuery.from_(self.table_abc)
+            .select("*")
+            .where(self.table_abc.category == "foobar")
+            .join(self.table_efg)
+            .on(self.table_abc.id == self.table_efg.abc_id)
+            .where(self.table_efg.date >= date(2024, 2, 22))
+            .limit(10)
+        )
+
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual(
+            "SELECT * FROM `abc` JOIN `efg` ON `abc`.`id`=`efg`.`abc_id` WHERE `abc`.`category`=%s AND `efg`.`date`>=%s LIMIT %s",
+            sql,
+        )
+        self.assertEqual(["foobar", date(2024, 2, 22), 10], values)
+
+    def test_select_subquery_in_postgres(self):
+        q = (
+            PostgreSQLQuery.from_(self.table_abc)
+            .select("*")
+            .where(self.table_abc.category == "foobar")
+            .where(
+                self.table_abc.id.isin(
+                    Query.from_(self.table_efg)
+                    .select(self.table_efg.abc_id)
+                    .where(self.table_efg.date >= date(2024, 2, 22))
+                )
+            )
+            .limit(10)
+        )
+
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual(
+            'SELECT * FROM "abc" WHERE "category"=$1 AND "id" IN (SELECT "abc_id" FROM "efg" WHERE "date">=$2) LIMIT $3',
+            sql,
+        )
+        self.assertEqual(["foobar", date(2024, 2, 22), 10], values)
+
+    def test_join_in_postgres(self):
+        subquery = (
+            Query.from_(self.table_efg)
+            .select(self.table_efg.fiz, self.table_efg.buz)
+            .where(self.table_efg.buz == "buz")
+        )
+
+        q = (
+            PostgreSQLQuery.from_(self.table_abc)
+            .join(subquery)
+            .on(self.table_abc.bar == subquery.buz)
+            .select(self.table_abc.foo, subquery.fiz)
+            .where(self.table_abc.bar == "bar")
+        )
+
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual(
+            'SELECT "abc"."foo","sq0"."fiz" FROM "abc" JOIN (SELECT "fiz","buz" FROM "efg" WHERE "buz"=$1)'
+            ' "sq0" ON "abc"."bar"="sq0"."buz" WHERE "abc"."bar"=$2',
+            sql,
+        )
+        self.assertEqual(["buz", "bar"], values)
+
+    def test_function_parameter(self):
+        q = (
+            Query.from_(self.table_abc)
+            .select("*")
+            .where(self.table_abc.category == Upper(ValueWrapper("foobar")))
+        )
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual('SELECT * FROM "abc" WHERE "category"=UPPER(?)', sql)
+
+        self.assertEqual(["foobar"], values)
+
+    def test_case_when_in_select(self):
+        q = Query.from_(self.table_abc).select(
+            Case().when(self.table_abc.category == "foobar", 1).else_(2)
+        )
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual('SELECT CASE WHEN "category"=? THEN ? ELSE ? END FROM "abc"', sql)
+        self.assertEqual(["foobar", 1, 2], values)
+
+    def test_case_when_in_where(self):
+        q = (
+            Query.from_(self.table_abc)
+            .select("*")
+            .where(
+                self.table_abc.category_int
+                > Case().when(self.table_abc.category == "foobar", 1).else_(2)
+            )
+        )
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual(
+            'SELECT * FROM "abc" WHERE "category_int">CASE WHEN "category"=? THEN ? ELSE ? END',
+            sql,
+        )
+        self.assertEqual(["foobar", 1, 2], values)
+
+    def test_limit_and_offest(self):
+        q = Query.from_(self.table_abc).select("*").limit(10).offset(5)
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual('SELECT * FROM "abc" LIMIT ? OFFSET ?', sql)
+        self.assertEqual([10, 5], values)
+
+    def test_limit_and_offest_in_mssql(self):
+        q = MSSQLQuery.from_(self.table_abc).select("*").limit(10).offset(5)
+        sql, values = q.get_parameterized_sql()
+        self.assertEqual(
+            'SELECT * FROM "abc" ORDER BY (SELECT 0) OFFSET ? ROWS FETCH NEXT ? ROWS ONLY', sql
+        )
+        self.assertEqual([5, 10], values)
+
+    def test_placeholder_factory(self):
+        parameterizer = Parameterizer(placeholder_factory=lambda _: "%s")
+        param = parameterizer.create_param(1)
+        self.assertEqual("%s", param.get_sql(PostgreSQLQuery.SQL_CONTEXT))
---
+++ packages/pypika-tortoise/pypika_tortoise-0.6.2.tar.gz/pypika_tortoise-0.6.2/tests/test_query.py
@@ -0,0 +1,215 @@
+import unittest
+from copy import copy
+
+from pypika_tortoise import Case, Query, Tables, Tuple, functions
+from pypika_tortoise.dialects import (
+    MySQLLoadQueryBuilder,
+    MySQLQuery,
+    MySQLQueryBuilder,
+    PostgreSQLQuery,
+    PostgreSQLQueryBuilder,
+    SQLLiteQuery,
+    SQLLiteQueryBuilder,
+)
+from pypika_tortoise.queries import CreateQueryBuilder, DropQueryBuilder, QueryBuilder, Table
+
+
+class QueryTablesTests(unittest.TestCase):
+    table_a, table_b, table_c, table_d = Tables("a", "b", "c", "d")
+
+    def test_replace_table(self):
+        query = Query.from_(self.table_a).select(self.table_a.time)
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('SELECT "time" FROM "b"', str(query))
+
+    def test_replace_only_specified_table(self):
+        query = Query.from_(self.table_a).select(self.table_a.time)
+        query = query.replace_table(self.table_b, self.table_c)
+
+        self.assertEqual('SELECT "time" FROM "a"', str(query))
+
+    def test_replace_insert_table(self):
+        query = Query.into(self.table_a).insert(1)
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('INSERT INTO "b" VALUES (1)', str(query))
+
+    def test_replace_insert_table_current_table_not_match(self):
+        query = Query.into(self.table_a).insert(1)
+        query = query.replace_table(self.table_c, self.table_b)
+
+        self.assertEqual('INSERT INTO "a" VALUES (1)', str(query))
+
+    def test_replace_update_table(self):
+        query = Query.update(self.table_a).set("foo", "bar")
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('UPDATE "b" SET "foo"=\'bar\'', str(query))
+
+    def test_replace_update_table_current_table_not_match(self):
+        query = Query.update(self.table_a).set("foo", "bar")
+        query = query.replace_table(self.table_c, self.table_b)
+
+        self.assertEqual('UPDATE "a" SET "foo"=\'bar\'', str(query))
+
+    def test_replace_delete_table(self):
+        query = Query.from_(self.table_a).delete()
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('DELETE FROM "b"', str(query))
+
+    def test_replace_join_tables(self):
+        query = (
+            Query.from_(self.table_a)
+            .join(self.table_b)
+            .on(self.table_a.customer_id == self.table_b.id)
+            .join(self.table_c)
+            .on(self.table_b.seller_id == self.table_c.id)
+            .select(self.table_a.star)
+        )
+        query = query.replace_table(self.table_a, self.table_d)
+
+        self.assertEqual(
+            'SELECT "d".* '
+            'FROM "d" '
+            'JOIN "b" ON "d"."customer_id"="b"."id" '
+            'JOIN "c" ON "b"."seller_id"="c"."id"',
+            str(query),
+        )
+
+    def test_replace_filter_tables(self):
+        query = (
+            Query.from_(self.table_a)
+            .select(self.table_a.name)
+            .where(self.table_a.name == "Mustermann")
+        )
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('SELECT "name" FROM "b" WHERE "name"=\'Mustermann\'', str(query))
+
+    def test_replace_having_table(self):
+        query = (
+            Query.from_(self.table_a)
+            .select(functions.Sum(self.table_a.revenue))
+            .groupby(self.table_a.customer)
+            .having(functions.Sum(self.table_a.revenue) >= 1000)
+        )
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual(
+            'SELECT SUM("revenue") FROM "b" GROUP BY "customer" HAVING SUM("revenue")>=1000',
+            str(query),
+        )
+
+    def test_replace_case_table(self):
+        query = Query.from_(self.table_a).select(
+            Case()
+            .when(self.table_a.fname == "Tom", "It was Tom")
+            .when(self.table_a.fname == "John", "It was John")
+            .else_("It was someone else.")
+            .as_("who_was_it")
+        )
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual(
+            "SELECT CASE "
+            "WHEN \"fname\"='Tom' THEN 'It was Tom' "
+            "WHEN \"fname\"='John' THEN 'It was John' "
+            "ELSE 'It was someone else.' END \"who_was_it\" "
+            'FROM "b"',
+            str(query),
+        )
+
+    def test_replace_orderby_table(self):
+        query = (
+            Query.from_(self.table_a).select(self.table_a.customer).orderby(self.table_a.customer)
+        )
+        query = query.replace_table(self.table_a, self.table_b)
+
+        self.assertEqual('SELECT "customer" FROM "b" ORDER BY "customer"', str(query))
+
+    def test_replace_tuple_table(self):
+        query = (
+            Query.from_(self.table_a)
+            .select(self.table_a.cost, self.table_a.revenue)
+            .where((self.table_a.cost, self.table_a.revenue) == Tuple(1, 2))
+        )
+
+        query = query.replace_table(self.table_a, self.table_b)
+
+        # Order is reversed due to lack of right equals method
+        self.assertEqual(
+            'SELECT "cost","revenue" FROM "b" WHERE (1,2)=("cost","revenue")',
+            str(query),
+        )
+
+    def test_is_joined(self):
+        q = Query.from_(self.table_a).join(self.table_b).on(self.table_a.foo == self.table_b.boo)
+
+        self.assertTrue(q.is_joined(self.table_b))
+        self.assertFalse(q.is_joined(self.table_c))
+
+
+class QueryBuilderTests(unittest.TestCase):
+    def test_query_builders_have_reference_to_correct_query_class(self):
+        with self.subTest("QueryBuilder"):
+            self.assertEqual(Query, QueryBuilder.QUERY_CLS)
+
+        with self.subTest("DropQueryBuilder"):
+            self.assertEqual(Query, DropQueryBuilder.QUERY_CLS)
+
+        with self.subTest("CreateQueryBuilder"):
+            self.assertEqual(Query, CreateQueryBuilder.QUERY_CLS)
+
+        with self.subTest("MySQLQueryBuilder"):
+            self.assertEqual(MySQLQuery, MySQLQueryBuilder.QUERY_CLS)
+
+        with self.subTest("MySQLLoadQueryBuilder"):
+            self.assertEqual(MySQLQuery, MySQLLoadQueryBuilder.QUERY_CLS)
+
+        with self.subTest("PostgreSQLQueryBuilder"):
+            self.assertEqual(PostgreSQLQuery, PostgreSQLQueryBuilder.QUERY_CLS)
+
+        with self.subTest("SQLLiteQueryBuilder"):
+            self.assertEqual(SQLLiteQuery, SQLLiteQueryBuilder.QUERY_CLS)
+
+    def test_query_builder_copy(self):
+        qb = QueryBuilder()
+        qb2 = copy(qb)
+
+        self.assertIsNot(qb, qb2)
+        self.assertEqual(qb, qb2)
+        self.assertEqual(qb._select_star_tables, qb2._select_star_tables)
+        self.assertIsNot(qb._select_star_tables, qb2._select_star_tables)
+        self.assertEqual(qb._from, qb2._from)
+        self.assertIsNot(qb._from, qb2._from)
+        self.assertEqual(qb._with, qb2._with)
+        self.assertIsNot(qb._with, qb2._with)
+        self.assertEqual(qb._selects, qb2._selects)
+        self.assertIsNot(qb._selects, qb2._selects)
+        self.assertEqual(qb._columns, qb2._columns)
+        self.assertIsNot(qb._columns, qb2._columns)
+        self.assertEqual(qb._values, qb2._values)
+        self.assertIsNot(qb._values, qb2._values)
+        self.assertEqual(qb._groupbys, qb2._groupbys)
+        self.assertIsNot(qb._groupbys, qb2._groupbys)
+        self.assertEqual(qb._orderbys, qb2._orderbys)
+        self.assertIsNot(qb._orderbys, qb2._orderbys)
+        self.assertEqual(qb._joins, qb2._joins)
+        self.assertIsNot(qb._joins, qb2._joins)
+        self.assertEqual(qb._unions, qb2._unions)
+        self.assertIsNot(qb._unions, qb2._unions)
+        self.assertEqual(qb._updates, qb2._updates)
+        self.assertIsNot(qb._updates, qb2._updates)
+        self.assertEqual(qb._on_conflict_fields, qb2._on_conflict_fields)
+        self.assertIsNot(qb._on_conflict_fields, qb2._on_conflict_fields)
+        self.assertEqual(qb._on_conflict_do_updates, qb2._on_conflict_do_updates)
+        self.assertIsNot(qb._on_conflict_do_updates, qb2._on_conflict_do_updates)
+
+    def test_get_parameterized_sql(self):
+        table = Table("abc")
+        q = QueryBuilder().from_(table).select("foo").where(table.bar == 1)
+        sql, params = q.get_parameterized_sql()
+        self.assertEqual('SELECT "foo" FROM "abc" WHERE "bar"=?', sql)
+        self.assertEqual([1], params)
