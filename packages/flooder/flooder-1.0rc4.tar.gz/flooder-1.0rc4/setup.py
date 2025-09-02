3f98fafc1288c9524dbb0ee27211813e8c8eff64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 07:05:17 +0000

    Add package flooder/1.0rc4/flooder-1.0rc4.tar.gz

---
+++ packages/flooder/flooder-1.0rc4.tar.gz/flooder-1.0rc4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flooder",
+    version="1.0rc4",
+    description="Flood complex PH",
+    author="Paolo Pellizzoni, Florian Graf, Martin Uray, Stefan Huber, Roland Kwitt",
+    author_email="roland.kwitt@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
