e24d9331ec7b3ce670e9bfab9953c214cffee3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 16:05:16 +0000

    Add package richie/3.2.1.dev1/richie-3.2.1.dev1.tar.gz

---
+++ packages/richie/richie-3.2.1.dev1.tar.gz/richie-3.2.1.dev1/src/frontend/js/components/SaleTunnel/index.tsx
@@ -0,0 +1,41 @@
+import { ModalProps } from '@openfun/cunningham-react';
+import {
+  CertificateProduct,
+  CourseLight,
+  Offering,
+  CredentialProduct,
+  Enrollment,
+  Order,
+  Organization,
+  Product,
+  ProductType,
+} from 'types/Joanie';
+import { CredentialSaleTunnel } from 'components/SaleTunnel/CredentialSaleTunnel';
+import { CertificateSaleTunnel } from 'components/SaleTunnel/CertificateSaleTunnel';
+import { PacedCourse } from 'types';
+
+export interface SaleTunnelProps extends Pick<ModalProps, 'isOpen' | 'onClose'> {
+  product: Product;
+  offering?: Offering;
+  organizations?: Organization[];
+  isWithdrawable: boolean;
+  course?: PacedCourse | CourseLight;
+  enrollment?: Enrollment;
+  onFinish?: (order: Order) => void;
+}
+
+/**
+ * This is just a simple wrapper that will render the correct SaleTunnel based on the product type.
+ */
+export const SaleTunnel = (props: SaleTunnelProps) => {
+  return (
+    <>
+      {props.product.type === ProductType.CREDENTIAL && (
+        <CredentialSaleTunnel {...props} product={props.product as CredentialProduct} />
+      )}
+      {props.product.type === ProductType.CERTIFICATE && (
+        <CertificateSaleTunnel {...props} product={props.product as CertificateProduct} />
+      )}
+    </>
+  );
+};
