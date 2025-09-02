f092b87edbce7ad1902573e2d248c718effb1f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 07:05:10 +0000

    Add package FlowerPower/0.21.0/flowerpower-0.21.0.tar.gz

---
+++ packages/FlowerPower/flowerpower-0.21.0.tar.gz/flowerpower-0.21.0/src/flowerpower/plugins/mqtt/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The flowerpower.plugins.mqtt module is deprecated. "
+    "Please use 'flowerpower-mqtt' instead. Install it with 'pip install flowerpower-mqtt'.",
+    DeprecationWarning,
+    stacklevel=2,
+)
