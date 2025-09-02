67ea673f831e9afb57d22e4d4e33bd75224c8121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 14:05:32 +0000

    Add package stereo-toolbox/0.4.0/stereo_toolbox-0.4.0.tar.gz

---
+++ packages/stereo-toolbox/stereo_toolbox-0.4.0.tar.gz/stereo_toolbox-0.4.0/stereo_toolbox/datasets/__init__.py
@@ -0,0 +1,17 @@
+from .sceneflow import SceneFlow_Dataset
+from .kitti import KITTI2015_Dataset, KITTI2012_Dataset
+from .middleburyeval3 import MiddleburyEval3_Dataset
+from .eth3d import ETH3D_Dataset
+from .drivingstereo import DrivingStereo_Dataset
+from .middlebury2021 import Middlebury2021_Dataset
+from .sintel import Sintel_Dataset
+from .hr_vs import HR_VS_Dataset
+from .booster import Booster_Dataset
+from .instereo2k import InStereo2k_Dataset
+from .crestereo import CREStereo_Dataset
+from .argoverse import Argoverse_Dataset
+from .holopix50k import Holopix50k_Dataset
+from .monotrap import MonoTrap_Dataset
+from .fallingthings import FallingThings_Dataset
+from .virtualkitti2 import VirtualKITTI2_Dataset
+from .layeredflow import LayeredFlow_Dataset
