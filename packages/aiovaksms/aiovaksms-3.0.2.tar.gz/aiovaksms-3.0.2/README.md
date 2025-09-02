647d60efcaf06517a8fba593e6070f8cbe1eda80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:05:08 +0000

    Add package aiovaksms/3.0.2/aiovaksms-3.0.2.tar.gz

---
+++ packages/aiovaksms/aiovaksms-3.0.2.tar.gz/aiovaksms-3.0.2/README.md
@@ -0,0 +1,214 @@
+# VAKSMS API for Python 3
+
+<div align="center">
+
+[![lolkof - AIOVAKSMS](https://img.shields.io/static/v1?label=lolkof&message=AIOVAKSMS&color=blue&logo=github)](https://github.com/AioSmsProviders/aiovaksms "Go to GitHub repo")
+
+[VAKSMS Official documentation](https://vak-sms.com/api/vak/)
+
+</div>
+
+## About
+
+This library is a wrapper for the https://vak-sms.com/api/vak/ API **from enthusiasts**. All methods are described and all types are
+**explicitly** defined. Methods that create requests to
+https://vak-sms.com/api/vak/
+return a pydantic's models for each response. Please write about all problems related to the library
+to [issues](https://github.com/AioSmsProviders/aiovaksms)
+
+API is up-to-date as of *22 May 2025*.
+
+* PyPl - https://pypi.org/project/aiovaksms/
+* Github - https://github.com/AioSmsProviders/aiovaksms
+* Requirements: Python >= 3.10
+
+### Features
+
+* It's completely **asynchronous**
+* You can use **multiple** clients to work with **multiple** users or shops
+* **All methods** for working with API are implemented
+* The library returns strictly typed for responses from APIs
+* For each method, **docstrings** are used
+* The library handle {type: error} responses and throws VaksmsBadRequest exception
+* **Modern**, strict code for Python 3.10+
+
+## Library Installation
+
+* Install via pip: `pip install aiovaksms`
+* Download sources - `git clone https://github.com/AioSmsProviders/aiovaksms`
+
+## Getting Started
+
+### first steps (new in 3.0 update)
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+
+    number = await client.get_number('ya')
+    print(number.tel)  # 79995554433
+
+    # recieve smscode
+    sms_code = await number.wait_sms_code(timeout=300, per_attempt=5) # do not indicate timeout and per_attempt
+    print(sms_code) #1234
+
+    # set status
+    await number.set_status('end')
+    # bad - ban number
+    # end - cancel number
+    # send - new sms
+
+
+asyncio.run(main())
+```
+
+### Get user balance
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    balances = await client.get_balance()
+    print(balances)  # balance = 100.0
+
+
+asyncio.run(main())
+```
+
+### Get number count
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    
+    data = await client.get_count_number('cp')
+    print(data)  # service='cp' count=4663 price=18.0
+
+
+asyncio.run(main())
+```
+
+### Get country list
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    data = await client.get_country_list()
+    print(data)  # [CountryOperator(countryName='Tajikistan', countryCode='tj', operatorList=['babilon mobile', 'beeline', 'megafon', 'tcell']), CountryOperator(countryName='Zimbabwe', countryCode='zw', operatorList=['econet', 'netone', 'telecel'])... ]
+
+
+asyncio.run(main())
+```
+
+### Get number
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    data = await client.get_number('ya')
+    
+    # An exclusive function for obtaining the lifetime of a number
+    # all known services whose lifetime differs from the standard 20 minutes
+    # are included in the library database as of 10/02/2024
+    # also work with "rent=True" parameter
+    print(data.lifetime) # 1200 lifetime from date of purchase
+    print(data.lives_up_to) # 1727823949 unix time of death
+    
+    print(data)  # tel=79296068469 service='ya' idNum='1725546315697382' lifetime=1200 lives_up_to=1727823949
+
+
+asyncio.run(main())
+```
+
+### Recieve smscode
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN')  # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net')  # work in russia
+    data = await client.get_sms_code('1725546315697382') # 1725546315697382 is number id (idNum)
+    print(data)  # smsCode='1234'
+
+
+asyncio.run(main())
+```
+
+### request a new sms
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    data = await client.set_status('1725546315697382', 'send') # 1725546315697382 is number id (idNum)
+    print(data)  # ready
+
+
+asyncio.run(main())
+```
+
+### get service full name, service info, service icons
+### this method not in official vaksms documentation
+
+```python
+import asyncio
+
+from aiovaksms import Vaksms
+
+
+async def main():
+    client = Vaksms('TOKEN') # use vaksms.com domain (not work in russia)
+    client = Vaksms('TOKEN', base_url='https://moresms.net') # work in russia
+    data = await client.get_count_number_list()
+    print(data)  # {'mr': Service(name='VK - MailRu', icon='https://vak-sms.com/static/service/mr.png', info='Тут можно принять смс от сервисов VKGroup.Не забывайте проверять номера на занятость через восстановление. Подробнее в базе знаний - https://bit.ly/3M6tXup', cost=22.0, rent=False, quantity=41153, private=False), ... }
+    print(data['mr'].name) # VK - MailRu
+    
+
+asyncio.run(main())
+```
+
+## Contact
+
+* E-Mail - lolkofprog@gmail.com
+* Telegram - [@lolkof](https://t.me/lolkof)
+
+## License
+
+Released under [GPL](/LICENSE) by [@lolkofka](https://github.com/AioSmsProviders).
---
+++ packages/aiovaksms/aiovaksms-3.0.2.tar.gz/aiovaksms-3.0.2/aiovaksms/client.py
@@ -0,0 +1,325 @@
+import asyncio
+import logging
+import time
+from typing import Literal
+
+import aiohttp
+from cachetools import TTLCache, cached
+
+from .exceptions import *
+from .models import *
+
+cache_all_data = TTLCache(maxsize=100, ttl=3600)  # 1 hour cache
+cache = TTLCache(maxsize=100, ttl=3600)  # 1 hour cache
+cache_countries = TTLCache(maxsize=100, ttl=3600)  # 1 hour cache
+
+
+async def _send_request(base_url: str, uri: str, proxy: str = None, **kwargs) -> dict[str, ...]:
+    async with aiohttp.ClientSession(base_url) as session:
+        try:
+            async with session.get(uri, proxy=proxy, **kwargs) as r:
+                response = await r.json(content_type=None)
+                if not isinstance(response, dict) or not response.get('error'):
+                    return response
+                else:
+                    raise VakSmsBadRequest(response['error'])
+        except (aiohttp.ClientError, asyncio.TimeoutError):
+            raise ValueError('Invalid Base URL')
+
+
+class Vaksms:
+    """
+    VakSms client for API interaction
+    Session will be automatically closed
+
+    API for https://vak-sms.com/
+    """
+
+    def __init__(self, api_key: str,
+                 base_url: str | None = None, timeout: int = 10, proxy: str = None):
+        """
+        Creates instance of one vaksms API client
+
+        Args:
+            api_key: API key from https://vak-sms.com/lk/
+            base_url: base URL (Optional)
+        """
+
+        self._api_key = api_key
+        self._base_url = base_url
+        self._timeout = timeout
+        self._proxy = proxy
+
+    async def get_balance(self) -> float:
+        """
+        Creates a request for get balances of user
+        See https://vak-sms.com/api/vak/
+
+        Returns: float of balance
+        """
+
+        response = await self.__create_request('/api/getBalance')
+
+        return response['balance']
+
+    @cached(cache)
+    async def get_count_number(self, service: str, operator: str = None, country: str = 'ru') -> CountNumber:
+        """
+        Creates a request for get count and price of number
+        See https://vak-sms.com/api/vak/
+        
+        Args:
+            service: service to check count
+            operator: operator to check count
+            country: country to check count
+        
+        Returns: Model from response JSON
+        """
+
+        params = {
+            'service': service,
+            'operator': operator,
+            'country': country,
+            'price': 1,
+        }
+
+        response = await self.__create_request('/api/getCountNumber', params)
+        response['service'] = list(response.keys())[0]
+        response['count'] = response[list(response.keys())[0]]
+        return CountNumber(**response)
+
+    @cached(cache_countries)
+    async def get_country_list(self) -> list[CountryOperator]:
+        """
+        Creates a request for get countries and operators inside a country
+        See https://vak-sms.com/api/vak/
+
+        Returns: list of Model from response JSON
+        """
+
+        response = await self.__create_request('/api/getCountryList', api_key_required=False)
+
+        return CountryList(response).root
+
+    async def get_number(self, service: str | list, operator: str = None, rent: bool = False, country: str = 'ru',
+                         soft_id: str = '1019') -> Number | MultipleResponse:
+        """
+        Creates a request for buying number
+        See https://vak-sms.com/api/vak/
+        
+        Args:
+            service: service to buy number may be list of services to buy 1 number for 2 services
+            operator: operator to buy number
+            rent: to buy 4 hours - life number
+            country: country to buy number
+            soft_id: to get referral earnings (set 1019 to support creator this library)
+        
+        Returns: Model from response JSON
+        """
+
+        if isinstance(service, list):
+            service = service[0] + ',' + service[1]
+
+        params = {
+            'service': service,
+            'operator': operator,
+            'rent': 'true' if rent else 'false',
+            'country': country,
+            'softId': soft_id
+        }
+
+        response = await self.__create_request('/api/getNumber/', params)
+
+        if isinstance(response, list):
+            return MultipleResponse(response).root
+        else:
+            response['service'] = service
+
+        return Number(self, **response, rent=rent)
+
+    async def prolong_number(self, service: str, tel: str | int, rent: bool = False,
+                             soft_id: str = '1019'):
+        """
+        Creates a request for buying number
+        See https://vak-sms.com/api/vak/
+
+        Args:
+            service: service to continuation number
+            tel: tel to continuation number, format: 79991112233
+            rent: bool need only for set lifetime
+            soft_id: to get referral earnings (set 1019 to support creator this library) may be not work in this method
+
+        Returns: Model from response JSON
+        """
+
+        params = {
+            'service': service,
+            'tel': tel,
+            'softId': soft_id
+        }
+
+        response = await self.__create_request('/api/prolongNumber', params)
+
+        response['service'] = service
+
+        return Number(**response, rent=rent)
+
+    async def set_status(self, number: str | Number, status: Literal['send', 'end', 'bad']):
+        """
+        Creates a request for buying number
+        statuses:
+        send = receiving new sms
+        end = cancel the number
+        bad = ban the number
+        
+        the difference between "end" and "bad" is that with bad the number will not come across to other people again,
+        including you, I recommend using statuses for their intended purpose so that the numbers do not run out quickly
+        and there is enough for everyone
+        
+        See https://vak-sms.com/api/vak/
+
+        Args:
+            number_id: idNum of number
+            status: can be set only "send", "end", "bad".
+
+        Returns: status
+        """
+        
+        number_id = number if not type(number) is Number else number.idNum
+        params = {
+            'idNum': number_id,
+            'status': status,
+        }
+
+        response = await self.__create_request('/api/setStatus', params)
+
+        return response['status']
+
+    async def get_sms_code(self, number: str | Number, get_all_numbers: bool | None = None) -> str | list[str]:
+        """
+        Checking sms codes
+
+        See https://vak-sms.com/api/vak/
+
+        Args:
+            number: idNum of number or Number object
+            get_all_numbers: set True, to get all sms codes who came on number
+
+        Returns: Model from response JSON, one sms or list of smses
+        """
+        number_id = number if not type(number) is Number else number.idNum
+        if not self._api_key:
+            raise ValueError('API key is required for this method')
+
+        params = {
+            'idNum': number_id,
+            'all': str(get_all_numbers),
+        }
+
+        response = await self.__create_request('/api/getSmsCode/', params)
+
+        return response
+    
+    async def wait_sms_code(self, number: str | Number, timeout: int = 60*5, per_attempt: int = 5) -> str | None:
+        """
+        Wait sms code
+
+        Args:
+            number_id: idNum of number or Number object
+            timeout: maximum time to wait sms code
+            per_attempt: time per attempt
+            
+        Returns: Sms
+        """
+        number_id = number if not type(number) is Number else number.idNum
+        if not self._api_key:
+            raise ValueError('API key is required for this method')
+
+        await self.set_status(number=number_id, status='send')
+        params = {
+            'idNum': number_id,
+            'all': str(None),
+        }
+
+        sms = None
+        start_time = time.time()
+        
+        while time.time() - start_time < timeout:
+            await asyncio.sleep(per_attempt)
+            response = await self.__create_request('/api/getSmsCode/', params)
+            sms = response['smsCode']
+            if type(sms) == list:
+                return sms[-1]
+        
+        return None
+        
+    @cached(cache_all_data)
+    async def get_count_number_list(self, country: str = 'RU', operator: str | None = None, rent: bool = False):
+        """
+        Get all services and prices, count, full name of service
+
+        Its method not in official docs
+
+        Returns: Model from response JSON
+        """
+
+        params = {
+            'country': country,
+        }
+        if operator:
+            params['operator'] = operator
+        params['rent'] = 'True' if rent else 'False',
+
+        response = await self.__create_request('/api/getCountNumbersList', params, api_key_required=False)
+        ret_d = {}
+        for service in response.keys():
+            s = Service(**response[service][0])
+            s.icon = self._base_url + s.icon
+            ret_d[service] = s
+
+        return ret_d
+
+    async def __create_request(self, uri: str, params: dict[str, ...] | None = None, api_key_required: bool = True) -> \
+            dict[str, ...]:
+        """
+        Creates a request to base URL and adds URI
+
+        Args:
+            uri: URI
+            params: Request params (Optional)
+
+        Returns: Model from response JSON
+        """
+
+        if api_key_required and not self._api_key:
+            raise ValueError('API key is required for this method')
+
+        if params is None:
+            params = {}
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        if self._api_key:
+            params['apiKey'] = self._api_key
+
+        kwargs = dict(
+            headers=headers,
+            params={k: v for k, v in params.items() if v is not None},
+            timeout=self._timeout
+        )
+
+        if self._base_url is None:
+            base_urls = ['https://vak-sms.com', 'https://moresms.net', 'https://vaksms.ru']
+
+            for base_url in base_urls:
+                try:
+                    r = await _send_request(base_url, uri, self._proxy, **kwargs)
+                    self._base_url = base_url
+                    return r
+                except ValueError:
+                    logging.warning(f'Failed to connect to {base_url}. Specify explicitly working base url')
+
+        return await _send_request(self._base_url, uri, self._proxy, **kwargs)
