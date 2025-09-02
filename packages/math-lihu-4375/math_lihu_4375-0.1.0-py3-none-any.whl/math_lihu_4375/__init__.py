c34d51d89b58f664f838e7c7dd8dcad6df2fe237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 14:05:44 +0000

    Add package math-lihu-4375/0.1.0/math_lihu_4375-0.1.0-py3-none-any.whl

---
+++ packages/math-lihu-4375/math_lihu_4375-0.1.0-py3-none-any.whl/math_lihu_4375/__init__.py
@@ -0,0 +1,15 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+def main() -> None:
+    mcp.run(transport="stdio")
