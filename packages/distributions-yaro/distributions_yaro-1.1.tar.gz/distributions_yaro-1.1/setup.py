c468faa410dc5caa2f7ad8c81cdbe3b069fd1f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 03:23:46 +0000

    Add package distributions-yaro/1.1/distributions_yaro-1.1.tar.gz

---
+++ packages/distributions-yaro/distributions_yaro-1.1.tar.gz/distributions_yaro-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="distributions_yaro",
+    version="1.1",
+    description="Gaussian distributions",
+    packages=["distributions"],
+    zip_safe=False,
+)
