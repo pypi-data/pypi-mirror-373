fd2afbc6c195e3ae42e7fa5b15c208c27ce7c45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 14:05:43 +0000

    Add package theia-client/0.1.7rc3/theia_client-0.1.7rc3.tar.gz

---
+++ packages/theia-client/theia_client-0.1.7rc3.tar.gz/theia_client-0.1.7rc3/theia/__init__.py
@@ -0,0 +1,12 @@
+from theia.client import client
+from theia.core import config, primitives
+
+BoundingBox = primitives.BoundingBox
+CameraConfig = config.CameraConfig
+Client = client.Client
+Detection = primitives.Detection
+EvaluatedDetection = primitives.EvaluatedDetection
+SafetyState = primitives.SafetyState
+ServerConfig = config.ServerConfig
+StreamStatus = primitives.StreamStatus
+SystemStatus = primitives.SystemStatus
