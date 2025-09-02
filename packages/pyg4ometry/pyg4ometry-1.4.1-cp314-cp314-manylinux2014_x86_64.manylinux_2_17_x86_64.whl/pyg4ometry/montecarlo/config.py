a2f104341e0ad0bdf1873e86c2d8b8bf28b08a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:04:58 +0000

    Add package pyg4ometry/1.4.1/pyg4ometry-1.4.1-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/pyg4ometry/pyg4ometry-1.4.1-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/pyg4ometry/montecarlo/config.py
@@ -0,0 +1,10 @@
+class Config:
+    def __init__(self, nHistory=1, seed=1):
+        self.nHistory = nHistory
+        self.seed = seed
+
+    def flukaString(self):
+        pass
+
+    def bdsimString(self):
+        pass
