9a1a58d6bc28de3928fa40a69c5f7c850c86ffea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:23 +0000

    Add package getpasstt34/99.0.1/getpasstt34-99.0.1.tar.gz

---
+++ packages/getpasstt34/getpasstt34-99.0.1.tar.gz/getpasstt34-99.0.1/README.md
@@ -0,0 +1,6 @@
+# Harmless Demo Package
+
+This is a safe proof-of-concept package.  
+PoC by **@bh3x**
+
+Nothing harmful is inside — this package is only a demo for publishing.
---
+++ packages/getpasstt34/getpasstt34-99.0.1.tar.gz/getpasstt34-99.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="getpasstt34",
+    version="99.0.1",
+    description="Safe demo package (PoC by @bh3x)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # important for Markdown
+    author="bh3x",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
