8615062dafcbdbb7b2ce65062e2c9315bb1406d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:06:35 +0000

    Add package monarchmoney-enhanced/0.2.3/monarchmoney_enhanced-0.2.3.tar.gz

---
+++ packages/monarchmoney-enhanced/monarchmoney_enhanced-0.2.3.tar.gz/monarchmoney_enhanced-0.2.3/monarchmoney/__init__.py
@@ -0,0 +1,16 @@
+"""
+monarchmoney
+
+A Python API for interacting with MonarchMoney.
+"""
+
+from .monarchmoney import (
+    LoginFailedException,
+    MonarchMoney,
+    MonarchMoneyEndpoints,
+    RequestFailedException,
+    RequireMFAException,
+)
+
+__version__ = "0.2.2"
+__author__ = "keithah"
