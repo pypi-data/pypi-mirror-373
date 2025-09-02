b499e01ec8bdad10e8426ec792e25aafb35328ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 16:05:53 +0000

    Add package ep-feature-sdk-4pd/1.0.0/ep_feature_sdk_4pd-1.0.0.tar.gz

---
+++ packages/ep-feature-sdk-4pd/ep_feature_sdk_4pd-1.0.0.tar.gz/ep_feature_sdk_4pd-1.0.0/setup.py
@@ -0,0 +1,48 @@
+import os
+
+from setuptools import setup, find_packages
+
+PACKAGE = 'ep_feature_sdk_4pd'
+NAME = 'ep_feature_sdk_4pd'
+DESCRIPTION = '4paradigm Ep Feature Service SDK Library for Python'
+AUTHOR = '4paradigm Ep Feature SDK'
+AUTHOR_EMAIL = ''
+URL = 'https://gitlab.4pd.io/electricityproject/ep-feature-sdk'
+VERSION = '1.0.0'
+REQUIRES = ['requests']
+
+LONG_DESCRIPTION = ''
+if os.path.exists('./README.md'):
+    with open('README.md', encoding='utf-8') as fp:
+        LONG_DESCRIPTION = fp.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    # license='Apache License 2.0',
+    url=URL,
+    keywords=['4pd_ep_feature'],
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    platforms='any',
+    install_requires=REQUIRES,
+    python_requires='>=3.6',
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development',
+    ],
+)
