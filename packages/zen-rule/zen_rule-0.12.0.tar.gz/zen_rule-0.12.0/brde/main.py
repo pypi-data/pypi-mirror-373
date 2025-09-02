66adba80fb7381f51465cd51502cb1982c2cb920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:05:59 +0000

    Add package zen-rule/0.12.0/zen_rule-0.12.0.tar.gz

---
+++ packages/zen-rule/zen_rule-0.12.0.tar.gz/zen_rule-0.12.0/brde/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from brde!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/zen-rule/zen_rule-0.12.0.tar.gz/zen_rule-0.12.0/brdeapp/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from brdeapp!")
+
+
+if __name__ == "__main__":
+    main()
