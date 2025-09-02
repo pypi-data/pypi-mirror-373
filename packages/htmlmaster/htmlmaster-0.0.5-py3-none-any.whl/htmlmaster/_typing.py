49da43f2d003c67b74fb5eb15e9558b1c4c7130a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:06:22 +0000

    Add package htmlmaster/0.0.5/htmlmaster-0.0.5-py3-none-any.whl

---
+++ packages/htmlmaster/htmlmaster-0.0.5-py3-none-any.whl/htmlmaster/_typing.py
@@ -0,0 +1,10 @@
+"""
+Contains typing classes.
+
+NOTE: this module is not intended to be imported at runtime.
+
+"""
+
+import loggings
+
+loggings.warning("this module is not intended to be imported at runtime")
