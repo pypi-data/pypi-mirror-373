7cfe198b469e5d9e9e23bc4a40e1d95aed7a1645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:04:53 +0000

    Add package adv-lion/0.1.0/adv_lion-0.1.0.tar.gz

---
+++ packages/adv-lion/adv_lion-0.1.0.tar.gz/adv_lion-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="adv_lion",
+    version="0.1.0",
+    author="Koratahiu",
+    author_email="hiuhonor@gmail.com",
+    license='Apache 2.0',
+    description="Pytorch Lion optimizer with updated and advanced features.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Koratahiu/Adv_Lion",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        "torch>=2.0",
+    ],
+    python_requires=">=3.8",
+    keywords=[
+        "llm",
+        "fine-tuning",
+        "pytorch",
+        "optimizer",
+        "lion",
+    ],
+)
