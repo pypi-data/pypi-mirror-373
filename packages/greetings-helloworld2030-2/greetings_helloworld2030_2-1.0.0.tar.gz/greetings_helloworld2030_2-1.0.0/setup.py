011e730c322cd5d2ad7f1914363928551de9813a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 03:23:51 +0000

    Add package greetings-helloworld2030-2/1.0.0/greetings_helloworld2030_2-1.0.0.tar.gz

---
+++ packages/greetings-helloworld2030-2/greetings_helloworld2030_2-1.0.0.tar.gz/greetings_helloworld2030_2-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='greetings', # must match the package folder name
+    version='1.0.0',
+    description='A simple Hello World!',
+    author = 'Fabio Nelli',
+    install_requires=[] # dependencies
+)
