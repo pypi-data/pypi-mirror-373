1e354b336528eb3c491d4213a529caaac0bf3218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 06:05:21 +0000

    Add package imdbinfo/0.5.2/imdbinfo-0.5.2.tar.gz

---
+++ packages/imdbinfo/imdbinfo-0.5.2.tar.gz/imdbinfo-0.5.2/imdbinfo/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+from .services import (
+    get_movie,
+    search_title,
+    get_name,
+    get_episodes,
+    get_all_episodes,
+    get_season_episodes,
+    get_akas,
+)
+
+__all__ = [
+    "get_movie",
+    "search_title",
+    "get_name",
+    "get_episodes",
+    "get_all_episodes",
+    "get_season_episodes",
+    "get_akas",
+]
+
+# setup library logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
