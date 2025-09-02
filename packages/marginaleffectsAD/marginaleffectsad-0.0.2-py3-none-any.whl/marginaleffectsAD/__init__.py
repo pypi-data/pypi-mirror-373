e10d0044132d1ee58adc2f83f8d29cbfd8b60b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:42 +0000

    Add package marginaleffectsAD/0.0.2/marginaleffectsad-0.0.2-py3-none-any.whl

---
+++ packages/marginaleffectsAD/marginaleffectsad-0.0.2-py3-none-any.whl/marginaleffectsAD/__init__.py
@@ -0,0 +1,7 @@
+# Estimation module for JAX-based marginal effects
+
+from .utils import array as array
+
+# Import submodules to make them accessible
+from . import linear as linear
+from . import glm as glm
