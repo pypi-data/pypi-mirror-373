1ca46dd94a2f0220d43635683c93b6f232d07cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 16:05:42 +0000

    Add package hestia-earth-models/0.74.7/hestia_earth_models-0.74.7.tar.gz

---
+++ packages/hestia-earth-models/hestia_earth_models-0.74.7.tar.gz/hestia_earth_models-0.74.7/hestia_earth/models/site/pre_checks/country.py
@@ -0,0 +1,6 @@
+from hestia_earth.schema import TermTermType
+
+from hestia_earth.models.utils.term import download_term
+
+
+def run(site: dict): return site | {'country': download_term(site.get('country', {}).get('@id'), TermTermType.REGION)}
