0952fadca7c584111352218ad06d5efc86273d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 07:05:06 +0000

    Add package PyPNM/2.21.2.34/pypnm-2.21.2.34.tar.gz

---
+++ packages/PyPNM/pypnm-2.21.2.34.tar.gz/pypnm-2.21.2.34/pypnm/__init__.py
@@ -0,0 +1,9 @@
+"""PNM (PPM and PGM) image files reading, displaying and writing for Python 3.4 and above.
+----
+
+2 Sep 2025 "Victory II" build. Fully interchangeable with previous
+9 May 2025 PyPNM 1.17.9.34 "Victory" build.
+
+Use `from pypnm import pnmlpnm` to access functions.
+
+PyPNM Documentation: https://dnyarri.github.io/pypnm/pypnm.pdf"""
