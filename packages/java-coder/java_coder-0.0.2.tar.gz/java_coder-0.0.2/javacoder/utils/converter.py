28097390541b6ba19301a93ff721182bbe87f400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 05:05:02 +0000

    Add package java-coder/0.0.2/java_coder-0.0.2.tar.gz

---
+++ packages/java-coder/java_coder-0.0.2.tar.gz/java_coder-0.0.2/javacoder/utils/converter.py
@@ -0,0 +1,18 @@
+import re
+
+
+def camel_to_snake(name):
+    # convert camelFormat to snake_format
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def is_collection(collection):
+    return hasattr(collection, '__iter__') and hasattr(collection, '__getitem__')
+
+
+def get_first_element(collect, eq_func):
+    for i in collect:
+        if eq_func(i):
+            return i
+    return None
