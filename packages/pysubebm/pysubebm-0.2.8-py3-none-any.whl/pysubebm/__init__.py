f390b9c4cb3acbe85b92a3a151c177f1e45e8423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 14:05:24 +0000

    Add package pysubebm/0.2.8/pysubebm-0.2.8-py3-none-any.whl

---
+++ packages/pysubebm/pysubebm-0.2.8-py3-none-any.whl/pysubebm/__init__.py
@@ -0,0 +1,10 @@
+from .run import run_subebm
+from .generate_data import generate
+from .data import get_sample_data_path, get_params_path
+
+__all__ = [
+    "run_subebm",
+    "generate",
+    "get_sample_data_path",
+    "get_params_path",
+]
