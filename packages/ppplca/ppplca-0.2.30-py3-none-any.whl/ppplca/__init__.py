29aabee48bd98e97354cbed7d886032c7a006878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 03:23:24 +0000

    Add package ppplca/0.2.30/ppplca-0.2.30-py3-none-any.whl

---
+++ packages/ppplca/ppplca-0.2.30-py3-none-any.whl/ppplca/__init__.py
@@ -0,0 +1,23 @@
+
+def install():
+    from ppplca.Commands.InstallCommand import InstallCommand
+    installcommand = InstallCommand()
+    installcommand.handle()
+
+def setup():
+    answer = input("Have you updated the config.ini file and saved the agrifootprint database? [Y/n]: ")
+    if answer.lower() != "y":
+        print("Please update the file and save the database before running the setup.")
+        return
+    from ppplca.Commands.SetupDatabaseCommand import SetupDatabaseCommand
+    setupdatabasecommand = SetupDatabaseCommand()
+    setupdatabasecommand.handle()
+
+def run(file="value_chains_test.xlsx", sheet_name = None):
+    answer = input(f"Have you updated the Processing_data.xlsx and {file} files? [Y/n]: ")
+    if answer.lower() != "y":
+        print("Please update the filese before running the setup.")
+        return
+    from ppplca.Commands.RunCommand import RunCommand
+    rundatabasecommand = RunCommand()
+    rundatabasecommand.handle(file, sheet_name)
