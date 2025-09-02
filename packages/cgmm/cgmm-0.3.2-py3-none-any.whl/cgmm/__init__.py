010faad93bc2a3a7957bea7cb602e0ec22ce551f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 06:05:43 +0000

    Add package cgmm/0.3.2/cgmm-0.3.2-py3-none-any.whl

---
+++ packages/cgmm/cgmm-0.3.2-py3-none-any.whl/cgmm/__init__.py
@@ -0,0 +1,9 @@
+from .conditioner import GMMConditioner
+from .regressor import ConditionalGMMRegressor
+
+__all__ = [
+    "GMMConditioner",
+    "ConditionalGMMRegressor",
+]
+
+__version__ = "0.3.2"
