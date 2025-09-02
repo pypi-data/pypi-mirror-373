80602ea68c721712933607e8c7c0be46615539a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:09 +0000

    Add package pyaffalddk/2.13.0/pyaffalddk-2.13.0-py3-none-any.whl

---
+++ packages/pyaffalddk/pyaffalddk-2.13.0-py3-none-any.whl/pyaffalddk/__init__.py
@@ -0,0 +1,28 @@
+# ruff: noqa: F401
+"""A Python Wrapper to communicate with AffaldDK API."""
+
+from pyaffalddk.api import GarbageCollection
+from pyaffalddk.interface import (
+    AffaldDKGarbageTypeNotFound,
+    AffaldDKNotSupportedError,
+    AffaldDKNotValidAddressError,
+    AffaldDKNoConnection,
+)
+from pyaffalddk.data import PickupEvents, PickupType, AffaldDKAddressInfo
+
+from pyaffalddk.municipalities import (
+    SUPPORTED_MUNICIPALITIES,
+    MUNICIPALITIES_LIST,
+)
+from pyaffalddk.const import (
+    ICON_LIST,
+    NAME_ARRAY,
+    NAME_LIST,
+    WEEKDAYS,
+    WEEKDAYS_SHORT,
+)
+
+__title__ = "pyaffalddk"
+__version__ = "2.13.0"
+__author__ = "briis"
+__license__ = "MIT"
