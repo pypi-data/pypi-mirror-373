70d28f65d5b31983901971b909ec467d1690ec78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 15:06:08 +0000

    Add package tessdb-dao/0.1.16/tessdb_dao-0.1.16.tar.gz

---
+++ packages/tessdb-dao/tessdb_dao-0.1.16.tar.gz/tessdb_dao-0.1.16/src/tessdbdao/__init__.py
@@ -0,0 +1,36 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2022
+#
+# See the LICENSE file for details
+# see the AUTHORS file for authors
+# ----------------------------------------------------------------------
+
+# --------------
+# local imports
+# -------------
+
+from ._version import __version__
+from .constants import (
+    GEO_COORD_EPSILON,
+    ObserverType,
+    PhotometerModel,
+    ValidState,
+    PopulationCentre,
+    TimestampSource,
+    ReadingSource,
+    RegisterState,
+    RegisterOp,
+)
+
+__all__ = [
+    "__version__",
+    "GEO_COORD_EPSILON",
+    "ObserverType",
+    "PhotometerModel",
+    "ValidState",
+    "PopulationCentre",
+    "TimestampSource",
+    "ReadingSource",
+    "RegisterState",
+    "RegisterOp",
+]
