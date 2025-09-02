6507744a9759986afeb842d599d784ea8147e42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 07:05:05 +0000

    Add package nobitex-python/0.0.0.dev0/nobitex_python-0.0.0.dev0-py3-none-any.whl

---
+++ packages/nobitex-python/nobitex_python-0.0.0.dev0-py3-none-any.whl/nobitex/__init__.py
@@ -0,0 +1,8 @@
+"""Nobitex: A Python SDK For Nobitex Exchange."""
+
+# using importlib.metadata
+import importlib.metadata
+
+__version__ = importlib.metadata.version("nobitex-python")
+
+__all__ = ["__version__"]
