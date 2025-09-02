33533f44bbb5fd8367d664789080ada836983d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 07:05:18 +0000

    Add package titascodes-nester/1.0.0/titascodes_nester-1.0.0.tar.gz

---
+++ packages/titascodes-nester/titascodes_nester-1.0.0.tar.gz/titascodes_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'titascodes-nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'titascodes',
+    author_email = 'titas.r@outlook.com',
+    url = 'http://www.titasraman.com',
+    description = 'nested lists printer',
+    )
