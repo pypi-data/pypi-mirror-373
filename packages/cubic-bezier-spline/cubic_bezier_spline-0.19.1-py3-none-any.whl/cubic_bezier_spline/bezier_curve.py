1ea16f337517d56bb5891c7bd5e42edb14a087d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 06:05:03 +0000

    Add package cubic-bezier-spline/0.19.1/cubic_bezier_spline-0.19.1-py3-none-any.whl

---
+++ packages/cubic-bezier-spline/cubic_bezier_spline-0.19.1-py3-none-any.whl/cubic_bezier_spline/bezier_curve.py
@@ -0,0 +1,259 @@
+"""BezierCurve object.
+
+:author: Shay Hill
+:created: 1/18/2020
+
+This uses matrix math to evaluate the Bezier curve. The math benefits from cacheing, so
+the curve object is immutable.
+
+You can evaluate the curve at time values outside of [0, 1], because the math allows
+it. You might intentionally do that (I don't know why), but it's more likely a
+mistake on your part. This package does not guard against that for you.
+"""
+
+from __future__ import annotations
+
+import dataclasses
+from collections.abc import Sequence
+from functools import cached_property
+from typing import Annotated, Any, TypeVar, Union
+
+import numpy as np
+import numpy.typing as npt
+from paragraphs import par
+from svg_path_data import get_svgd_from_cpts
+
+from cubic_bezier_spline.control_point_casting import as_nested_tuple, as_points_array
+from cubic_bezier_spline.curve_length import get_approximate_curve_length
+from cubic_bezier_spline.matrices import get_mix_matrix
+
+Points = Union[Sequence[Sequence[float]], npt.NDArray[np.floating[Any]]]
+APoints = Annotated[npt.NDArray[np.floating[Any]], "(-1, -1)"]
+TPoints = tuple[tuple[float, ...], ...]
+
+_BezierCurveT = TypeVar("_BezierCurveT", bound="BezierCurve")
+
+
+@dataclasses.dataclass(frozen=True)
+class BezierCurve:
+    """A non-rational Bezier curve.
+
+    Array annotations are used to document the shape of the arrays. I've used the
+    convention `p` for the number of points (degree + 1) and `v` for the
+    dimensionality of the space.
+
+    For instance
+    * the `as_array` array is a 2D array of shape (p, v). [point, point, point, ...]
+    * the `tmat` array is a 1D array of shape (p,). [t^0, t^1, t^2, ...]
+    * the `zmat` array is a 2D array of shape (p, p).
+    """
+
+    cpts: TPoints
+    as_array: APoints = dataclasses.field(init=False, repr=False, compare=False)
+
+    def __init__(self, points: Points) -> None:
+        """Initialize BezierCurve.
+
+        :param points: control points, each a sequence of floats
+
+        Stores points internally as a tuple of tuples (`.cpts`) as a
+        canonical representation. Also stores an array of the same information
+        (`.points`), for convenience and to give argument validation through
+        `as_points_array`.
+        """
+        object.__setattr__(self, "as_array", as_points_array(points))  # validates
+        object.__setattr__(self, "cpts", as_nested_tuple(self.as_array))
+
+    @property
+    def degree(self) -> int:
+        """Degree of curve.
+
+        :return: len(self.cpts) - 1
+        """
+        return len(self.cpts) - 1
+
+    def __getitem__(self, item: int) -> npt.NDArray[np.floating[Any]]:
+        """Return item-th point.
+
+        :param item: index of [p0, p1, p2, p3]
+        :return: One control point as an array
+        """
+        return self.as_array[item]
+
+    def __array__(self) -> Points:
+        """Return self.as_array (it's an array anyway).
+
+        :return: self.as_array when encountering np.array(self)
+        """
+        return self.as_array
+
+    def __call__(
+        self, time: float, derivative: int = 0
+    ) -> npt.NDArray[np.floating[Any]]:
+        """Cubic Bezier calculation at time.
+
+        :param time: time on curve (typically 0 - 1)
+        :return: Non-rational Bezier at time
+
+        Include shortcuts for time in {0, 1} to make testing easier and avoid *some*
+        floating-point errors.
+        """
+        if derivative == 0:
+            if time == 0:
+                return self.as_array[0]
+            if time == 1:
+                return self.as_array[-1]
+            return self._get_tmat(time) @ self._mixed_points
+        return self.derivative(derivative)(time)
+
+    def _get_tmat(
+        self, time: float
+    ) -> Annotated[npt.NDArray[np.floating[Any]], "(p,)"]:
+        """Get the t matrix for time.
+
+        :param time: time on curve (typically 0 - 1)
+        :return: t matrix for time: [1, t, t^2, t^3]
+        """
+        return np.array([time**x for x in range(self.degree + 1)])
+
+    def get_zmat(
+        self, time: float
+    ) -> Annotated[npt.NDArray[np.floating[Any]], "(p, p)"]:
+        """Get a 2D zero matrix with tmat on the diagonal.
+
+        :param time: time on curve (typically 0 - 1)
+        :return: 2D zero matrix with tmat on the diagonal
+        [
+            [1, 0, 0, 0],
+            [0, t, 0, 0],
+            [0, 0, t^2, 0],
+            [0, 0, 0, t^3]
+        ]
+        """
+        return np.diagflat(self._get_tmat(time))
+
+    @cached_property
+    def mmat(self) -> Annotated[npt.NDArray[np.floating[Any]], "(p, p)"]:
+        """Get the mix matrix for this curve.
+
+        :return: mix matrix for this curve
+        """
+        return get_mix_matrix(self.degree + 1)
+
+    @cached_property
+    def _mixed_points(self) -> Annotated[npt.NDArray[np.floating[Any]], "(p,v)"]:
+        """Points scaled by binomial coefficients.
+
+        :return: Points scaled by binomial coefficients
+
+        Scale this by time matrix to evaluate curve at time.
+        """
+        return self.mmat @ self.as_array
+
+    def split(self: _BezierCurveT, *time_args: float) -> list[_BezierCurveT]:
+        """Split a BezierCurve into two Bezier curves of the same degree.
+
+        :param time_args: time at which to split the curve. Multiple args accepted.
+            Should must in [0, 1] and monotonically increasing. Out of range values
+            will be clipped to [0, 1].
+        :return: two new BezierCurve instances
+        """
+        time_args = tuple(max(0, min(1, time)) for time in time_args)
+        curves = [self]
+        time_at = 0.0
+        for time in time_args:
+            time_prime = (time - time_at) / (1 - time_at)
+            if time_prime == time_at:
+                point = np.array([self(time_at)] * (self.degree + 1))
+                curves[-1:] = [type(self)(point), curves[-1]]
+                continue
+            if time_prime == 1:
+                point = np.array(self.cpts[-1:] * (self.degree + 1))
+                curves[-1:] = [curves[-1], type(self)(point)]
+                time_at = 1
+                continue
+            qmat = (
+                np.linalg.inv(curves[-1].mmat)
+                @ curves[-1].get_zmat(time_prime)
+                @ curves[-1].mmat
+            )
+
+            qmat_prime = np.zeros_like(qmat)
+            for i in range(qmat.shape[0]):
+                j = i + 1
+                qmat_prime[-j, -j:] = qmat[i, :j]
+            curves[-1:] = [
+                type(self)(qmat @ curves[-1].as_array),
+                type(self)(qmat_prime @ curves[-1].as_array),
+            ]
+            time_at = time
+        return curves
+
+    def elevated(self, to_degree: int | None = None) -> BezierCurve:
+        """Create a new curve, elevated 1 or optionally more degrees.
+
+        :param to_degree: final degree of Bezier curve
+        :return: Bezier curve of identical shape with degree increased
+        :raise ValueError: if to_degree is less than current degree
+        """
+        if to_degree == self.degree:
+            return self
+        if to_degree is None:
+            to_degree = self.degree + 1
+        elif to_degree < self.degree:
+            msg = par(
+                f"""cannot elevate BezierCurve degree={self.degree} to BezierCurve
+                degree={to_degree}"""
+            )
+            raise ValueError(msg)
+        nn, ps = len(self.cpts), self.as_array
+        # mypy likes linspace more that arange
+        rats = np.linspace(1, nn - 1, nn - 1)[:, None] / nn
+        return type(self)(
+            np.concatenate([ps[:1], ps[:-1] * rats + ps[1:] * (1 - rats), ps[-1:]])
+        ).elevated(to_degree)
+
+    @cached_property
+    def length(self) -> float:
+        """Get the approximate length of a Bezier curve.
+
+        :return: approximate length of the Bezier curve
+        """
+        return get_approximate_curve_length(self)
+
+    @cached_property
+    def reversed(self) -> BezierCurve:
+        """Create a new curve, reversed.
+
+        :return: Bezier curve of identical shape with control points reversed
+        """
+        return type(self)(self.as_array[::-1])
+
+    def derivative(self, derivative: int) -> BezierCurve:
+        """Nth derivative of a Bezier curve.
+
+        :param derivative: 0 -> the curve itself, 1 -> 1st, 2 -> 2nd, etc.
+        :return: points to calculate nth derivative.
+        :raise ValueError: if derivative is greater than degree
+
+        The derivative of a Bezier curve of degree n is a Bezier curve of degree
+        n-1 with control points n*(p1-p0), n*(p2-p1), n*(p3-p2), ...
+        """
+        if derivative == 0:
+            return self
+        if derivative > self.degree:
+            msg = par(
+                f"""Bezier curve of degree {self.degree} does not have a
+                {derivative}th derivative."""
+            )
+            raise ValueError(msg)
+        points = (self.as_array[1:] - self.as_array[:-1]) * self.degree
+        return type(self)(points).derivative(derivative - 1)
+
+    @cached_property
+    def svgd(self) -> str:
+        """Get the SVG data for the spline.
+
+        :return: SVG data string (the d="" attribute of an svg "path" element)
+        """
+        return get_svgd_from_cpts([self.cpts], 6)
---
+++ packages/cubic-bezier-spline/cubic_bezier_spline-0.19.1-py3-none-any.whl/cubic_bezier_spline/construct_splines.py
@@ -0,0 +1,210 @@
+"""Constructors for Bezier splines.
+
+:author: Shay Hill
+::created: 10/4/2020
+"""
+
+from __future__ import annotations
+
+import enum
+from collections.abc import Sequence
+from typing import Annotated, Any, Union
+
+import numpy as np
+import numpy.typing as npt
+
+from cubic_bezier_spline.bezier_spline import BezierSpline
+from cubic_bezier_spline.control_point_casting import (
+    as_closed_points_array,
+    as_open_points_array,
+)
+from cubic_bezier_spline.pairwise import pairwise
+
+Points = Union[Sequence[Sequence[float]], npt.NDArray[np.floating[Any]]]
+APoints = Annotated[npt.NDArray[np.floating[Any]], "(-1, -1)"]
+
+_LINEAR = 2
+_QUADRATIC = 3
+
+
+class _OpenOrClosed(enum.Enum):
+    """Enum for whether a spline is open or closed."""
+
+    OPEN = enum.auto()
+    CLOSED = enum.auto()
+
+
+def _get_141_matrix(dim: int, open_or_closed: _OpenOrClosed) -> npt.NDArray[np.bool_]:
+    """Create the 141 matrix necessary to get interpolated points from control points.
+
+    :param dim: size of the matrix
+    :param open_or_closes: will the spline be open or closed? If closed, the spline
+        will wrap around to connect the first and last points.
+    :return: a (dim, dim) matrix with 1, 4, 1 on the diagonal and 0 elsewhere.
+    """
+    ones = [1] * (dim - 1)
+    mat_141 = np.diag(ones, -1) + np.diag([4] * dim) + np.diag(ones, 1)
+    if open_or_closed == _OpenOrClosed.CLOSED:
+        mat_141 += np.diag([1], dim - 1) + np.diag([1], -(dim - 1))
+    return mat_141
+
+
+def _new_linear_spline(cpts: Points, open_or_closed: _OpenOrClosed) -> BezierSpline:
+    """Create a linear spline from a sequence of points.
+
+    :param cpts: points to connect with line segments
+    :param open_or_closes: will the spline be open or closed? If closed, the spline
+        will wrap around to connect the first and last points.
+    :return: A spline connecting the points with line segments.
+    """
+    if open_or_closed == _OpenOrClosed.CLOSED:
+        cpts_ = as_closed_points_array(cpts)
+    else:
+        cpts_ = as_open_points_array(cpts)
+    return BezierSpline(pairwise(cpts_))
+
+
+def new_open_linear_spline(cpts: Points) -> BezierSpline:
+    """Create a linear spline from a sequence of points.
+
+    :param cpts: points to connect with line segments
+    :return: A spline connecting the points with line segments.
+    """
+    return _new_linear_spline(cpts, _OpenOrClosed.OPEN)
+
+
+def new_closed_linear_spline(cpts: Points) -> BezierSpline:
+    """Create a linear spline from a sequence of points.
+
+    :param cpts: points to connect with line segments
+    :return: A spline connecting the points with line segments.
+    """
+    return _new_linear_spline(cpts, _OpenOrClosed.CLOSED)
+
+
+def _new_approximating_spline(
+    cpts: Points, open_or_closed: _OpenOrClosed
+) -> BezierSpline:
+    """Approximate a set of points as a composite Bezier curve (Bezier spline).
+
+    :param cpts: points to approximate
+    :param open_or_closes: will the spline be open or closed? If closed, the spline
+        will wrap around to connect the first and last points.
+    :return: A spline beginning at cpts[0], ending at cpts[-1], and shaped by
+        cpts[1:-1] or, if closed, a spline shaped by all control points, beginning
+        and ending at the same point.
+    """
+    if open_or_closed == _OpenOrClosed.CLOSED:
+        cpts_ = as_closed_points_array(cpts)
+        cpts_ = np.concatenate([cpts_[-2:-1], cpts_, cpts_[1:2]])
+    else:
+        cpts_ = as_open_points_array(cpts)
+
+    thirds = [
+        [x, (2 * x + y) / 3, (x + 2 * y) / 3, y] for x, y in zip(cpts_, cpts_[1:])
+    ]
+    for prev_curve, next_curve in zip(thirds, thirds[1:]):
+        new_point = (prev_curve[2] + next_curve[1]) / 2
+        prev_curve[-1] = new_point
+        next_curve[0] = new_point
+
+    if open_or_closed == _OpenOrClosed.CLOSED:
+        return BezierSpline(thirds[1:-1])
+    return BezierSpline(thirds)
+
+
+def new_open_approximating_spline(cpts: Points) -> BezierSpline:
+    """Approximate a set of points as a composite Bezier curve (Bezier spline).
+
+    :param cpts: points to approximate
+    :return: A spline beginning at cpts[0], ending at cpts[-1], and shaped by
+        cpts[1:-1].
+    """
+    return _new_approximating_spline(cpts, _OpenOrClosed.OPEN)
+
+
+def new_closed_approximating_spline(cpts: Points) -> BezierSpline:
+    """Approximate a set of points as a composite Bezier curve (Bezier spline).
+
+    :param cpts: points to approximate
+    :return: A spline passing through all control points, beginning and ending at
+        the same point.
+    """
+    return _new_approximating_spline(cpts, _OpenOrClosed.CLOSED)
+
+
+# ===================================================================================
+#   Interpolating splines
+# ===================================================================================
+
+
+def _get_b_matrix(cpts: APoints) -> Annotated[npt.NDArray[np.floating[Any]], "(p-2,v)"]:
+    """Get the B matrix for a set of points.
+
+    :param cpts: points to interpolate
+    :return: A (p-2, v) matrix of scaled control points
+
+    For a set of points (p, v), the B matrix is a (p-2, v) matrix of scaled control
+    points needed to compute interpolating control points.
+
+    This is part of solving for B control points that, when approximated, will
+    effectively be S control points interpolated.
+
+    [4, 1, 0, 0] [P1]   [6 * (S[1] - S[0])]
+    [1, 4, 1, 0] [P2] = [6 *  S[2]        ]
+    [0, 1, 4, 1] [P3]   [6 *  S[3]        ]
+    [0, 0, 1, 4] [P4]   [6 * (S[4] - S[5])]
+    """
+    return np.concatenate(
+        [[6 * cpts[1] - cpts[0]], 6 * cpts[2:-2], [6 * cpts[-2] - cpts[-1]]]
+    )
+
+
+def get_closed_b_points(cpts: Points) -> APoints:
+    """Get points B that, when approximated, will interpolate the control points.
+
+    :param cpts: points to be interpolated
+    :return: B points that, when approximated, will interpolate cpts
+    """
+    cpts_ = as_open_points_array(cpts)
+    mat_141 = _get_141_matrix(len(cpts_), open_or_closed=_OpenOrClosed.CLOSED)
+    return np.linalg.inv(mat_141) @ cpts_ * 6
+
+
+def get_open_b_points(cpts: Points) -> APoints:
+    """Get points B that, when approximated, will interpolate the control points.
+
+    :param cpts: points to be interpolated
+    :return: B points that, when approximated, will interpolate cpts
+    """
+    cpts_ = as_open_points_array(cpts)
+    if len(cpts_) == _LINEAR:
+        return cpts_
+    if len(cpts_) == _QUADRATIC:
+        midpt = (-cpts_[0] + 6 * cpts_[1] - cpts_[2]) / 4
+        return np.asarray([cpts_[0], midpt, cpts_[2]])
+    mat_141 = _get_141_matrix(len(cpts_) - 2, _OpenOrClosed.OPEN)
+    mat_b = _get_b_matrix(cpts_)
+    interior_pts = np.linalg.inv(mat_141) @ mat_b
+    return np.concatenate([cpts_[:1], interior_pts, cpts_[-1:]])
+
+
+def new_closed_interpolating_spline(cpts: Points) -> BezierSpline:
+    """Get a closed cubic interpolating spline.
+
+    :param cpts: points to interpolate
+    :return: A spline passing through all control points, beginning and ending at
+        cpts[0]
+    """
+    b_points = get_closed_b_points(cpts)
+    return new_closed_approximating_spline(b_points)
+
+
+def new_open_interpolating_spline(cpts: Points) -> BezierSpline:
+    """Get an open cubic interpolating spline.
+
+    :param cpts: points to interpolate
+    :return: A spline passing through all control points.
+    """
+    b_points = get_open_b_points(cpts)
+    return new_open_approximating_spline(b_points)
