7bfbd0db8cb2e33dc97317e2fa1aac284eabfc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:15 +0000

    Add package structifyai/1.138.10/structifyai-1.138.10-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.138.10-py3-none-any.whl/structify/types/entity_summarize_params.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["EntitySummarizeParams"]
+
+
+class EntitySummarizeParams(TypedDict, total=False):
+    dataset: Required[str]
+
+    entity_id: Required[str]
+
+    properties: Required[List[str]]
+
+    extra_instructions: Optional[List[str]]
+    """A list of instructions for each property to guide the summarization process"""
