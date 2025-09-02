9262bcbf76c55beeb0910c0f3b994ab31c41f918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 06:05:59 +0000

    Add package pyg4ometry/1.4.2/pyg4ometry-1.4.2-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/pyg4ometry/pyg4ometry-1.4.2-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/pyg4ometry/features/_accelerator.py
@@ -0,0 +1,97 @@
+import numpy as _np
+
+from .algos import FeatureData as _FeatureData
+from .algos import CoordinateSystem as _CoordinateSystem
+from .algos import Plane as _Plane
+from .algos import extract as _extract
+from .. import pyoce as _oce
+
+
+def beamPipeCADFeature(shape):
+    wirePnts = []
+
+    topoExp = _oce.TopExp_Explorer(shape, _oce.TopAbs_WIRE, _oce.TopAbs_VERTEX)
+    location = _oce.TopLoc_Location()
+
+    while topoExp.More():
+        wire = _oce.TopoDS.Wire(topoExp.Current())
+        # print(wire)
+        topoExpWire = _oce.TopExp_Explorer(wire, _oce.TopAbs_EDGE, _oce.TopAbs_VERTEX)
+
+        edgePnts = []
+        while topoExpWire.More():
+            edge = _oce.TopoDS.Edge(topoExpWire.Current())
+            edgeLocation = _oce.TopLoc_Location()
+            start = 0
+            end = 0
+            curve, l, s, e = _oce.BRep_Tool.Curve(edge, edgeLocation, start, end)
+
+            for par in _np.linspace(s, e, 20):
+                pnt = curve.Value(par)
+                edgePnts.append([pnt.X(), pnt.Y(), pnt.Z()])
+            # print(edge,curve,l,s,e)
+
+            topoExpWire.Next()
+
+        wirePnts.append(edgePnts)
+        topoExp.Next()
+
+    return wirePnts
+
+
+def beamPipe(
+    stlFileName,
+    feature1=-1,
+    feature2=-1,
+    planeAngles=[[0, 0, 0]],
+    vis=True,
+    interactive=True,
+):
+    datFileName = stlFileName.replace("stl", "dat")
+
+    if feature1 == -1:
+        vis = True
+        interactive = True
+
+    v = _extract(
+        stlFileName,
+        angle=46,
+        circumference=2 * _np.pi * 8,
+        planes=[],
+        outputFileName=datFileName,
+        bViewer=vis,
+        bViewerInteractive=interactive,
+    )
+
+    fd = _FeatureData()
+    fd.readFile(datFileName)
+
+    if feature1 == -1:
+        return
+
+    p1 = fd.features[feature1]["plane"]
+    p2 = fd.features[feature2]["plane"]
+
+    pp1 = _Plane(p1[0:3], p1[3:])
+    pp2 = _Plane(p2[0:3], p2[3:])
+    pp3 = _Plane([0, 0, 0], [0, 0, 1])
+
+    cs = _CoordinateSystem()
+    cs.makeFromPlanes(pp1, pp2, pp3)
+
+    csa = [cs.coordinateSystem(ang[0], ang[1], ang[2]) for ang in planeAngles]
+
+    if vis:
+        v.addPlane(cs.origin, cs.e1, cs.e2, cs.dist)
+        v.addAxis(cs.origin, [cs.dist, cs.dist, cs.dist], cs.rot, label=True, disableCone=True)
+        v.view(interactive=True)
+
+    v = _extract(
+        stlFileName,
+        angle=46,
+        circumference=2 * _np.pi * 8,
+        planes=csa,
+        outputFileName=datFileName,
+        bViewer=vis,
+        bViewerInteractive=interactive,
+    )
