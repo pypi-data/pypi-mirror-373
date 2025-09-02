9abc7b6dd2eaba2d35eef01532a70117c29e5659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 03:23:04 +0000

    Add package vayu-client/1.0.12/vayu_client-1.0.12.tar.gz

---
+++ packages/vayu-client/vayu_client-1.0.12.tar.gz/vayu_client-1.0.12/vayu_sdk/vayu.py
@@ -0,0 +1,44 @@
+from .clients import VayuClient
+from .apis import * 
+
+class Vayu:
+    __api_key: str = None
+    __host: str = None
+    __client: VayuClient = None
+
+    def __init__(self, api_key: str, host: str = None):
+        self.__client = None
+        self.__host = host
+        self.__api_key = api_key
+
+    @property
+    def contracts(self) -> ContractsAPI:
+        return ContractsAPI(self.__client)
+
+    @property
+    def customers(self) -> CustomersAPI:
+        return CustomersAPI(self.__client)
+
+    @property
+    def events(self) -> EventsAPI:
+        return EventsAPI(self.__client)
+
+    @property
+    def invoices(self) -> InvoicesAPI:
+        return InvoicesAPI(self.__client)
+
+    @property
+    def meters(self) -> MetersAPI:
+        return MetersAPI(self.__client)
+
+    @property
+    def plans(self) -> PlansAPI:
+        return PlansAPI(self.__client)
+    
+    @property
+    def webhooks(self) -> WebhooksAPI:
+        return WebhooksAPI(self.__client)
+    
+    def login(self):
+        self.__client = VayuClient(self.__api_key, self.__host)
+        self.__client.login()
