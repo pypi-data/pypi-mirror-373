e5900a80baaca23e258198ac95efa50256087736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:36 +0000

    Add package trading-sdk/0.1.8/trading_sdk-0.1.8.tar.gz

---
+++ packages/trading-sdk/trading_sdk-0.1.8.tar.gz/trading_sdk-0.1.8/src/trading_sdk/spot/trading/__init__.pyi
@@ -0,0 +1,13 @@
+from ._trading import Trading
+from .cancel_order import CancelOrder
+from .edit_order import EditOrder
+from .place_order import PlaceOrder
+from .place_orders import PlaceOrders
+
+__all__ = [
+  'CancelOrder',
+  'PlaceOrder',
+  'EditOrder',
+  'Trading',
+  'PlaceOrders',
+]
