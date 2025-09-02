662b34df1c37c06d271f28894ed502cfe6a2903b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:04:57 +0000

    Add package async-http-server/0.1.2/async_http_server-0.1.2.tar.gz

---
+++ packages/async-http-server/async_http_server-0.1.2.tar.gz/async_http_server-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async-http-server",          # PyPI name (must be unique)
+    version="0.1.2",
+    description="A simple async HTTP server package",
+    author="Divy Das",
+    author_email="Divya.Das@email.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
