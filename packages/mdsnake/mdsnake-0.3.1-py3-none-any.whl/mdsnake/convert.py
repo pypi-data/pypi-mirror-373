d2fa361ff1653ff0f3f8ac667abf7cc3a8c55331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-02 04:05:17 +0000

    Add package mdsnake/0.3.1/mdsnake-0.3.1-py3-none-any.whl

---
+++ packages/mdsnake/mdsnake-0.3.1-py3-none-any.whl/mdsnake/convert.py
@@ -0,0 +1,25 @@
+"""Libraries"""
+from markdown_pdf import MarkdownPdf, Section
+from markdown import markdown
+
+from .dialogs import success_message
+
+def convert_md(markdown_text: str, filename: str, location: str):
+    """
+    Converts markdown to HTML or PDFs
+    :param md:
+    :param filetype:
+    :param filename:
+    :param location:
+    """
+    if filename.split(".")[1] == "html":
+        converted_markdown = markdown(markdown_text, extensions=["fenced_code", "tables"])
+        with open(location / filename, "w", encoding="utf-8") as file:
+            file.write(converted_markdown)
+    elif filename.split(".")[1] == "pdf":
+        pdf = MarkdownPdf()
+        pdf.meta["title"] = "Markdown"
+        pdf.add_section(Section(markdown_text, toc=False))
+        pdf.save(location / filename)
+
+    success_message("Successfully converted!")
