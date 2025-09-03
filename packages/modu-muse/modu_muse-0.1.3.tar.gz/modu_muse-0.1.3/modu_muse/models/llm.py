bf4cacfab2edefcbd25e10a7d5392eaba6ea54d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:22 +0000

    Add package modu-muse/0.1.3/modu_muse-0.1.3.tar.gz

---
+++ packages/modu-muse/modu_muse-0.1.3.tar.gz/modu_muse-0.1.3/modu_muse/models/llm.py
@@ -0,0 +1,19 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class LLMWrapper:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.hidden_size = model.config.hidden_size
+
+    def generate(self, prompt, context=None):
+        if context is not None:
+            prompt = f"{prompt}\n[Vision Context]: {context.detach().cpu().numpy().tolist()}"
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        outputs = self.model.generate(**inputs, max_new_tokens=100)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+def load_llm(name):
+    tokenizer = AutoTokenizer.from_pretrained(name)
+    model = AutoModelForCausalLM.from_pretrained(name)
+    return LLMWrapper(model, tokenizer)
