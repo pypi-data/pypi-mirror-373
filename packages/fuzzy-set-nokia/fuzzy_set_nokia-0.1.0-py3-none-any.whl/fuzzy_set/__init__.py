af90bc3f1fe09f0aa5157c4b10b5eb568bf76e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 04:06:31 +0000

    Add package fuzzy-set-nokia/0.1.0/fuzzy_set_nokia-0.1.0-py3-none-any.whl

---
+++ packages/fuzzy-set-nokia/fuzzy_set_nokia-0.1.0-py3-none-any.whl/fuzzy_set/__init__.py
@@ -0,0 +1,15 @@
+# © 2025 Nokia
+# Licensed under the BSD 3-Clause License
+# SPDX-License-Identifier: BSD-3-Clause
+
+__author__ = "Marc-Olivier Buob"
+__maintainer__ = "Marc-Olivier Buob"
+__email__ = "marc-olivier.buob@nokia-bell-labs.com"
+__license__ = "BSD-3"
+__version__ = "0.1.0"
+
+
+from .fuzzy_number import FuzzyNumber
+from .fuzzy_set import INFINITY, REALS, FuzzySet
+from .intersection import find_intersection
+from .trapezoidal_fuzzy_number import TrapezoidalFuzzyNumber
