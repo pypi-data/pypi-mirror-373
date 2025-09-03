b32b127e25a2cc58bf49d97f65851aa178b8c503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:19 +0000

    Add package PD-loss-balancing/0.1.25/pd_loss_balancing-0.1.25-py3-none-any.whl

---
+++ packages/PD-loss-balancing/pd_loss_balancing-0.1.25-py3-none-any.whl/PD_loss_balancing/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.25"
+
+from .main import (
+    FixedLossWeighter,
+    PDLossWeighter,
+    PLossWeighter,
+    LinearTrajectoryTarget,
+    ConstantTarget,
+    RelativeTarget,
+    Target,
+)
