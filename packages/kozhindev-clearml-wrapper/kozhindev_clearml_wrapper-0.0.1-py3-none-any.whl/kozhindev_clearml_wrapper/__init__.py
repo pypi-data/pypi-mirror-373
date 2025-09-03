860a9778d2b61fa094888d8cc53b6260fc0cb3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:06:53 +0000

    Add package kozhindev-clearml-wrapper/0.0.1/kozhindev_clearml_wrapper-0.0.1-py3-none-any.whl

---
+++ packages/kozhindev-clearml-wrapper/kozhindev_clearml_wrapper-0.0.1-py3-none-any.whl/kozhindev_clearml_wrapper/__init__.py
@@ -0,0 +1,14 @@
+from .utils import (
+    log_metrics,
+    get_local_dataset_path,
+    prepare_task,
+    get_local_model_path
+)
+
+
+__all__ = [
+    "log_metrics",
+    "get_local_dataset_path",
+    "prepare_task",
+    "get_local_model_path"
+]
