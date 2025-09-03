97efd24e185d5cd33d10ca324e4925882b74a138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:05:57 +0000

    Add package seshat-classifier/0.0.18/seshat_classifier-0.0.18.tar.gz

---
+++ packages/seshat-classifier/seshat_classifier-0.0.18.tar.gz/seshat_classifier-0.0.18/tests/test_cosmos.py
@@ -0,0 +1,14 @@
+from seshat_classifier import seshat
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+cosmos = pd.read_csv("~/Documents/Star_Formation/YSO+Classification/Synthetic_Data/Data/COSMOSWeb_Labeled.csv")
+cosmos['Class'] = 'Gal'
+cosmos.loc[cosmos.Label==3,'label'] = 'BD'
+display_labels = np.unique(cosmos.Class.values)
+cosmos, test_cos, display_labels = seshat.ml_test(cosmos,cosmological=True,classes=display_labels,return_test=True)
+
+ax = seshat.cm_custom(cosmos.label.values,cosmos.pred,cmap='Greys',display_labels=display_labels)
+plt.tight_layout()
+plt.savefig("cosmos_cm_test.png")
