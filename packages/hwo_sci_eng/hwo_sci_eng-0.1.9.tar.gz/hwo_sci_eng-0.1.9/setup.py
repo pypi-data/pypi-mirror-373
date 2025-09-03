5424f6933524697af63dd647afd3ef343047eeea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:22 +0000

    Add package hwo_sci_eng/0.1.9/hwo_sci_eng-0.1.9.tar.gz

---
+++ packages/hwo_sci_eng/hwo_sci_eng-0.1.9.tar.gz/hwo_sci_eng-0.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name="hwo_sci_eng",
+      version="0.1.9",
+      description="HWO Science Engineering Interface", 
+      author="Breann Sitarski, NASA/GSFC, Jason Tumlinson, STScI", 
+      author_email=" ",
+      license="CC",
+      keywords=["simulation", "astronomy", "astrophysics"],
+      url="https://github.com/HWO-GOMAP-Working-Groups/Sci-Eng-Interface", 
+      packages=["hwo_sci_eng/obs_config", "hwo_sci_eng/json", "hwo_sci_eng/utils", "hwo_sci_eng/test"], 
+      package_data={'':['*.yaml', '*.json', 'json/*json', '*/*/*.yaml', '*/*.yaml']}, 
+      include_package_data=True,
+      classifiers=[
+          "Programming Language :: Python :: 3.12",
+      ],
+      install_requires=[
+            "setuptools>=61.0", 
+      ],
+)
