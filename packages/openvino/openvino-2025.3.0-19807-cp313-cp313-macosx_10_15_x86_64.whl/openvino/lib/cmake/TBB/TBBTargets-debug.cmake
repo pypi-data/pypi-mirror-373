714661a43308a6f9fdeaace931478928fe62df04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:34 +0000

    Add package openvino/2025.3.0/openvino-2025.3.0-19807-cp313-cp313-macosx_10_15_x86_64.whl

---
+++ packages/openvino/openvino-2025.3.0-19807-cp313-cp313-macosx_10_15_x86_64.whl/openvino/lib/cmake/TBB/TBBTargets-debug.cmake
@@ -0,0 +1,50 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "TBB::tbb" for configuration "Debug"
+set_property(TARGET TBB::tbb APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TBB::tbb PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libtbb_debug.12.13.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libtbb_debug.12.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets TBB::tbb )
+list(APPEND _cmake_import_check_files_for_TBB::tbb "${_IMPORT_PREFIX}/lib/libtbb_debug.12.13.dylib" )
+
+# Import target "TBB::tbbmalloc" for configuration "Debug"
+set_property(TARGET TBB::tbbmalloc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TBB::tbbmalloc PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libtbbmalloc_debug.2.13.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libtbbmalloc_debug.2.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets TBB::tbbmalloc )
+list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc "${_IMPORT_PREFIX}/lib/libtbbmalloc_debug.2.13.dylib" )
+
+# Import target "TBB::tbbmalloc_proxy" for configuration "Debug"
+set_property(TARGET TBB::tbbmalloc_proxy APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TBB::tbbmalloc_proxy PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "TBB::tbbmalloc"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy_debug.2.13.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libtbbmalloc_proxy_debug.2.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets TBB::tbbmalloc_proxy )
+list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc_proxy "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy_debug.2.13.dylib" )
+
+# Import target "TBB::tbbbind_2_5" for configuration "Debug"
+set_property(TARGET TBB::tbbbind_2_5 APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TBB::tbbbind_2_5 PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libtbbbind_2_5_debug.3.13.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libtbbbind_2_5_debug.3.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets TBB::tbbbind_2_5 )
+list(APPEND _cmake_import_check_files_for_TBB::tbbbind_2_5 "${_IMPORT_PREFIX}/lib/libtbbbind_2_5_debug.3.13.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
