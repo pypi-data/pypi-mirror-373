f3ae66e07dbb3a90830307d7f7081aa723c68f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 14:08:10 +0000

    Add package bikidata/0.3.1/bikidata-0.3.1.tar.gz

---
+++ packages/bikidata/bikidata-0.3.1.tar.gz/bikidata-0.3.1/src/bikidata/__init__.py
@@ -0,0 +1,27 @@
+from .main import (
+    build,
+    build_ftss,
+    build_from_iterator,
+    decode_unicode_escapes,
+    literal_to_parts,
+    log,
+)
+
+try:
+    from .semantic import build_semantic
+except:
+    log.warning(
+        "Failed to import build_semantic. "
+        "Make sure you have the required dependencies installed and COHERE_API_KEY env var defined."
+    )
+
+from .query import (
+    spo,
+    query,
+    raw,
+    total,
+    count_by_property,
+    properties,
+    query_async,
+    TimeoutError,
+)
