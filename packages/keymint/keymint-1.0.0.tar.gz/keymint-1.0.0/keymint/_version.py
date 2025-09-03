8a9df2f7e5e7670ffde046aaeb36e9fa7310ea17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:08:28 +0000

    Add package keymint/1.0.0/keymint-1.0.0.tar.gz

---
+++ packages/keymint/keymint-1.0.0.tar.gz/keymint-1.0.0/keymint/_version.py
@@ -0,0 +1,6 @@
+"""KeyMint Python SDK version information."""
+
+__version__ = "1.0.0"
+__author__ = "KeyMint"
+__email__ = "admin@keymint.dev"
+__url__ = "https://github.com/keymint-dev/keymint-python"
