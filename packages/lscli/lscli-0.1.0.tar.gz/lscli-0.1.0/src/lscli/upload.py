2471a979ec2739c8ecdc2602ba2572657c8add87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:24 +0000

    Add package lscli/0.1.0/lscli-0.1.0.tar.gz

---
+++ packages/lscli/lscli-0.1.0.tar.gz/lscli-0.1.0/src/lscli/upload.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from minio import Minio
+
+
+def upload(input_dir: Path, bucket_name: str, minio_client: Minio, prefix: str):
+    file_list = input_dir.glob("**/*")
+    for file_path in file_list:
+        if file_path.is_file():
+            try:
+                object_name = str(Path(prefix) / file_path.relative_to(input_dir))
+                minio_client.fput_object(
+                    bucket_name=bucket_name,
+                    object_name=file_path.name,
+                    file_path=str(file_path),
+                )
+                print(f"Uploaded {file_path.name} to MinIO.")
+            except Exception as e:
+                print(f"Failed to upload {file_path.name}: {e}")
