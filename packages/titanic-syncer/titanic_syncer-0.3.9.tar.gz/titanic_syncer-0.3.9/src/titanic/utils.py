428f822f7ee61e0a9a27fadff95f5febdc481524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 04:07:28 +0000

    Add package titanic-syncer/0.3.9/titanic_syncer-0.3.9.tar.gz

---
+++ packages/titanic-syncer/titanic_syncer-0.3.9.tar.gz/titanic_syncer-0.3.9/src/titanic/utils.py
@@ -0,0 +1,66 @@
+import hashlib
+import os
+import pathspec
+import tempfile
+
+
+def hash_file(filepath):
+    sha256 = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        while chunk := f.read(8192):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+
+def load_ignore_patterns(ignore_file):
+    patterns = []
+
+    if os.path.exists(ignore_file):
+        with open(ignore_file, 'r') as f:
+            patterns = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+    spec = pathspec.PathSpec.from_lines('gitwildmatch', patterns)
+    return spec
+
+
+def should_ignore(file_path, ignore_spec):
+    return ignore_spec.match_file(file_path)
+
+
+def symlink(target, link_name, overwrite=False):
+    '''
+    Create a symbolic link named link_name pointing to target.
+    If link_name exists then FileExistsError is raised, unless overwrite=True.
+    When trying to overwrite a directory, IsADirectoryError is raised.
+    '''
+
+    if not overwrite:
+        os.symlink(target, link_name)
+        return
+
+    # os.replace() may fail if files are on different filesystems
+    link_dir = os.path.dirname(link_name)
+
+    # Create link to target with temporary filename
+    while True:
+        temp_link_name = tempfile.mktemp(dir=link_dir)
+
+        # os.* functions mimic as closely as possible system functions
+        # The POSIX symlink() returns EEXIST if link_name already exists
+        # https://pubs.opengroup.org/onlinepubs/9699919799/functions/symlink.html
+        try:
+            os.symlink(target, temp_link_name)
+            break
+        except FileExistsError:
+            pass
+
+    # Replace link_name with temp_link_name
+    try:
+        # Pre-empt os.replace on a directory with a nicer message
+        if not os.path.islink(link_name) and os.path.isdir(link_name):
+            raise IsADirectoryError(f"Cannot symlink over existing directory: '{link_name}'")
+        os.replace(temp_link_name, link_name)
+    except Exception:
+        if os.path.islink(temp_link_name):
+            os.remove(temp_link_name)
+        raise
