1c717de6afbce3bba30e4f062be0613218abad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 03:14:04 +0000

    Add package physiocore/0.3.3/physiocore-0.3.3-py3-none-any.whl

---
+++ packages/physiocore/physiocore-0.3.3-py3-none-any.whl/physiocore/lib/exercise_lib.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ExerciseType(Enum):
+    """Supported exercise types"""
+    ANKLE_TOE = "ankle_toe_movement"
+    BRIDGING = "bridging"
+    COBRA = "cobra_stretch"
+    PRONE_SLR = "any_prone_slr"
+    SLR = "any_slr"
