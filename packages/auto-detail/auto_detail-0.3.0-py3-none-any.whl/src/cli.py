52f65585eef7ceb77b486b44b6ad71dae75144d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:54 +0000

    Add package auto-detail/0.3.0/auto_detail-0.3.0-py3-none-any.whl

---
+++ packages/auto-detail/auto_detail-0.3.0-py3-none-any.whl/src/cli.py
@@ -0,0 +1,19 @@
+"""This module provides the CLI for auto_detail."""
+
+import click
+from src import auto_detail
+
+
+@click.group(invoke_without_command=True)
+@click.version_option()
+@click.pass_context
+@click.option("--reasons", help="Reasons for the PR.", default="")
+def main(ctx: click.Context, reasons: str):
+    """A CLI tool to automatically generate pull request details."""
+    if ctx.invoked_subcommand is None:
+        auto_detail.main(reasons)
+
+
+main.add_command(auto_detail.new)
+main.add_command(auto_detail.list_details, name="list")
+main.add_command(auto_detail.set_key)
