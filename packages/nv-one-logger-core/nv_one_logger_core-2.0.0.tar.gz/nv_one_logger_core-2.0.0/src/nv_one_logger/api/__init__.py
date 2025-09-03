f7d58c2bf1628f965115007067856043d086f39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 14:07:36 +0000

    Add package nv-one-logger-core/2.0.0/nv_one_logger_core-2.0.0.tar.gz

---
+++ packages/nv-one-logger-core/nv_one_logger_core-2.0.0.tar.gz/nv_one_logger_core-2.0.0/src/nv_one_logger/api/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+"""
+This package contains the API for the OneLogger.
+
+The classes in this package are used to set up OneLogger and capture spans, events, and errors.
+See README.md for more information.
+"""
