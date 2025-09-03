85fc2417f121bee9816be0ef4f473fa2c67154d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:36 +0000

    Add package MAMMOth-commons/0.1.10/mammoth_commons-0.1.10-py3-none-any.whl

---
+++ packages/MAMMOth-commons/mammoth_commons-0.1.10-py3-none-any.whl/tests/test_tc7_researchers.py
@@ -0,0 +1,34 @@
+from mammoth_commons import testing
+from mai_bias.catalogue.dataset_loaders.data_researchers import data_researchers
+from mai_bias.catalogue.model_loaders.compute_researcher_ranking import (
+    model_mitigation_ranking,
+)
+from mai_bias.catalogue.metrics.ranking_fairness import exposure_distance_comparison
+
+
+def test_researchers_ranking_comparison():
+    with testing.Env(
+        data_researchers,
+        model_mitigation_ranking,
+        exposure_distance_comparison,
+    ) as env:
+        dataset = env.data_researchers(
+            paper_graph_path="./data/researchers/physics_papers.csv.tar.bz2",
+            paper_affiliations_path="./data/researchers/affiliations.csv.tar.bz2",
+        )
+
+        model_mitigation = env.model_mitigation_ranking()
+
+        analysis_outcome_mitigation = env.exposure_distance_comparison(
+            dataset,
+            model_mitigation,
+            n_runs=10,
+            sampling_attribute="Nationality_IncomeGroup",
+            ranking_variable="Degree",
+            sensitive=["Gender"],
+        )
+        analysis_outcome_mitigation.show()
+
+
+if __name__ == "__main__":
+    test_researchers_ranking_comparison()
