7f4e05153e0dab6e56bc58ed381853dcef78c559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:05:30 +0000

    Add package skais-mapper/0.1.16/skais_mapper-0.1.16-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/skais-mapper/skais_mapper-0.1.16-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/skais_mapper/illustris/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025-present Philipp Denzel <phdenzel@gmail.com>
+# SPDX-FileNotice: Part of skais-mapper
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""All illustris utilities for loading simulation data.
+
+Adapted from: https://github.com/illustristng/illustris_python
+"""
+
+import skais_mapper.illustris.util
+import skais_mapper.illustris.snapshots
+import skais_mapper.illustris.groupcat #noqa
