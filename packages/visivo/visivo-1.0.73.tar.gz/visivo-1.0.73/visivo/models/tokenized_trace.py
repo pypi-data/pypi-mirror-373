6a7dab0d074b8bc4aa4a4e8cf861ac4878181d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:05:55 +0000

    Add package visivo/1.0.73/visivo-1.0.73.tar.gz

---
+++ packages/visivo/visivo-1.0.73.tar.gz/visivo-1.0.73/visivo/models/tokenized_trace.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Optional, Dict
+
+
+class TokenizedTrace(BaseModel):
+    sql: str
+    cohort_on: str
+    source: str
+    source_type: str
+    groupby_statements: Optional[List[str]] = None
+    select_items: Optional[Dict] = None
+    filter_by: Optional[Dict[str, List[Optional[str]]]] = None
+    order_by: Optional[List[str]] = None
