983abba54f1efa70e83ef1b55e594d8b3881e624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:22 +0000

    Add package scout-browser/4.104.0/scout_browser-4.104.0.tar.gz

---
+++ packages/scout-browser/scout_browser-4.104.0.tar.gz/scout_browser-4.104.0/scout/server/extensions/chanjo_extension.py
@@ -0,0 +1,102 @@
+"""
+Generate coverage reports using chanjo and chanjo-report. Documentation under -> `docs/admin-guide/chanjo_coverage_integration.md`
+"""
+
+import json
+import logging
+
+import requests
+from flask import current_app, request, url_for
+from flask_babel import Babel
+from markupsafe import Markup
+
+LOG = logging.getLogger(__name__)
+REF_CHROM_MT_STATS = "14"
+
+
+class ChanjoReport:
+    """Interfaces with chanjo-report. Creates the /reports endpoints in scout domain. Use Babel to set report language."""
+
+    def init_app(self, app):
+        try:
+            from chanjo_report.server.app import configure_template_filters
+            from chanjo_report.server.blueprints import report_bp
+            from chanjo_report.server.extensions import api as chanjo_api
+        except ImportError as error:
+            chanjo_api = None
+            report_bp = None
+            configure_template_filters = None
+            LOG.error(error)
+
+        if not chanjo_api:
+            raise ImportError(
+                "An SQL db path was given, but chanjo-report could not be registered."
+            )
+
+        def get_locale():
+            """Determine locale to use for translations."""
+            accept_languages = current_app.config.get("ACCEPT_LANGUAGES", ["en"])
+
+            # first check request args
+            session_language = Markup.escape(request.args.get("lang"))
+            if session_language in accept_languages:
+                current_app.logger.info("using session language: %s", session_language)
+                return session_language
+
+            # language can be forced in config
+            user_language = current_app.config.get("REPORT_LANGUAGE")
+            if user_language:
+                return user_language
+
+            # try to guess the language from the user accept header that
+            # the browser transmits.  We support de/fr/en in this example.
+            # The best match wins.
+            return request.accept_languages.best_match(accept_languages)
+
+        babel = Babel(app)
+        babel.init_app(app, locale_selector=get_locale)
+        chanjo_api.init_app(app)
+        configure_template_filters(app)
+        app.register_blueprint(report_bp, url_prefix="/reports")
+        app.config["chanjo_report"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True if app.debug else False
+
+    def mt_coverage_stats(self, individuals: dict) -> dict:
+        """Send a request to chanjo endpoint to retrieve MT vs autosome coverage stats
+
+        Args:
+            individuals(dict): case_obj["individuals"] object
+
+        Returns:
+            coverage_stats(dict): a dictionary with mean MT and autosome transcript coverage stats
+        """
+        coverage_stats = {}
+        ind_ids = []
+        for ind in individuals:
+            ind_ids.append(ind["individual_id"])
+
+        # Prepare complete url to Chanjo report chromosome mean coverage calculation endpoint
+        cov_calc_url = url_for("report.json_chrom_coverage", _external=True)
+        # Prepare request data to calculate mean MT coverage
+        data = dict(sample_ids=",".join(ind_ids), chrom="MT")
+        # Send POST request with data to chanjo endpoint
+        resp = requests.post(cov_calc_url, json=data)
+        mt_cov_data = json.loads(resp.text)
+
+        # Change request data to calculate mean autosomal coverage
+        data["chrom"] = REF_CHROM_MT_STATS
+        # Send POST request with data to chanjo endpoint
+        resp = requests.post(cov_calc_url, json=data)
+        ref_cov_data = json.loads(resp.text)  # mean coverage over the transcripts of ref chrom
+
+        for ind in ind_ids:
+            if not (mt_cov_data.get(ind) and ref_cov_data.get(ind)):
+                continue
+            coverage_info = dict(
+                mt_coverage=round(mt_cov_data[ind], 2),
+                autosome_cov=round(ref_cov_data[ind], 2),
+                mt_copy_number=round((mt_cov_data[ind] / ref_cov_data[ind]) * 2, 2),
+            )
+            coverage_stats[ind] = coverage_info
+
+        return coverage_stats
---
+++ packages/scout-browser/scout_browser-4.104.0.tar.gz/scout_browser-4.104.0/tests/server/blueprints/alignviewers/test_alignviewers_views.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+import responses
+from flask import session, url_for
+
+from scout.utils.ensembl_rest_clients import RESTAPI_URL
+
+
+def test_remote_static_no_auth(app):
+    """Test endpoint that serves alignment files as non-logged user"""
+    # GIVEN a running demo app
+    with app.test_client() as client:
+        # GIVEN that user is not logged in
+        resp = client.get(
+            url_for(
+                "alignviewers.remote_static",
+                file="../demo/ACC5963A1_lanes_1234_star_sorted_sj_filtered_sorted.bed.gz",
+            )
+        )
+        # THEN endpoint should return forbidden (403)
+        assert resp.status_code == 403
+
+
+def test_test_remote_static_not_in_session(app):
+    """Test endpoint that serves alignment files that are not saved in the session"""
+
+    # GIVEN a running demo app
+    with app.test_client() as client:
+        # GIVEN that user is ßlogged in
+        client.get(url_for("auto_login"))
+        # If requested file doesn't have a valid extension
+        resp = client.get(
+            url_for(
+                "alignviewers.remote_static",
+                file="config.py",
+            )
+        )
+        # THEN endpoint should return forbidden (403)
+        assert resp.status_code == 403
+
+
+def test_remote_static(app):
+    """Test endpoint that serves files as a logged user"""
+    # GIVEN a file on disk
+    file = "../demo/ACC5963A1_lanes_1234_star_sorted_sj_filtered_sorted.bed.gz"
+
+    # GIVEN a running demo app
+    with app.test_client() as client:
+        # GIVEN that user is logged in
+        client.get(url_for("auto_login"))
+
+        # GIVEN that resource file exists in user session
+        with client.session_transaction() as session:
+            session["igv_tracks"] = [file]
+
+        # THEN the resource should be available to the user
+        resp = client.get(
+            url_for(
+                "alignviewers.remote_static",
+                file=file,
+            )
+        )
+        assert resp.status_code == 200
+
+
+def test_remote_cors_wrong_resource(app):
+    """Test endpoint that serves as a proxy to the actual remote track on the cloud"""
+    # GIVEN a resource not present in session["igv_tracks"]
+    an_url = "http://google.com"
+
+    # GIVEN a running demo app
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for("auto_login"))
+
+        # WHEN the remote cors endpoint is invoked with an url
+        resp = client.get(url_for("alignviewers.remote_cors", remote_url=an_url))
+
+        # THEN it should return forbidden (403)
+        assert resp.status_code == 403
+
+
+def test_remote_cors(app):
+    """Test endpoint that serves as a proxy to the actual remote track on the cloud"""
+    # GIVEN an igv track on the cloud
+    cloud_track_url = "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/25777460/GRCh37.variant_call.clinical.pathogenic_or_likely_pathogenic.vcf.gz.tbi"
+
+    # GIVEN a running demo app
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for("auto_login"))
+
+        # GIVEN that resource url exists in user session
+        with client.session_transaction() as session:
+            session["igv_tracks"] = [cloud_track_url]
+
+        # WHEN the remote cors endpoint is invoked with cloud_track_url
+        resp = client.get(url_for("alignviewers.remote_cors", remote_url=cloud_track_url))
+
+        # THEN response should be successful
+        assert resp.status_code == 200
+
+
+def test_igv_not_authorized(app, user_obj, case_obj, variant_obj):
+    """Test view requests and produces igv alignments, when the user dosn't have access to the case"""
+
+    # GIVEN an initialized app
+    with app.test_client() as client:
+        # GIVEN that the user is logged in but not authorized to see the page
+        client.get(url_for("auto_login_not_authorized"))
+
+        # WHEN the igv endpoint is invoked with the right parameters
+        resp = client.get(
+            url_for(
+                "alignviewers.igv",
+                institute_id=case_obj["owner"],
+                case_name=case_obj["display_name"],
+                variant_id=variant_obj["_id"],
+            )
+        )
+
+        # THEN the response should be "not authorized" (403)
+        assert resp.status_code == 403
+
+
+def test_igv_authorized(app, user_obj, case_obj, variant_obj):
+    """Test view requests and produces igv alignments, when the user has access to the case"""
+
+    # GIVEN an initialized app
+    with app.test_client() as client:
+        # GIVEN that the user is logged in
+        client.get(url_for("auto_login"))
+
+        # WHEN the igv endpoint is invoked with the right parameters
+        resp = client.get(
+            url_for(
+                "alignviewers.igv",
+                institute_id=case_obj["owner"],
+                case_name=case_obj["display_name"],
+                variant_id=variant_obj["_id"],
+            )
+        )
+
+        # THEN the response should be a valid HTML page
+        assert resp.status_code == 200
+        # AND when the response is closed case IGV tracks should be removed from session
+        resp.close()
+        assert session.get("igv_tracks") is None
+
+
+@responses.activate
+def test_sashimi_igv(app, user_obj, case_obj, variant_obj, ensembl_liftover_response):
+    """Test view requests and produces igv alignments, when the user has access to the case"""
+
+    # GIVEN a mocked response from the Ensembl liftover service
+    chromosome = variant_obj["chromosome"]
+    position = variant_obj["position"]
+    mocked_liftover_url = f"{RESTAPI_URL}/map/human/GRCh37/{chromosome}:{position}..{position}/GRCh38?content-type=application/json"
+    responses.add(
+        responses.GET,
+        mocked_liftover_url,
+        json=ensembl_liftover_response,
+        status=200,
+    )
+
+    # GIVEN an initialized app
+    with app.test_client() as client:
+        # GIVEN that the user is logged in
+        client.get(url_for("auto_login"))
+
+        # WHEN the igv endpoint is invoked with the right parameters
+        resp = client.get(
+            url_for(
+                "alignviewers.sashimi_igv",
+                institute_id=case_obj["owner"],
+                case_name=case_obj["display_name"],
+                variant_id=variant_obj["_id"],
+            )
+        )
+
+        # THEN the response should be a valid HTML page
+        assert resp.status_code == 200
