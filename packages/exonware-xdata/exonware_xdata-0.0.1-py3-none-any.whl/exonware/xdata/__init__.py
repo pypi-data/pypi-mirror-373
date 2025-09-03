4e4917cfdf196961e273dbd50d878c6d233b57c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 14:07:36 +0000

    Add package exonware-xdata/0.0.1/exonware_xdata-0.0.1-py3-none-any.whl

---
+++ packages/exonware-xdata/exonware_xdata-0.0.1-py3-none-any.whl/exonware/xdata/__init__.py
@@ -0,0 +1,26 @@
+"""
+{LIBRARY_NAME}: {LIBRARY_DESCRIPTION}
+
+Company: eXonware.com
+Author: Eng. Muhammad AlShehri
+Email: connect@exonware.com
+Version: 0.0.1
+Generation Date: {GENERATION_DATE}
+"""
+
+# Version information
+__version__ = "0.0.1"
+__author__ = "Eng. Muhammad AlShehri"
+__email__ = "connect@exonware.com"
+__company__ = "eXonware.com"
+
+# Import your main classes/functions here
+# Example:
+# from .core import YourMainClass
+# from .utils import your_utility_function
+
+# Define what gets imported with "from exonware.{LIBRARY_NAME} import *"
+__all__ = [
+    # Add your public API here
+    # Example: "YourMainClass", "your_utility_function"
+]
