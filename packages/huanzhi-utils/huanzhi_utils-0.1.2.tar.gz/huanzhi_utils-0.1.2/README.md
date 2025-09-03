22e71d6f6da6156423a26fe2fe693b1bc2cf7506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:17 +0000

    Add package huanzhi-utils/0.1.2/huanzhi_utils-0.1.2.tar.gz

---
+++ packages/huanzhi-utils/huanzhi_utils-0.1.2.tar.gz/huanzhi_utils-0.1.2/README.md
@@ -0,0 +1,3 @@
+# Huanzhi_Utils
+
+This is a utils library that I often use in my project. I'm lazy and I don't want to copy and paste the same code to every project, so I decided to create this library. I will keep updating it as I develop more projects.
---
+++ packages/huanzhi-utils/huanzhi_utils-0.1.2.tar.gz/huanzhi_utils-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="huanzhi_utils",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+    author="Huanzhi Mao",
+    author_email="huanzhimao@gmail.com"
+)
