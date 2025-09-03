be7d516353d4253b86b8a02ff9d47a009eeb9fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:06:16 +0000

    Add package Grok3API/0.1.0rc1/grok3api-0.1.0rc1.tar.gz

---
+++ packages/Grok3API/grok3api-0.1.0rc1.tar.gz/grok3api-0.1.0rc1/grok3api/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+# log_level = logging.DEBUG
+logger = logging.getLogger("grok3api")
+# logger.setLevel(log_level)
+
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(log_level)
+
+# formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s")
+# console_handler.setFormatter(formatter)
+#
+# logger.addHandler(console_handler)
