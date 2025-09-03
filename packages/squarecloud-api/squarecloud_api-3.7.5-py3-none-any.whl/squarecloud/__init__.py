141079b8e4061d1088a04eed1667783cc4fb0b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:05:36 +0000

    Add package squarecloud-api/3.7.5/squarecloud_api-3.7.5-py3-none-any.whl

---
+++ packages/squarecloud-api/squarecloud_api-3.7.5-py3-none-any.whl/squarecloud/__init__.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from . import errors, utils
+from .app import Application
+from .client import Client
+from .data import (
+    AppData,
+    Backup,
+    BackupInfo,
+    DeployData,
+    DNSRecord,
+    DomainAnalytics,
+    FileInfo,
+    LogsData,
+    PlanData,
+    ResumedStatus,
+    StatusData,
+    UploadData,
+    UserData,
+)
+from .file import File
+from .http.endpoints import Endpoint
+from .http.http_client import Response
+
+__all__ = [
+    'Application',
+    'Client',
+    'File',
+    'Endpoint',
+    'Response',
+    'AppData',
+    'Backup',
+    'BackupInfo',
+    'DeployData',
+    'DNSRecord',
+    'DomainAnalytics',
+    'FileInfo',
+    'LogsData',
+    'PlanData',
+    'ResumedStatus',
+    'StatusData',
+    'UploadData',
+    'UserData',
+    'errors',
+    'utils',
+]
+
+__version__ = '3.7.4'
