5ddfaf5bfa1623d2bd877e29dcf204aadd4713c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:07:42 +0000

    Add package shadowshell/202509020003/shadowshell-202509020003-py3-none-any.whl

---
+++ packages/shadowshell/shadowshell-202509020003-py3-none-any.whl/shadowshell/monitor/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: shadow shell
+"""
+
+from shadowshell.monitor.monitor import function_monitor, performance_monitor
+
+__all__ = [
+    'function_monitor',
+    'performance_monitor'
+]
