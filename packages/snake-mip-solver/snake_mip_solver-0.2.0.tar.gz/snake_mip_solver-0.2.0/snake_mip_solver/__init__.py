3109ee4890f2d34047067a80df606854f22e79e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 19:06:11 +0000

    Add package snake-mip-solver/0.2.0/snake_mip_solver-0.2.0.tar.gz

---
+++ packages/snake-mip-solver/snake_mip_solver-0.2.0.tar.gz/snake_mip_solver-0.2.0/snake_mip_solver/__init__.py
@@ -0,0 +1,9 @@
+"""
+Snake MIP Solver -  Mixed Integer Programming approach to solving Snake logic puzzles.
+"""
+
+from .puzzle import SnakePuzzle
+from .solver import SnakeSolver
+
+__version__ = "0.1.0"
+__all__ = ["SnakePuzzle", "SnakeSolver"]
