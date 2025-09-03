f3bb4d192f93a140471215555731b39c15b880fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:06:05 +0000

    Add package titascodes-nester/1.2.0/titascodes_nester-1.2.0.tar.gz

---
+++ packages/titascodes-nester/titascodes_nester-1.2.0.tar.gz/titascodes_nester-1.2.0/test/testernester.py
@@ -0,0 +1,8 @@
+import nester-1.0.0
+movies = [
+ "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+ ["Graham Chapman",
+ ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol(movies)
+
