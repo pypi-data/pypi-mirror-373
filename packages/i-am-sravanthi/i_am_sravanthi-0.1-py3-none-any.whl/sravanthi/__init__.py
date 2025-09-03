4d42da2fad7e55fb3b70afa3758e2f26a2769823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:28 +0000

    Add package i-am-sravanthi/0.1/i_am_sravanthi-0.1-py3-none-any.whl

---
+++ packages/i-am-sravanthi/i_am_sravanthi-0.1-py3-none-any.whl/sravanthi/__init__.py
@@ -0,0 +1,17 @@
+class Sravanthi:
+    def __str__(self):
+        return (
+            "Hi, I'm Sravanthi.\n"
+            "Present nenu B.Tech 2nd year, CSE chaduvuthunnanu...!.\n"
+            "Naku ma amma, nannante chaala istam...!.\n"
+            "Present aite machine learning, data science nerchukuntunnanu...!.\n"
+            "inka na gurinchi cheppalante, aha, anni cheppestara enti...?...!.\n"
+            "Tarvata Maatladukundam...!.\n"
+            "ika unta mari bye bye...!.\n"
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+import sys
+sys.modules[__name__] = Sravanthi()
