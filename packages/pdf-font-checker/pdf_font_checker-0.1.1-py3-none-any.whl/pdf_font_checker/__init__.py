7400449fb75f7ebd2ee9569d2d1bff546b18d695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:05:39 +0000

    Add package pdf-font-checker/0.1.1/pdf_font_checker-0.1.1-py3-none-any.whl

---
+++ packages/pdf-font-checker/pdf_font_checker-0.1.1-py3-none-any.whl/pdf_font_checker/__init__.py
@@ -0,0 +1,4 @@
+"""pdf_font_checker package."""
+from .core import list_pdf_fonts, analyze_pdf, get_pdf_info_dict, get_pdf_terminal_output, ensure_mutool
+__all__ = ["list_pdf_fonts", "analyze_pdf", "get_pdf_info_dict", "get_pdf_terminal_output", "ensure_mutool"]
+__version__ = "0.1.0"
