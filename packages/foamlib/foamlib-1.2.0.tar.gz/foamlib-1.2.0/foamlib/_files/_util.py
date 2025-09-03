10c0798209e7f0c161903d62952cf9f98f1139fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:07:31 +0000

    Add package foamlib/1.2.0/foamlib-1.2.0.tar.gz

---
+++ packages/foamlib/foamlib-1.2.0.tar.gz/foamlib-1.2.0/foamlib/_files/_util.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import sys
+from typing import TypeVar
+
+if sys.version_info >= (3, 9):
+    from collections.abc import Iterable, MutableMapping
+else:
+    from typing import Iterable, MutableMapping
+
+from multicollections import MultiDict
+from multicollections.abc import MutableMultiMapping
+
+_K = TypeVar("_K")
+_V = TypeVar("_V")
+_MM = TypeVar("_MM", bound=MutableMapping[_K, _V])  # type: ignore[valid-type]
+
+
+def add_to_mapping(
+    d: _MM,
+    key: _K,
+    value: _V,
+    /,
+) -> _MM | MultiDict[_K, _V]:
+    if isinstance(d, MutableMultiMapping):
+        d.add(key, value)
+        return d
+
+    if key not in d:
+        d[key] = value
+        return d
+
+    ret: MultiDict[_K, _V] = MultiDict(d)
+    ret.add(key, value)
+    return ret
+
+
+def as_dict_check_unique(items: Iterable[tuple[_K, _V]]) -> dict[_K, _V]:
+    ret = {}
+    for key, value in items:
+        if key in ret:
+            msg = f"Duplicate key found: {key}"
+            raise ValueError(msg)
+        ret[key] = value
+    return ret
