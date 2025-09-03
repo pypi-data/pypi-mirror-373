1da1f2ceba4c75c7b1b4865df34ae5ca30ed6445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 04:06:07 +0000

    Add package logzai-otlp/0.1.7/logzai_otlp-0.1.7-py3-none-any.whl

---
+++ packages/logzai-otlp/logzai_otlp-0.1.7-py3-none-any.whl/logzai_otlp/__init__.py
@@ -0,0 +1,13 @@
+from .main import init, log, debug, info, warning, warn, error, critical, shutdown
+
+__all__ = [
+    "init",
+    "log",
+    "debug",
+    "info",
+    "warning",
+    "warn",
+    "error",
+    "critical",
+    "shutdown",
+]
