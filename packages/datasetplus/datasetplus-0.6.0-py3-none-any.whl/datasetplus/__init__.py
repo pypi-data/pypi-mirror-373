71143168ac30306c82363b1a0018bd9145f40500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:26 +0000

    Add package datasetplus/0.6.0/datasetplus-0.6.0-py3-none-any.whl

---
+++ packages/datasetplus/datasetplus-0.6.0-py3-none-any.whl/datasetplus/__init__.py
@@ -0,0 +1,9 @@
+"""DatasetPlus - An enhanced wrapper for Hugging Face datasets."""
+
+from .core import DatasetPlus, DatasetPlusMeta, DatasetPlusExcels, MyLLMTool, DataTool
+
+__version__ = "0.1.0"
+__author__ = "sweeneyhe"
+__email__ = "sweeenyhe@gmail.com"
+
+__all__ = ["DatasetPlus", "DatasetPlusMeta", "DatasetPlusExcels", "MyLLMTool", "DataTool"]
