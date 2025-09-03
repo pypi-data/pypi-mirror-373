1c421f4da1ac1bd1fb7891c4935a65f29357edd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 03:13:57 +0000

    Add package freetext-sdk/1.2768.0/freetext_sdk-1.2768.0-py3-none-any.whl

---
+++ packages/freetext-sdk/freetext_sdk-1.2768.0-py3-none-any.whl/freetext-sdk/api/__init__.py
@@ -0,0 +1,40 @@
+# flake8: noqa
+
+# import apis into api package
+from freetext-sdk.api.chatbot_api import ChatbotApi
+from freetext-sdk.api.connectors_api import ConnectorsApi
+from freetext-sdk.api.connectors_data_api import ConnectorsDataApi
+from freetext-sdk.api.conversation_api import ConversationApi
+from freetext-sdk.api.dataset_api import DatasetApi
+from freetext-sdk.api.document_connector_api import DocumentConnectorApi
+from freetext-sdk.api.embedder_endpoint_api import EmbedderEndpointApi
+from freetext-sdk.api.google_drive_connector_api import GoogleDriveConnectorApi
+from freetext-sdk.api.llm_endpoint_api import LLMEndpointApi
+from freetext-sdk.api.metadata_api import MetadataApi
+from freetext-sdk.api.one_drive_connector_api import OneDriveConnectorApi
+from freetext-sdk.api.one_note_connector_api import OneNoteConnectorApi
+from freetext-sdk.api.pinecone_connector_api import PineconeConnectorApi
+from freetext-sdk.api.publication_office_connector_api import PublicationOfficeConnectorApi
+from freetext-sdk.api.qn_a_connector_api import QnAConnectorApi
+from freetext-sdk.api.rdf_connector_api import RDFConnectorApi
+from freetext-sdk.api.share_point_connector_api import SharePointConnectorApi
+from freetext-sdk.api.slack_bot_api import SlackBotApi
+from freetext-sdk.api.socket_api import SocketApi
+from freetext-sdk.api.speech_to_text_api import SpeechToTextApi
+from freetext-sdk.api.summariser_api import SummariserApi
+from freetext-sdk.api.synonyms_api import SynonymsApi
+from freetext-sdk.api.task_chat_api import TaskChatApi
+from freetext-sdk.api.task_entity_linking_api import TaskEntityLinkingApi
+from freetext-sdk.api.task_nlp_api import TaskNLPApi
+from freetext-sdk.api.task_relation_extraction_api import TaskRelationExtractionApi
+from freetext-sdk.api.task_report_copilot_api import TaskReportCopilotApi
+from freetext-sdk.api.task_search_api import TaskSearchApi
+from freetext-sdk.api.task_sparql2_text_api import TaskSparql2TextApi
+from freetext-sdk.api.task_text2_sparql_api import TaskText2SparqlApi
+from freetext-sdk.api.tasks_api import TasksApi
+from freetext-sdk.api.websearch_connector_api import WebsearchConnectorApi
+from freetext-sdk.api.website_connector_api import WebsiteConnectorApi
+from freetext-sdk.api.default_api import DefaultApi
+from freetext-sdk.api.e_consilium_search_connector_api import EConsiliumSearchConnectorApi
+from freetext-sdk.api.e_consilium_search_and_index_connector_api import EConsiliumSearchAndIndexConnectorApi
+
