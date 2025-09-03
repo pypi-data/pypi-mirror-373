71b834d74db3ab971cfcb47f4dc15b10e5b3b2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:33 +0000

    Add package ppplca/0.2.34/ppplca-0.2.34.tar.gz

---
+++ packages/ppplca/ppplca-0.2.34.tar.gz/ppplca-0.2.34/ppplca/Commands/RunCommand.py
@@ -0,0 +1,217 @@
+class RunCommand:
+
+    def __init__(self):
+        pass
+
+    def handle(self,file,sheet_name):
+        import lca_algebraic as agb
+        import bw2data as bd
+
+        self.set_project_name()
+        answer = input("Would you like to conduct a regionalized assessment for water use, land use related biodiversity loss, and human health impacts from particulate matter or a non-regionalized assessment using ReCiPe 2016? [regionalized/recipe]")
+        if answer.lower() == "regionalized":
+            impact_categories = [agb.findMethods('GWP_100a', mainCat='IPCC_AR6')[0],
+                        agb.findMethods('GWP_100a', mainCat='IPCC_AR6')[1],
+                        agb.findMethods('GWP_100a', mainCat='IPCC_AR6')[2],
+                        agb.findMethods('GWP_100a', mainCat='IPCC_AR6')[3],
+                        agb.findMethods('Particulate matter', mainCat='PM regionalized')[0],
+                        agb.findMethods('Water stress', mainCat='AWARE regionalized')[0],
+                        agb.findMethods('Occupation', mainCat='Biodiversity regionalized')[0],
+                        agb.findMethods('Transformation', mainCat='Biodiversity regionalized')[0]]
+            af_reg_name, ei_reg_name, bio_name = self.set_database_names_regionalized()
+            self.check_databases(af_reg_name, ei_reg_name)
+            value_chains_data = self.import_value_chains(file,sheet_name)
+            self.analysis(ei_reg_name, af_reg_name, bio_name, value_chains_data, impact_categories)
+        elif answer.lower() == "recipe":
+            af_name, ei_name, bio_name = self.set_database_names()
+            self.check_databases(af_name, ei_name)
+            self.update_recipe_water_use(bio_name)
+            impact_categories = [method for method in bd.methods if 'ReCiPe 2016 v1.03, midpoint (H) no LT' in method[0]]
+            value_chains_data = self.import_value_chains(file,sheet_name)
+            self.analysis(ei_name, af_name, bio_name, value_chains_data, impact_categories)
+        else:
+            print("Please enter either 'regionalized' or 'recipe'.")
+            return
+
+    @staticmethod
+    def set_project_name():
+        import bw2data as bd
+        from ppplca.config import config
+
+        bd.projects.dir
+        project = config('project.name')
+        bd.projects.set_current(project)
+    
+    @staticmethod
+    def update_recipe_water_use(bio_name):
+        import bw2data as bd
+
+        bio3 = bd.Database(bio_name)
+        method = [method for method in bd.methods if 'ReCiPe 2016 v1.03, midpoint (H) no LT' in method[0] and "water use no LT" in method[1] and "water consumption potential (WCP) no LT" in method[2]][0]
+        m = bd.Method(method)
+        cfs = m.load()
+
+        water_use_list = [act for act in bio3 if "Water" in act['name']
+                            and 'natural resource' in act['categories']
+                            and 'air' not in act['name']
+                            and 'ocean' not in act['name']
+                            and 'ocean' not in act.get('categories')]
+        water_emission_list = [act for act in bio3 if "Water" in act['name']
+                                and 'water' in act['categories']
+                                and 'ocean' not in act.get('categories')]
+
+        existing_flows = {cf[0] for cf in cfs}
+
+        for water_flow in water_use_list:
+            if water_flow.key not in existing_flows:
+                new_cf = (water_flow.key, 1)
+                cfs.append(new_cf)
+        for water_flow in water_emission_list:
+            if water_flow.key not in existing_flows:
+                new_cf = (water_flow.key, -1)
+                cfs.append(new_cf)
+
+        m.write(cfs)
+
+    @staticmethod
+    def set_database_names_regionalized():
+        af_reg_name = "agrifootprint 6.3 all allocations_regionalized"
+        ei_reg_name = "ecoinvent-3.10-cutoff_regionalized"
+        bio_name = "ecoinvent-3.10-biosphere"
+
+        return af_reg_name, ei_reg_name, bio_name
+    
+    @staticmethod
+    def set_database_names():
+        af_reg_name = "agrifootprint 6.3 all allocations"
+        ei_reg_name = "ecoinvent-3.10-cutoff"
+        bio_name = "ecoinvent-3.10-biosphere"
+
+        return af_reg_name, ei_reg_name, bio_name
+    
+    @staticmethod
+    def check_databases(af_name, ei_name):
+        import bw2data as bd
+
+        if ei_name in bd.databases and af_name in bd.databases:
+            print(f"{ei_name} and {af_name} have already been imported.")
+            
+        else:
+            print("Databases have not been imported yet. Please run script databases_setup.py first.")
+            raise ValueError("Databases not found.")
+    
+    @staticmethod
+    def import_value_chains(file, sheet_name = None):
+        import pandas as pd
+
+        if file == "value_chains_test.xlsx":
+            value_chains_data = pd.read_excel(file, sheet_name='Value_chains')
+        elif sheet_name:
+            value_chains_data = pd.read_excel(file, sheet_name=sheet_name)
+        else:
+            value_chains_data = pd.read_excel(file, sheet_name=0)
+
+        return value_chains_data
+    
+    def analysis(self, ei_reg_name, af_reg_name, bio_name, value_chains_data, impact_categories):
+        import pandas as pd
+        import lca_algebraic as agb
+        from ppplca.Models.ValueChain import ValueChain
+        from ppplca.Actions.CreateParams import CreateParams
+
+        for _, value_chain_data in value_chains_data.iterrows():
+
+            location_string = self.create_location_string(value_chain_data)
+
+            ValueChain_ = ValueChain(value_chain_data['product'], location_string)
+
+            #Load parametrized formulas
+            name_formula_sheet = f"Formulas_{ValueChain_.product}"
+            formulas = pd.read_excel("Processing_data.xlsx", sheet_name = name_formula_sheet)
+
+            user_db = "ForegroundDB"
+            agb.resetDb(user_db)
+            agb.resetParams()
+
+            print(f"Analysis for {ValueChain_.product} in locations {ValueChain_.location_string}.")
+
+            CreateParams.createProcessParams(product=ValueChain_.product)
+            params = agb.all_params()
+
+            for i in range(1,len(value_chain_data),2):
+                result = self.load_production_stages(i, value_chain_data, formulas, ValueChain_, ei_reg_name, af_reg_name, bio_name, user_db, params)
+                if result is None:
+                    continue
+                else:
+                    stage_name, stage, params = result
+                ValueChain_.addStage(stage_name,stage)
+
+            self.LCA_calculations(ValueChain_, params, user_db, impact_categories)
+
+    @staticmethod
+    def create_location_string(value_chain_data):
+        locations = []
+        processing_locations = []
+        for i in range(1,len(value_chain_data),2):
+            if value_chain_data.index.tolist()[i] == 'cultivation_country':
+                locations.append(value_chain_data.iloc[i].split(" - ")[1])
+            elif value_chain_data.index.tolist()[i] == 'pointofuse_country':
+                locations.append(value_chain_data.iloc[i].split(" - ")[1])
+            elif type(value_chain_data.iloc[i]) == str and value_chain_data.iloc[i].split(" - ")[1] not in processing_locations:
+                locations.append(value_chain_data.iloc[i].split(" - ")[1])
+                processing_locations.append(value_chain_data.iloc[i].split(" - ")[1])
+        location_string = '-'.join(locations)
+
+        return location_string
+    
+    @staticmethod
+    def load_production_stages(i, value_chain_data, formulas, ValueChain_, ei_reg_name, af_reg_name, bio_name, user_db, params):
+        import numpy as np
+        from ppplca.Actions.CreateParams import CreateParams
+        from ppplca.Models.ProductionStage import ProductionStage
+        import lca_algebraic as agb
+
+        if type(value_chain_data.iloc[i]) == float and np.isnan(value_chain_data.iloc[i]):
+            return
+        elif i == 1:
+            stage_name = value_chain_data.index[i].split("_")[0]
+            stage = ProductionStage(stage_name,value_chain_data.iloc[i],False,None)
+        else:
+            stage_name = value_chain_data.index[i].split("_")[0]
+            previous_location = value_chain_data[:i][value_chain_data[:i].str.contains("-", na=False)].iloc[-1]
+            if value_chain_data.iloc[i-1] == "Yes":
+                stage = ProductionStage(stage_name,value_chain_data.iloc[i],True,previous_location)
+                formula_transport_quantity = formulas[formulas["production_stage"] == stage_name].iloc[0]["formula"]
+                stage.loadTransportExchanges()
+                CreateParams.createTransportParams(stage,ValueChain_.crop_name_short)
+                params = agb.all_params()
+                stage.createTransportActivities(ei_reg_name,user_db,params,formula_transport_quantity)
+            else:
+                stage = ProductionStage(stage_name,value_chain_data.iloc[i],False,previous_location)
+        if value_chain_data.index.tolist()[i] == 'cultivation_country':
+            stage.loadAgriExchange()
+        else:
+            exchanges = formulas[formulas["production_stage"] == stage_name]
+            stage.loadExchanges(exchanges)
+        stage.loadActivities(ValueChain_.crop_name, ei_reg_name, af_reg_name, bio_name, user_db)
+        print(f'Creating activity for {stage_name}.')
+        stage.createForegroundActivities(user_db, params)
+
+        return stage_name, stage, params
+            
+    @staticmethod
+    def LCA_calculations(ValueChain_, params, user_db, impact_categories):
+        import numpy as np
+        import matplotlib
+        matplotlib.use("Agg")
+        from ppplca.config import config
+        from ppplca.Models.LCA import LCA
+            
+        n_iterations = int(config('montecarlo.n_iterations'))
+        np.random.seed(42)
+        lca = LCA(impact_categories, params)
+        lca.createParameterValues(n_iterations)
+        lca.LCATotalInventory(user_db)
+        lca.ContributionAnalysis(ValueChain_, n_iterations)
+        lca.calculateSobolIndices()
+        lca.exportResults(ValueChain_)
---
+++ packages/ppplca/ppplca-0.2.34.tar.gz/ppplca-0.2.34/ppplca/Commands/SetupDatabaseCommand.py
@@ -0,0 +1,205 @@
+class SetupDatabaseCommand:
+
+    def __init__(self):
+        pass
+
+    def handle(self):
+        import bw2data as bd
+        import time
+        from ppplca.config import config
+
+        bd.projects.dir
+        bd.projects.set_current(config('project.name'))
+
+        answer = input("Would you like to set up regionalized databases to assess regionalized water use ipacts, land use related biodiversity loss, and human health impacts from particulate matter emissions (this will take around 48h)? [Y/n] ")
+        if answer.lower() == "y":
+            answer = input("Database setup is starting, it will take around 48 hours to complete on a regular laptop. Are you sure you want to continue? [Y/n]")
+            if answer.lower() != "y":
+                current_time = time.localtime()
+                print(f"[{time.strftime('%H:%M:%S', current_time)}] Database setup aborted. Rerun the command...")
+                return 
+            print(f"Database setup is starting...")
+            
+            start = time.time()
+
+            ei_name, bio_name = self.load_ecoinvent_database()
+            af_name = self.load_agrifootprint_database(ei_name, bio_name)
+            eidb = bd.Database(ei_name)
+            afdb = bd.Database(af_name)
+            eidb_reg, afdb_reg = self.regionalize_databases(ei_name, af_name)
+            self.create_electricity_market_groups(eidb_reg)
+            self.create_agri_activities(eidb_reg, afdb_reg)
+            self.create_heat_activities(eidb_reg)
+            self.create_electricity_market_groups(eidb)
+            self.create_agri_activities(eidb, afdb)
+            self.create_heat_activities(eidb)
+
+            end = time.time()
+            print(f"Database setup completed in {end - start} seconds.")
+        elif answer.lower() == "n":
+            start = time.time()
+
+            ei_name, bio_name = self.load_ecoinvent_database()
+            af_name = self.load_agrifootprint_database(ei_name, bio_name)
+            eidb = bd.Database(ei_name)
+            afdb = bd.Database(af_name)
+            self.create_electricity_market_groups(eidb)
+            self.create_agri_activities(eidb, afdb)
+            self.create_heat_activities(eidb)
+
+            end = time.time()
+            print(f"Database setup completed in {end - start} seconds.")
+        else:
+            print("Please enter either 'Y' or 'n'.")
+            return
+
+    @staticmethod
+    def load_ecoinvent_database():
+        import bw2data as bd
+        import bw2io as bi
+        from ecoinvent_interface import Settings
+        from ppplca.config import config
+
+        print("Loading ecoinvent database...")
+        ei_name = "ecoinvent-3.10-cutoff"
+        bio_name = "ecoinvent-3.10-biosphere"
+        my_settings = Settings(username=config('database.username'), password=config('database.password'))
+        if ei_name in bd.databases and bio_name in bd.databases:
+            print(ei_name + " and " + bio_name + " have already been imported.")
+        elif ei_name in bd.databases and not bio_name in bd.databases:
+            print(ei_name + " has already been imported." + bio_name + " is missing. Please, delete " + ei_name + " and run the command again.")
+        else:
+            bi.import_ecoinvent_release("3.10","cutoff",my_settings.username,my_settings.password)
+
+        print("Ecoinvent database loaded successfully.")
+
+        return ei_name, bio_name
+
+    @staticmethod
+    def load_agrifootprint_database(ei_name, bio_name):
+        from ppplca.Actions.import_agrifootprint_db_functions import import_agrifootprint
+
+        import_agrifootprint(ei_name,bio_name)
+
+        af_name = "agrifootprint 6.3 all allocations"
+
+        return af_name
+    
+    @staticmethod
+    def regionalize_databases(ei_name, af_name):
+        import bw2data as bd
+        from ppplca.Actions.bw_base_set_up import bw_set_up, regionalize_db
+
+        bw_set_up()
+        regionalize_db(ei_name)
+        regionalize_db(af_name)
+
+        eidb_reg = bd.Database("ecoinvent-3.10-cutoff_regionalized")
+        afdb_reg = bd.Database("agrifootprint 6.3 all allocations_regionalized")
+
+        return eidb_reg, afdb_reg
+    
+    @staticmethod
+    def create_electricity_market_groups(eidb_reg):
+        import pandas as pd
+        import importlib.resources as resources
+        import numpy as np
+
+        with resources.open_text("ppplca.data.grid_shares","Electricity_grid_shares_CN_pea.csv") as f:
+            electricity_share_CN_pea = pd.read_csv(f, sep = ";", decimal = ".", dtype= {"Region": str, "Grid": str, "Area": np.float64, "Share": np.float64})
+        with resources.open_text("ppplca.data.grid_shares","Electricity_grid_shares_CN_soy.csv") as f:
+            electricity_share_CN_soy = pd.read_csv(f, sep = ";", decimal = ".", dtype= {"Region": str, "Grid": str, "Area": np.float64, "Share": np.float64})
+        with resources.open_text("ppplca.data.grid_shares","Electricity_grid_shares_CN_wheat.csv") as f:
+            electricity_share_CN_wheat = pd.read_csv(f, sep = ";", decimal = ".", dtype= {"Region": str, "Grid": str, "Area": np.float64, "Share": np.float64})
+        with resources.open_text("ppplca.data.grid_shares","Electricity_grid_shares_US_soy.csv") as f:
+            electricity_share_US_soy = pd.read_csv(f, sep = ";", decimal = ".", dtype= {"Region": str, "Grid": str, "Area": np.float64, "Share": np.float64})
+        with resources.open_text("ppplca.data.grid_shares","Electricity_grid_shares_US_wheat.csv") as f:
+            electricity_share_US_wheat = pd.read_csv(f, sep = ";", decimal = ".", dtype= {"Region": str, "Grid": str, "Area": np.float64, "Share": np.float64}) 
+
+        SetupDatabaseCommand.create_electricity_market_group_processes("CN-SGCC",electricity_share_CN_pea,"Pea",eidb_reg)
+        SetupDatabaseCommand.create_electricity_market_group_processes("CN-SGCC",electricity_share_CN_soy,"Soy",eidb_reg)
+        SetupDatabaseCommand.create_electricity_market_group_processes("CN-SGCC",electricity_share_CN_wheat,"Wheat",eidb_reg)
+        SetupDatabaseCommand.create_electricity_market_group_processes("US",electricity_share_US_soy,"Soy",eidb_reg)
+        SetupDatabaseCommand.create_electricity_market_group_processes("US",electricity_share_US_wheat,"Wheat",eidb_reg)
+
+    @staticmethod
+    def create_electricity_market_group_processes(country,grid_shares,product,db):
+
+        if [act for act in db if "market group for electricity used in " + product + " processing, low voltage" == act["name"] and country[:2] in act["location"]] != []:
+            print("Electricity market group for " + product + " processing in " + country[:2] + " already exists.")
+        else:
+            existing_electricity_market_group = [act for act in db if "market group for electricity, low voltage" == act['name'] and country == act['location']][0]
+
+            new_electricity_market_group = existing_electricity_market_group.copy()
+            new_electricity_market_group["name"] = "market group for electricity used in " + product + " processing, low voltage"
+            if country == "CN-SGCC":
+                new_electricity_market_group["location"] = "CN"
+                new_electricity_market_group.save()
+            for exchange in [exc for exc in new_electricity_market_group.exchanges() if exc["type"] == "technosphere"]:
+                exchange_input = [act for act in db if exchange["input"][1] == act['code']][0]
+                if exchange_input["location"] in grid_shares["Grid"].to_list():
+                    exchange["amount"] = grid_shares.loc[grid_shares["Grid"]==exchange_input["location"]]["Share"].to_list()[0]
+                    exchange.save()
+                else:
+                    exchange.delete()
+            if country == "CN-SGCC":
+                central_grid = [act for act in db if "market for electricity, low voltage" == act['name'] and "CN-CSG" == act['location']][0]
+                new_electricity_market_group.new_exchange(input=central_grid.key,amount=grid_shares.loc[grid_shares["Grid"]=="CN-CSG"]["Share"].to_list()[0],unit="kilowatt hour",type="technosphere").save()
+                new_electricity_market_group.new_exchange(input=new_electricity_market_group.key,amount=1,unit="kilowatt hour",type="production").save()
+            new_electricity_market_group.save()
+            print(new_electricity_market_group["name"] + " created.")
+
+    @staticmethod
+    def create_agri_activities(eidb_reg, afdb_reg):
+        import pandas as pd
+        from ppplca.Actions.CreateAgriActivities import CreateAgriActivities
+
+        countries = pd.read_excel('value_chains_test.xlsx', sheet_name="Countries (don't change)")
+
+        crop_names = ["Peas","Soybeans","Wheat"]
+        for country in countries["Code"]:
+            if SetupDatabaseCommand.isinEurope(country):
+                for crop_name in crop_names:
+                    CreateAgriActivities.create_missing_agri_activities(country,crop_name,eidb_reg,afdb_reg)
+    
+    @staticmethod
+    def isinEurope(country):
+        import pandas as pd
+        import importlib.resources as resources
+
+        with resources.open_text("ppplca.data.transport","European_countries.csv") as f:
+            european_countries = pd.read_csv(f, sep = ";", dtype= {"Country": str, "Code": str})
+        test = country in list(european_countries["Code"])
+        return test
+    
+    @staticmethod
+    def create_heat_activities(eidb_reg):
+        import pandas as pd
+        from ppplca.Actions.CreateHeatActivities import CreateHeatActivities
+
+        countries = pd.read_excel('value_chains_test.xlsx', sheet_name="Countries (don't change)")
+
+        for country in countries["Code"]:
+            if country == "CN":
+                for crop_name in ["Peas","Soybeans","Wheat"]:
+                    test_activity = [act for act in eidb_reg if "custom_heat_process_" + str(country) + "_" + str(crop_name) in act['code'] and country in act['location']]
+                    if test_activity == []:
+                        print("Creating heat production activity for " + crop_name + " in " + country + ".")
+                        CreateHeatActivities.create_heat_production_process(country,crop_name,eidb_reg)
+                    else:
+                        print("Heat production activity for " + crop_name + " in " + country + " is already present.")
+            elif country == "US":
+                for crop_name in ["Soybeans","Wheat"]:
+                    test_activity = [act for act in eidb_reg if "custom_heat_process_" + str(country) + "_" + str(crop_name) in act['code'] and country in act['location']]
+                    if test_activity == []:
+                        print("Creating heat production activity for " + crop_name + " in " + country + ".")
+                        CreateHeatActivities.create_heat_production_process(country,crop_name,eidb_reg)
+                    else:
+                        print("Heat production activity for " + crop_name + " in " + country + " is already present.")
+            else:
+                test_activity = [act for act in eidb_reg if "custom_heat_process_" + str(country) + "_" in act['code'] and country in act['location']]
+                if test_activity == []:
+                        print("Creating heat production activity for in " + country + ".")
+                        CreateHeatActivities.create_heat_production_process(country,"",eidb_reg)
+                else:
+                    print("Heat production activity for in " + country + " is already present.")
