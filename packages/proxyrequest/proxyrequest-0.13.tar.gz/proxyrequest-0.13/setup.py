a30016351de00679011a9253583db3b3bfd90490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:06:48 +0000

    Add package proxyrequest/0.13/proxyrequest-0.13.tar.gz

---
+++ packages/proxyrequest/proxyrequest-0.13.tar.gz/proxyrequest-0.13/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proxyrequest",  # Name of your package
+    version="0.13",  # Initial version
+    install_requires=[
+        "requests==2.31.0",
+        # "UnicodeDammit",  # Remove if it's incorrectly added
+    ],
+    author="Ramesh Chandra",
+    author_email="rameshsofter@gmail.com",
+    description="A Python package for making HTTP requests with proxy support and fetching HTML content using requests or Selenium.",
+    long_description=open('README.md').read(),  # Optional: include a README
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # This will find all the packages in your project
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
