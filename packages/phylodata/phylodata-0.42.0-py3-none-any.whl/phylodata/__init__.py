3b89b8b1ed40536f43362d3a92e9287f25ecb237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:26 +0000

    Add package phylodata/0.42.0/phylodata-0.42.0-py3-none-any.whl

---
+++ packages/phylodata/phylodata-0.42.0-py3-none-any.whl/phylodata/__init__.py
@@ -0,0 +1,13 @@
+from phylodata.data_types import FileType  # noqa: F401
+from phylodata.loader.get_files import (  # noqa: F401
+    get_file,
+    get_file_of_type,
+    get_files,
+    get_files_of_type,
+)
+from phylodata.loader.load_experiment import (  # noqa: F401
+    ExperimentToLoad,
+    load_experiment,
+    load_experiment_from_local_dir,
+    load_experiments,
+)
