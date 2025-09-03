03339704d7ef1402d6da4e2f0d345b318eae6c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:12 +0000

    Add package core-system-grpc/0.2.56/core_system_grpc-0.2.56.tar.gz

---
+++ packages/core-system-grpc/core_system_grpc-0.2.56.tar.gz/core_system_grpc-0.2.56/generated/python/__init__.py
@@ -0,0 +1,23 @@
+"""Core System gRPC Package - Python Client Library"""
+
+from .services.product_service_pb2 import *
+from .services.product_service_pb2_grpc import *
+from .messages.common.pagination_pb2 import *
+from .messages.common.search_pb2 import *
+from .messages.product.common_pb2 import *
+from .messages.product.request_pb2 import *
+from .messages.product.response_pb2 import *
+
+__version__ = "0.2.44"
+__all__ = [
+    "GetProductsByShopRequest",
+    "GetProductsByShopResponse",
+    "ProductServiceStub",
+    "ProductServiceServicer",
+    "PaginationRequest",
+    "PaginationResponse",
+    "SearchRequest",
+    "SortRequest",
+    "SortDirection",
+    "Product",
+]
