dedc33b555cf4d1a20f89654a9aa9f13939f2c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:06:33 +0000

    Add package contrastive-rl-pytorch/0.0.10/contrastive_rl_pytorch-0.0.10.tar.gz

---
+++ packages/contrastive-rl-pytorch/contrastive_rl_pytorch-0.0.10.tar.gz/contrastive_rl_pytorch-0.0.10/tests/test_contrastive_rl.py
@@ -0,0 +1,45 @@
+import pytest
+param = pytest.mark.parametrize
+
+import torch
+
+def test_contrast_loss():
+    from contrastive_rl_pytorch.contrastive_rl import contrastive_loss
+
+    embeds1 = torch.randn(10, 512)
+    embeds2 = torch.randn(10, 512)
+
+    loss = contrastive_loss(embeds1, embeds2)
+    assert loss.numel() == 1
+
+def test_contrast_wrapper():
+    from contrastive_rl_pytorch.contrastive_rl import ContrastiveWrapper
+
+    from x_mlps_pytorch import MLP
+    encoder = MLP(16, 256, 128)
+
+    past_obs = torch.randn(10, 16)
+    future_obs = torch.randn(10, 16)
+
+    wrapper = ContrastiveWrapper(encoder)
+
+    loss = wrapper(past_obs, future_obs)
+    assert loss.numel() == 1
+
+@param('var_traj_len', (False, True))
+def test_contrast_trainer(
+    var_traj_len
+):
+    from contrastive_rl_pytorch.contrastive_rl import ContrastiveRLTrainer
+    from x_mlps_pytorch import MLP
+
+    encoder = MLP(16, 256, 128)
+
+    trainer = ContrastiveRLTrainer(
+        encoder,
+        cpu = True
+    )
+
+    trajectories = torch.randn(256, 512, 16)
+
+    trainer(trajectories, 2, lens = torch.randint(256, 512, (256,)) if var_traj_len else None)
