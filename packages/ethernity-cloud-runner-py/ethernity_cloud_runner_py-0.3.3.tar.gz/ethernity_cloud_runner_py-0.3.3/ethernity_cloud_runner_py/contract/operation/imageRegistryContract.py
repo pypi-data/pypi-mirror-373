6eecc184d4729781154e0e7c75d8766e869ed8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:08:36 +0000

    Add package ethernity-cloud-runner-py/0.3.3/ethernity_cloud_runner_py-0.3.3.tar.gz

---
+++ packages/ethernity-cloud-runner-py/ethernity_cloud_runner_py-0.3.3.tar.gz/ethernity_cloud_runner_py-0.3.3/ethernity_cloud_runner_py/contract/operation/imageRegistryContract.py
@@ -0,0 +1,71 @@
+from typing import Any
+import os
+from eth_typing import Address
+from eth_utils.address import to_checksum_address
+from web3 import Web3
+from web3.contract.contract import Contract
+from web3.middleware import ExtraDataToPOAMiddleware
+from web3.types import TxParams
+
+from ...contract.abi.imageRegistryAbi import contract
+from ...enums import ECNetwork
+
+class ImageRegistryContract:
+    def __init__(
+        self,
+        network_name = "BOXBERG", 
+        network_type = "TESTNET",
+        signer: Any = None,
+        request_kwargs=None
+    ):
+        self.signer = signer
+        
+        
+        # Access the network class (e.g., ECNetwork.BLOXBERG)
+        network_class = getattr(ECNetwork, network_name.upper())
+        
+        # Access the network type class within the network class (e.g., ECNetwork.BLOXBERG.TESTNET)
+        self.network_config = getattr(network_class, network_type.upper())
+
+        self.provider = Web3(Web3.HTTPProvider(self.network_config.RPC_URL,request_kwargs=request_kwargs or {'timeout': 10}))
+
+        if self.network_config.MIDDLEWARE == "POA":
+            self.provider.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+            
+        self.contract = self.provider.eth.contract(
+            address=to_checksum_address(
+                self.network_config.IMAGE_REGISTRY_CONTRACT_ADDRESS
+            ),
+            abi=contract["abi"],
+        )
+
+    def get_signer(self) -> Any:
+        return self.signer
+
+    def get_contract(self) -> Contract:
+        return self.contract
+
+    def get_provider(self) -> Web3:
+        return self.provider
+
+    def get_enclave_details_v3(
+        self, secureLockImage: str, secureLockVersion: str, trustedZoneImage: str = "", trustedZoneVersion: str = ""
+    ) -> Any:
+        try:
+            if not trustedZoneImage or secureLockImage == trustedZoneImage:
+                return self.contract.functions.getLatestTrustedZoneImageCertPublicKey(
+                    secureLockImage, trustedZoneVersion
+                ).call()
+            else:
+                trustedZonePublicKey = (
+                    self.contract.functions.getLatestTrustedZoneImageCertPublicKey(
+                        trustedZoneImage, trustedZoneVersion
+                    ).call()[1]
+                )
+                imageDetails = self.contract.functions.getLatestImageVersionPublicKey(
+                    secureLockImage, secureLockVersion
+                ).call()
+                return [imageDetails[0], trustedZonePublicKey, imageDetails[2]]
+        except Exception as e:
+            print(e)
+            return None
---
+++ packages/ethernity-cloud-runner-py/ethernity_cloud_runner_py-0.3.3.tar.gz/ethernity_cloud_runner_py-0.3.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (
+    this_directory / "ethernity_cloud_runner_py" / "README.md"
+).read_text()
+
+print(this_directory);
+
+
+setup(
+    name="ethernity-cloud-runner-py",
+    version="0.3.3",
+    url="https://github.com/ethernity-cloud/ethernity-cloud-runner-py",
+    author="Ethernity Cloud Team",
+    author_email="contact@ethernity.cloud",
+    description="Ethernity Cloud Runner Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages("."),
+    install_requires=[
+        "web3==7.6.0",
+        "eth-typing",
+        "ipfshttpclient",
+        "python-dotenv",
+        "tinyec",
+        "cryptography",
+        "requests",
+        "pyasn1",
+        "pynacl",
+        "ckzg==2.0.1"
+    ],
+)
