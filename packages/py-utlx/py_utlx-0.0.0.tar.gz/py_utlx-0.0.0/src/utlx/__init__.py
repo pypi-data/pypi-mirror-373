ae0161e025322de45a12f08fdd7298c4ea343ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:06:29 +0000

    Add package py-utlx/0.0.0/py_utlx-0.0.0.tar.gz

---
+++ packages/py-utlx/py_utlx-0.0.0.tar.gz/py_utlx-0.0.0/src/utlx/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2004 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
+
+from ._utlx import * ; del _utlx  # type: ignore[name-defined]  # noqa
---
+++ packages/py-utlx/py_utlx-0.0.0.tar.gz/py_utlx-0.0.0/tests/test_main.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2004 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+import unittest
+
+import utlx
+from utlx import *
+
+
+class UtlxTestCase(unittest.TestCase):
+
+    def test_utlx(self):
+        pass
