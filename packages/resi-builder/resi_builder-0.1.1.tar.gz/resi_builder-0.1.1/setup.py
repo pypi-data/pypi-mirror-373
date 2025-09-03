98bf0acf148bfb15ba2274204181fbc8d2fe4800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 15:06:08 +0000

    Add package resi-builder/0.1.1/resi_builder-0.1.1.tar.gz

---
+++ packages/resi-builder/resi_builder-0.1.1.tar.gz/resi_builder-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="resi-builder",
+    version="0.1.1",
+    author="Mario Cerda",
+    author_email="cerdamario13@gmail.com",
+    description="Create a resume and cover letter automatically",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["resi", "resi.*"]),
+    python_requires=">=3.10",
+)
