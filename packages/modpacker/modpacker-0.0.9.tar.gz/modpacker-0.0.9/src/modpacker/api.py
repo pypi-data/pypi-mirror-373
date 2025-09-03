c511b97b47598eff61e39ca4cb5dbe52a798d0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:56 +0000

    Add package modpacker/0.0.9/modpacker-0.0.9.tar.gz

---
+++ packages/modpacker/modpacker-0.0.9.tar.gz/modpacker-0.0.9/src/modpacker/api.py
@@ -0,0 +1,40 @@
+import json
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+session = requests.Session()
+
+
+def get(url: str) -> dict:
+    r = session.get(url)
+    try:
+        return r.json()
+    except Exception:
+        logger.error("API GET call was unsuccessful!")
+        logger.error(f"URL: {url}")
+        logger.error(f"Return code: {r.status_code}")
+        if r.text:
+            logger.error("Response:")
+            logger.error(r.text)
+
+
+def post(url: str, data: dict) -> dict:
+    r = session.post(
+        url,
+        data=json.dumps(data),
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+    try:
+        return r.json()
+    except Exception:
+        logger.error("API POST call was unsuccessful!")
+        logger.error(f"URL: {url}")
+        logger.error(f"Return code: {r.status_code}")
+        if r.text:
+            logger.error("Response:")
+            logger.error(r.text)
