4cbf74a557b5d46d9d57e5a191ef1a96c87ea7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:05:54 +0000

    Add package edges/1.0.0/edges-1.0.0-py3-none-any.whl

---
+++ packages/edges/edges-1.0.0-py3-none-any.whl/edges/__init__.py
@@ -0,0 +1,12 @@
+"""
+edges: A Python package for calculating the environmental impact of products by
+applying characterization factors conditioned by the context of exchanges.
+"""
+
+__all__ = ("EdgeLCIA", "CostLCIA", "get_available_methods")
+
+__version__ = "1.0.0"
+
+from .edgelcia import EdgeLCIA
+from .costs import CostLCIA
+from .utils import get_available_methods
