0ffe4aef9854ffd6c0d88fdf6eb350c115726285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:06:31 +0000

    Add package gh-comment-reader/0.1.0/gh_comment_reader-0.1.0.tar.gz

---
+++ packages/gh-comment-reader/gh_comment_reader-0.1.0.tar.gz/gh_comment_reader-0.1.0/github_comment_reader/__init__.py
@@ -0,0 +1,7 @@
+"""GitHub Comment Reader - CLI tool for easily reading GitHub PR comments."""
+
+__version__ = "0.1.0"
+
+from pathlib import Path
+
+GITHUB_COMMENTS_BASE_DIR = Path(__file__).parent
