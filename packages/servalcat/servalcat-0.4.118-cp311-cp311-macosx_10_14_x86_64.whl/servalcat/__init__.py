ad2e4fd2c608ab66acd76d8209f898999ecbc412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 04:07:34 +0000

    Add package servalcat/0.4.118/servalcat-0.4.118-cp311-cp311-macosx_10_14_x86_64.whl

---
+++ packages/servalcat/servalcat-0.4.118-cp311-cp311-macosx_10_14_x86_64.whl/servalcat/__init__.py
@@ -0,0 +1,10 @@
+"""
+Author: "Keitaro Yamashita, Garib N. Murshudov"
+MRC Laboratory of Molecular Biology
+    
+This software is released under the
+Mozilla Public License, version 2.0; see LICENSE.
+"""
+
+__version__ = '0.4.118'
+__date__ = '2025-08-24'
