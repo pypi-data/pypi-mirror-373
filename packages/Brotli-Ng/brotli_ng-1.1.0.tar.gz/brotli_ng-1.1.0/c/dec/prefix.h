961105bd5010ac859fceb95491b21b8a58d1adb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:25 +0000

    Add package Brotli-Ng/1.1.0/brotli_ng-1.1.0.tar.gz

---
+++ packages/Brotli-Ng/brotli_ng-1.1.0.tar.gz/brotli_ng-1.1.0/c/dec/prefix.h
@@ -0,0 +1,28 @@
+/* Copyright 2013 Google Inc. All Rights Reserved.
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+/* Lookup tables to map prefix codes to value ranges. This is used during
+   decoding of the block lengths, literal insertion lengths and copy lengths. */
+
+#ifndef BROTLI_DEC_PREFIX_H_
+#define BROTLI_DEC_PREFIX_H_
+
+#include "../common/constants.h"
+#include "../common/platform.h"  /* IWYU pragma: keep */
+
+typedef struct CmdLutElement {
+  uint8_t insert_len_extra_bits;
+  uint8_t copy_len_extra_bits;
+  int8_t distance_code;
+  uint8_t context;
+  uint16_t insert_len_offset;
+  uint16_t copy_len_offset;
+} CmdLutElement;
+
+BROTLI_INTERNAL extern const BROTLI_MODEL("small")
+    CmdLutElement kCmdLut[BROTLI_NUM_COMMAND_SYMBOLS];
+
+#endif  /* BROTLI_DEC_PREFIX_H_ */
