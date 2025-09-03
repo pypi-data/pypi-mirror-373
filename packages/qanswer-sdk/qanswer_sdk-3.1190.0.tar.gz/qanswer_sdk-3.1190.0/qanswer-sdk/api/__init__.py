ff107f1f940d2de4f1b9c62eea9223a1f42cd2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:05:30 +0000

    Add package qanswer-sdk/3.1190.0/qanswer_sdk-3.1190.0.tar.gz

---
+++ packages/qanswer-sdk/qanswer_sdk-3.1190.0.tar.gz/qanswer_sdk-3.1190.0/qanswer-sdk/api/__init__.py
@@ -0,0 +1,47 @@
+# flake8: noqa
+
+# import apis into api package
+from qanswer-sdk.api.admin_api import AdminApi
+from qanswer-sdk.api.ai_assistant_api import AiAssistantApi
+from qanswer-sdk.api.ai_assistant_access_rights_api import AiAssistantAccessRightsApi
+from qanswer-sdk.api.bot_slack_api import BotSlackApi
+from qanswer-sdk.api.chat_completion_api import ChatCompletionApi
+from qanswer-sdk.api.chatbot_api import ChatbotApi
+from qanswer-sdk.api.connector_document_api import ConnectorDocumentApi
+from qanswer-sdk.api.connector_e_consilium_api import ConnectorEConsiliumApi
+from qanswer-sdk.api.connector_google_drive_api import ConnectorGoogleDriveApi
+from qanswer-sdk.api.connector_one_drive_api import ConnectorOneDriveApi
+from qanswer-sdk.api.connector_onenote_api import ConnectorOnenoteApi
+from qanswer-sdk.api.connector_pinecone_api import ConnectorPineconeApi
+from qanswer-sdk.api.connector_qn_a_api import ConnectorQnAApi
+from qanswer-sdk.api.connector_rdf_api import ConnectorRDFApi
+from qanswer-sdk.api.connector_rdf_answer_api import ConnectorRDFAnswerApi
+from qanswer-sdk.api.connector_sharepoint_api import ConnectorSharepointApi
+from qanswer-sdk.api.connector_websearch_api import ConnectorWebsearchApi
+from qanswer-sdk.api.connector_website_api import ConnectorWebsiteApi
+from qanswer-sdk.api.connectors_api import ConnectorsApi
+from qanswer-sdk.api.connectors_data_api import ConnectorsDataApi
+from qanswer-sdk.api.conversation_logs_api import ConversationLogsApi
+from qanswer-sdk.api.dataset_config_api import DatasetConfigApi
+from qanswer-sdk.api.debug_api import DebugApi
+from qanswer-sdk.api.llm_api import LlmApi
+from qanswer-sdk.api.organizations_teams_api import OrganizationsTeamsApi
+from qanswer-sdk.api.payment_api import PaymentApi
+from qanswer-sdk.api.socket_api import SocketApi
+from qanswer-sdk.api.speech_to_text_api import SpeechToTextApi
+from qanswer-sdk.api.tag_api import TagApi
+from qanswer-sdk.api.task_chat_api import TaskChatApi
+from qanswer-sdk.api.task_rdf_linker_api import TaskRDFLinkerApi
+from qanswer-sdk.api.task_rdf_sparql_endpoint_api import TaskRDFSparqlEndpointApi
+from qanswer-sdk.api.task_rdf_text_linker_api import TaskRDFTextLinkerApi
+from qanswer-sdk.api.task_relation_extraction_api import TaskRelationExtractionApi
+from qanswer-sdk.api.task_report_copilot_api import TaskReportCopilotApi
+from qanswer-sdk.api.task_search_api import TaskSearchApi
+from qanswer-sdk.api.task_text2sparql_api import TaskText2sparqlApi
+from qanswer-sdk.api.task_text_classification_api import TaskTextClassificationApi
+from qanswer-sdk.api.task_sparql2text_api import TaskSparql2textApi
+from qanswer-sdk.api.user_api import UserApi
+from qanswer-sdk.api.connector_e_consilium_search_index_connector_api import ConnectorEConsiliumSearchIndexConnectorApi
+from qanswer-sdk.api.connector_publication_office_controller_api import ConnectorPublicationOfficeControllerApi
+from qanswer-sdk.api.llm_consumption_controller_api import LlmConsumptionControllerApi
+
