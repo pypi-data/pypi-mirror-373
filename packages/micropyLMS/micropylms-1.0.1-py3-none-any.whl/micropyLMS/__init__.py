e9dfa504d9d474d3d1cfb0e442444382222c4d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:06:01 +0000

    Add package micropyLMS/1.0.1/micropylms-1.0.1-py3-none-any.whl

---
+++ packages/micropyLMS/micropylms-1.0.1-py3-none-any.whl/micropyLMS/__init__.py
@@ -0,0 +1,9 @@
+"""
+Author: Brent Goode
+
+Dependency lite and micropython friendly library for interacting with Lyrion 
+Music Server (LMS, nee Squezebox Server) systems
+https://lyrion.org/reference/lyrion-music-server/
+"""
+
+__all__ = ['build_url','core_query','get_players','get_player','Player']
