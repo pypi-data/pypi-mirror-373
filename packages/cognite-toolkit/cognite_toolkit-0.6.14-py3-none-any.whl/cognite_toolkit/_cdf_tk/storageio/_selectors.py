bdda7efff035a6e9ed3f179af486b3a427247a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:34 +0000

    Add package cognite-toolkit/0.6.14/cognite_toolkit-0.6.14-py3-none-any.whl

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.14-py3-none-any.whl/cognite_toolkit/_cdf_tk/storageio/_selectors.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from cognite_toolkit._cdf_tk.utils.file import to_directory_compatible
+
+
+@dataclass(frozen=True)
+class AssetCentricSelector:
+    """Data class for asset-centric data selection."""
+
+
+@dataclass(frozen=True)
+class DataSetSelector(AssetCentricSelector):
+    """Select data associated with a specific data set."""
+
+    data_set_external_id: str
+
+    def __str__(self) -> str:
+        return f"DataSet={to_directory_compatible(self.data_set_external_id)}"
+
+
+@dataclass(frozen=True)
+class AssetSubtreeSelector(AssetCentricSelector):
+    """Select data associated with an asset and its subtree."""
+
+    hierarchy: str
+
+    def __str__(self) -> str:
+        return f"AssetSubtree={to_directory_compatible(self.hierarchy)}"
+
+
+@dataclass(frozen=True)
+class AssetCentricFileSelector(AssetCentricSelector):
+    """Select data from a specific file."""
+
+    datafile: Path
+
+    def __str__(self) -> str:
+        return f"File={self.datafile.name}"
