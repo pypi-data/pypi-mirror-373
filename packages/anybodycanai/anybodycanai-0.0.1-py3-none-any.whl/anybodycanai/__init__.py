053a2f3d1a32ce2ab16f74771e7fd0852ac8c60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:06:22 +0000

    Add package anybodycanai/0.0.1/anybodycanai-0.0.1-py3-none-any.whl

---
+++ packages/anybodycanai/anybodycanai-0.0.1-py3-none-any.whl/anybodycanai/__init__.py
@@ -0,0 +1,1 @@
+from .core import starter_function
---
+++ packages/anybodycanai/anybodycanai-0.0.1-py3-none-any.whl/anybodycanai/core.py
@@ -0,0 +1,10 @@
+def starter_function(name: str = "AI") -> str:
+    """A starter function to say hello to the world.
+
+    Args:
+        name (str, optional): Name of the person to say hello to. Defaults to "World".
+
+    Returns:
+        str: A string saying hello to the world.
+    """
+    return f"Hello {name}!"
