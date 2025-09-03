2954c6713ab5fab6bf8be6b3c509da41143931a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:06:00 +0000

    Add package octonomicon-shared/0.1.3/octonomicon_shared-0.1.3-py3-none-any.whl

---
+++ packages/octonomicon-shared/octonomicon_shared-0.1.3-py3-none-any.whl/octonomicon_shared/utils/env/load_env_variable.py
@@ -0,0 +1,30 @@
+import os
+from typing import Literal, overload
+from octonomicon_shared.errors import EnvError
+from dotenv import load_dotenv
+
+
+@overload
+def load_env_variable(key: str, *, dotenv_path: str | None = None) -> str: ...
+
+
+@overload
+def load_env_variable(
+    key: str, *, required: Literal[True], dotenv_path: str | None = None) -> str: ...
+
+
+@overload
+def load_env_variable(
+    key: str, *, required: Literal[False], dotenv_path: str | None = None) -> str | None: ...
+
+
+def load_env_variable(key: str, *, required: bool = True, dotenv_path: str | None = None):
+    value = os.getenv(key)
+
+    if not value:
+        load_dotenv(dotenv_path)
+
+    if required and not value:
+        raise EnvError(key)
+
+    return value
