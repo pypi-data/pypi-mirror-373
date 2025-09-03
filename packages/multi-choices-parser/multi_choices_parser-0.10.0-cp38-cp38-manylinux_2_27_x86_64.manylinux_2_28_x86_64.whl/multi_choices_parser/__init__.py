b1aaaa9697d141d4ed039a9df62934a3d9669757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 06:07:01 +0000

    Add package multi-choices-parser/0.10.0/multi_choices_parser-0.10.0-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/multi-choices-parser/multi_choices_parser-0.10.0-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/multi_choices_parser/__init__.py
@@ -0,0 +1,10 @@
+# Software Name : multi-choices-parser
+# SPDX-FileCopyrightText: Copyright (c) 2025 Orange SA
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# This software is distributed under the GNU General Public License v2.0 or later,
+# see the "LICENSE.txt" file for more details or GNU General Public License v2.0 or later
+
+# Authors: Hichem Ammar Khodja
+
+from .parser import MultiChoicesParser, DEFAULT_END_SYMB, ParserError
---
+++ packages/multi-choices-parser/multi_choices_parser-0.10.0-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/multi_choices_parser/_core.pyi
@@ -0,0 +1,12 @@
+# Software Name : multi-choices-parser
+# SPDX-FileCopyrightText: Copyright (c) 2025 Orange SA
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# This software is distributed under the GNU General Public License v2.0 or later,
+# see the "LICENSE.txt" file for more details or GNU General Public License v2.0 or later
+
+# Authors: Hichem Ammar Khodja
+
+from __future__ import annotations
+
+def construct_tree(list_of_choices : list[list[list[int]]]) -> str: ...
