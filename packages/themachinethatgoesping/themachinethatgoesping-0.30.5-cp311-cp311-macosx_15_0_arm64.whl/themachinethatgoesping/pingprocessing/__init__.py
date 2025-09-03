56f016dacbdc0454225ab3157c09bad30703474c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 07:06:01 +0000

    Add package themachinethatgoesping/0.30.5/themachinethatgoesping-0.30.5-cp311-cp311-macosx_15_0_arm64.whl

---
+++ packages/themachinethatgoesping/themachinethatgoesping-0.30.5-cp311-cp311-macosx_15_0_arm64.whl/themachinethatgoesping/pingprocessing/__init__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 - 2023 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# folders
+from . import core
+from . import filter_pings
+from . import split_pings
+from . import group_pings
+from . import overview
+from . import watercolumn as watercolumn_ext  # flake8: noqa
+from . import widgets
+from . import testing
+
+# modules
+
+#cpp modules
+from themachinethatgoesping.pingprocessing_cppy import *  # flake8: noqa
+
+# overwrite timeconv module using the loaded python extension
+watercolumn = watercolumn_ext
+
+__version__ = "@PROJECT_VERSION@"
