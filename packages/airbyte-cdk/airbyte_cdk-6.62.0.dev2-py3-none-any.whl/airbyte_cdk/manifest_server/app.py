e83b1020fd37ff23475104a01670f01c62f4fc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 05:06:39 +0000

    Add package airbyte-cdk/6.62.0.dev2/airbyte_cdk-6.62.0.dev2-py3-none-any.whl

---
+++ packages/airbyte-cdk/airbyte_cdk-6.62.0.dev2-py3-none-any.whl/airbyte_cdk/manifest_server/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from .routers import capabilities, health, manifest
+
+app = FastAPI(
+    title="Manifest Server",
+    description="A service for running low-code Airbyte connectors",
+    version="0.1.0",
+    contact={
+        "name": "Airbyte",
+        "url": "https://airbyte.com",
+    },
+)
+
+app.include_router(health.router)
+app.include_router(capabilities.router)
+app.include_router(manifest.router, prefix="/v1")
