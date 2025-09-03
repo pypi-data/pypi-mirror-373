8cfc91adea35c41fbc897a6d632c8a44f748992b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:25 +0000

    Add package cloxz/0.6.3/cloxz-0.6.3.tar.gz

---
+++ packages/cloxz/cloxz-0.6.3.tar.gz/cloxz-0.6.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloxz",
+    version="0.6.3",
+    packages=find_packages(),
+    install_requires=["typer", "rich"],
+    entry_points={
+        "console_scripts": [
+            "cxz=clock.main:app",
+        ],
+    },
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
