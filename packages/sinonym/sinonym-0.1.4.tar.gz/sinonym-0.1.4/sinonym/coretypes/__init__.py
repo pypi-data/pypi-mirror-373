b0f339ba4f38dc030029918afd4dbb97e0a45cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 16:06:36 +0000

    Add package sinonym/0.1.4/sinonym-0.1.4.tar.gz

---
+++ packages/sinonym/sinonym-0.1.4.tar.gz/sinonym-0.1.4/sinonym/coretypes/__init__.py
@@ -0,0 +1,28 @@
+"""
+Core types for Chinese name processing.
+
+This package contains result types, configuration classes, and other
+data structures used throughout the Chinese name detection system.
+"""
+
+from sinonym.coretypes.config import ChineseNameConfig
+from sinonym.coretypes.results import (
+    BatchFormatPattern,
+    BatchParseResult,
+    CacheInfo,
+    IndividualAnalysis,
+    NameFormat,
+    ParseCandidate,
+    ParseResult,
+)
+
+__all__ = [
+    "BatchFormatPattern",
+    "BatchParseResult",
+    "CacheInfo",
+    "ChineseNameConfig",
+    "IndividualAnalysis",
+    "NameFormat",
+    "ParseCandidate",
+    "ParseResult",
+]
