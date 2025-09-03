71b10525659fd9149dd7c644ccf1e58ed554c3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 18:07:02 +0000

    Add package cli-base-utilities/0.22.0/cli_base_utilities-0.22.0-py3-none-any.whl

---
+++ packages/cli-base-utilities/cli_base_utilities-0.22.0-py3-none-any.whl/cli_base/click_defaults.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import rich_click as click
+
+
+OPTION_ARGS_DEFAULT_TRUE = dict(is_flag=True, show_default=True, default=True)
+OPTION_ARGS_DEFAULT_FALSE = dict(is_flag=True, show_default=True, default=False)
+ARGUMENT_EXISTING_DIR = dict(
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True, path_type=Path)
+)
+ARGUMENT_NOT_EXISTING_DIR = dict(
+    type=click.Path(
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+        readable=False,
+        writable=True,
+        path_type=Path,
+    )
+)
+ARGUMENT_EXISTING_FILE = dict(
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, path_type=Path)
+)
