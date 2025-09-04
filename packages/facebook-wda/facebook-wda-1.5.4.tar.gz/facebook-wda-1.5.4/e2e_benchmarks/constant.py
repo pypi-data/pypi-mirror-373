e5845d7085114703173232fdcb8c00da56881830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:14:04 +0000

    Add package facebook-wda/1.5.4/facebook-wda-1.5.4.tar.gz

---
+++ packages/facebook-wda/facebook-wda-1.5.4.tar.gz/facebook-wda-1.5.4/e2e_benchmarks/constant.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+UNDER_TEST_BUNDLE_ID = "com.test.cert.TestCert"
+
+UNKNOW_BUNDLE_ID = "com.apple.unknown"
+
+HOME_SCREEN_BUNDLE_ID = "com.apple.springboard"
+
+class ALERT_ENUMS(Enum):
+    CONFIRM = "ACCEPT_OR_REJECT_ALERT"
+    INPUT = "INPUT_ALERT"
+    
+
