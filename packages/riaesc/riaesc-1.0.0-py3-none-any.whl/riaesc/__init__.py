a9be9dca072357efff71a9227f889645fc570b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:14:24 +0000

    Add package riaesc/1.0.0/riaesc-1.0.0-py3-none-any.whl

---
+++ packages/riaesc/riaesc-1.0.0-py3-none-any.whl/riaesc/__init__.py
@@ -0,0 +1,4 @@
+from .asyncsocks import AsyncSOCKSSocket, SOCKSError, AuthenticationError, ConnectionError, create_connection
+
+__version__ = "1.0.0"
+__all__ = ["AsyncSOCKSSocket", "SOCKSError", "AuthenticationError", "ConnectionError", "create_connection"]
