fc924ca707d07200c6b92f4e003fbc198ccbbfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:14:12 +0000

    Add package databoost/0.1.4/databoost-0.1.4.tar.gz

---
+++ packages/databoost/databoost-0.1.4.tar.gz/databoost-0.1.4/src/databoost/__init__.py
@@ -0,0 +1,12 @@
+"""
+Databoost - Advanced Data Science utilities
+Includes:
+- Custom metrics (metrics)
+- Advanced plotting (plots)
+- Future ML helpers
+"""
+
+__version__ = "0.1.4"
+
+from . import metrics
+from . import plots
