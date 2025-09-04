0731b3f77463bd92d4f96304890adf8a83c0f863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:14:12 +0000

    Add package databoost/0.1.4/databoost-0.1.4-py3-none-any.whl

---
+++ packages/databoost/databoost-0.1.4-py3-none-any.whl/databoost/metrics/ScoS.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def ScoS(y_true, y_pred, alpha=1, method="linear"):
+    """
+    Scolz's Score (ScoS)
+    Penalizes more distant errors.
+    
+    Parameters:
+    ----------
+    y_true : array-like
+        Array o lista dei valori reali
+    y_pred : array-like
+        Array o lista dei valori predetti
+    alpha : float, optional
+        Esponente che aumenta il peso degli errori più grandi (default=1)
+
+    Returns:
+    -------
+    float
+        ScoS score: più basso = migliore
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    
+    diff = np.abs(y_true - y_pred)
+
+    if method = "linear":
+        weighted = diff * alpha
+    elif method = "quadratic":
+        weighted = diff**2 * alpha
+    elif method = "log":
+        weighted = np.log2(diff) * alpah
+    
+    score = weighted.mean()
+    
+    return score
